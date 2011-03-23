b963a1c590250812b4117ac94ce50994600e5343
pmeenan <pmeenan@webpagetest.org>
2011-03-23 13:31:11 +0000

    Switched the extension to use the official navigation timing spec and only execute the script after the page has completed.

--- agent/browser/chrome/extension/script.js
+++ agent/browser/chrome/extension/script.js
@@ -2,28 +2,13 @@
 // loaded.
 
 // The port for communicating back to the extension.
+var WPT_load_time = 0;
+try {
+WPT_load_time = window.performance.timing['loadEventStart'] 
+                - window.performance.timing['navigationStart'];
+} catch(e) {}
+
+// send the navigation timings back to the extension
 var WPTExtensionConnection = chrome.extension.connect();
-
-// The url of the page load.
-var WPTExtensionPageUrl = window.location.toString();
-
-function sendTimesToExtension() {
-  if (window.parent != window) {
-    return;
-  }
-  var load_times = window.chrome.loadTimes();
-  // For now, wait for finishedLoadTime by deferring this message-post with a
-  // timer.
-  if (load_times.finishLoadTime != 0) {
-    WPTExtensionConnection.postMessage({message: 'load',
-                                        url: WPTExtensionPageUrl,
-                                        values: load_times});
-  } else {
-    window.setTimeout(sendTimesToExtension, 100);
-  }
-}
-
-// Currently this script runs at document-idle, but need to make sure it is run
-// after all activity in the page is done.
-sendTimesToExtension();
-
+WPTExtensionConnection.postMessage({message: 'load',
+								load_time: WPT_load_time});
