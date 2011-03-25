c05f21b9178b5399a92f45abe17d2a997e2c54f4
pmeenan <pmeenan@webpagetest.org>
2011-03-25 20:46:26 +0000

    Added support for recognizing deflate in addition to gzip

--- agent/browser/ie/pagetest/PagetestReporting.cpp
+++ agent/browser/ie/pagetest/PagetestReporting.cpp
@@ -1844,7 +1844,7 @@ void CPagetestReporting::CheckGzip(CString &buff)
 				totalBytes += w->in;
 				DWORD target = w->in;
 				
-				if( enc.Find(_T("gzip")) >= 0 )
+				if( enc.Find(_T("gzip")) >= 0 || enc.Find(_T("deflate")) >= 0 )
 					w->gzipScore = 100;
 				else if( w->in < 1400 )	// if it's less than 1 packet anyway, give it a pass
 					w->gzipScore = 100;
