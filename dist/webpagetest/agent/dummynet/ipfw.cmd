2b71069ea6bb0dde6587e90f509b28853e19f12b
pmeenan <pmeenan@webpagetest.org>
2011-04-19 12:56:38 +0000

    Changed the ipfw configuration to use queues to more fairly balance the bandwidth across the different sockets (from the experience on the web page replay project).

--- dist/webpagetest/agent/dummynet/ipfw.cmd
+++ dist/webpagetest/agent/dummynet/ipfw.cmd
@@ -3,8 +3,10 @@ set CYGWIN=nodosfilewarning
 ipfw -q flush
 ipfw -q pipe flush
 
-ipfw pipe 1 config delay 0ms
-ipfw pipe 2 config delay 0ms
+ipfw pipe 1 config delay 0ms noerror
+ipfw pipe 2 config delay 0ms noerror
+ipfw queue 1 config pipe 1 queue 100 noerror mask dst-port 0xffff
+ipfw queue 2 config pipe 2 queue 100 noerror mask src-port 0xffff
 
 ipfw add skipto 60000 proto tcp src-port 80 out
 ipfw add skipto 60000 proto tcp dst-port 80 in
@@ -13,7 +15,7 @@ ipfw add skipto 60000 proto tcp dst-port 445 in
 ipfw add skipto 60000 proto tcp src-port 3389 out
 ipfw add skipto 60000 proto tcp dst-port 3389 in
 
-ipfw add pipe 1 ip from any to any in
-ipfw add pipe 2 ip from any to any out
+ipfw add queue 1 ip from any to any in
+ipfw add queue 2 ip from any to any out
 
 ipfw add 60000 allow ip from any to any
