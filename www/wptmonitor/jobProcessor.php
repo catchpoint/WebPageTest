224d6d220576d2f54da97791f40ec1389a980d55
tonyperkins <perkins.tony@gmail.com>
2011-04-30 18:28:20 +0000

    changed rounding logic

--- www/wptmonitor/jobProcessor.php
+++ www/wptmonitor/jobProcessor.php
@@ -63,7 +63,7 @@ include 'alert_functions.inc';
               $message .= "Ratio: NO TESTERS SERVING THIS REGION\n";
             }
             $message .= "Target Ratio: 40\n";
-            $message .= "Please add ".round($requiredAdditionalTesters,0)." additional agents for this location.";
+            $message .= "Please add ".ceil($requiredAdditionalTesters)." additional agents for this location.";
         }
       }
     }
