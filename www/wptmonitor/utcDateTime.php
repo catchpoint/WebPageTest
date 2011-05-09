a65c6534626b928a1a1f395750d8c3b7a47a2b0e
tonyperkins <perkins.tony@gmail.com>
2011-05-09 18:30:42 +0000

    Fix for: PHP Notice:  Undefined index:  edited_user_timezone in /var/www/html/wptmonitor/utcDateTime.php

--- www/wptmonitor/utcDateTime.php
+++ www/wptmonitor/utcDateTime.php
@@ -1,19 +1,24 @@
 <script type="text/javascript">
-  function changeTimeZone(zone){
+  function changeTimeZone(zone) {
     idx = zone.selectedIndex;
     document.getElementById('timeZoneForm').submit();
   }
 </script>
-  <form id="timeZoneForm" action="">
-<?php
-  include_once 'utils.inc';
-
-  if ($timezone=$_REQUEST['edited_user_timezone']){
-    $_SESSION['ls_timezone'] = $timezone;
+<form id="timeZoneForm" action="">
+  <?php
+    include_once 'utils.inc';
+  // Default if we don't find a setting.
+  $timezone = 'GMT';
+  if (array_key_exists('edited_user_timezone', $_REQUEST)) {
+    if (!empty($_REQUEST['edited_user_timezone'])) {
+      $timezone = $_REQUEST['edited_user_timezone'];
+    }
   }
-    date_default_timezone_set($_SESSION['ls_timezone']);
+  $_SESSION['ls_timezone'] = $timezone;
 
-  print date('m/d/Y H:i:s')."<br>Timezone: ";
-  print get_tz_options(date_default_timezone_get(),"edited_user_timezone","Timezone","","changeTimeZone(this);");
-?>
+  date_default_timezone_set($_SESSION['ls_timezone']);
+
+  print date('m/d/Y H:i:s') . "<br>Timezone: ";
+  print get_tz_options(date_default_timezone_get(), "edited_user_timezone", "Timezone", "", "changeTimeZone(this);");
+  ?>
 </form>
