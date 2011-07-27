485593a4a8abdb8db5bb78bd3660b69a4607f1bc
skerner <samkerner.github@gmail.com>
2011-07-27 17:55:14 +0000

    Add method and tests to find a DOM element using the attribute'value syntax.
    This method will be used to implement the click command in a subsequent change.
    
    BUG=None
    TEST=Unit tests testFindDomElements*, Run on stable and canary on windows.
    
    Review URL: http://webpagetest-code-review.appspot.com/38001

--- agent/browser/chrome/extension/wpt/allTests.js
+++ agent/browser/chrome/extension/wpt/allTests.js
@@ -1,7 +1,9 @@
 goog.require('wpt.commands');
+goog.require('wpt.contentScript');
 goog.require('goog.testing.jsunit');
+goog.require('goog.testing.AsyncTestCase');
 
-goog.provide('wpt.allTest');
+goog.provide('wpt.allTests');
 
 /**
  * Test the setcookie command.
@@ -59,3 +61,81 @@ function testSetCookie() {
                      }],
                     cookieLog);
 };
+
+function testFindDomElements() {
+  // For testing, we search under a div in allTests.html:
+  var root = document.getElementById('testFindDomElements');
+
+  assertArrayEquals("No elements with attribute 'zzz'",
+                    [], wpt.contentScript.findDomElements_(root, "zzz'one"));
+
+
+  assertArrayEquals("No elements with attribute 'aaa' that have value 'zzz'",
+                    [], wpt.contentScript.findDomElements_(root, "aaa'zzz"));
+
+
+  var actual = wpt.contentScript.findDomElements_(root, "aaa'one");
+  assertEquals("One item matching \"aaa'one\"", 1, actual.length);
+  assertEquals("First span", actual[0].innerText);
+
+
+  actual = wpt.contentScript.findDomElements_(root, "bbb'two");
+  assertEquals("Two items matching \"bbb'two\"",
+               2, actual.length);
+
+  assertEquals("Items should be in tree-order: First item is a span",
+               "First span", actual[0].innerText);
+
+  assertEquals("Items should be in tree-order: Second item is a div",
+               "First div", actual[1].innerText);
+
+  actual = wpt.contentScript.findDomElements_(root, "bbb'three");
+  assertEquals("One item matching \"bbb'three\"", 1, actual.length);
+  assertEquals("Second div", actual[0].innerText);
+}
+
+function testFindDomElementWithGetElementById() {
+  // For testing, we search under a div in allTests.html:
+  var root = document.getElementById('testFindDomElements');
+
+  // Get nothing for an id that does not exist.
+  assertArrayEquals("No elements with id thisIdDoesNotExist",
+                    [], wpt.contentScript.findDomElements_(
+                        root, "id'thisIdDoesNotExist"));
+
+  // Get the element whose id we search for:
+  var actual = wpt.contentScript.findDomElements_(
+      root, "id'thisIdExists");
+  assertEquals("One elements with id thisIdDoesNotExist",
+               1, actual.length);
+  assertEquals("Should be an anchor tag.",
+               "Anchor with id", actual[0].innerText);
+}
+
+function testFindDomElementWithGetElementByName() {
+  // For testing, we search under a div in allTests.html:
+  var root = document.getElementById('testFindDomElements');
+
+  // Get nothing for a name that does not exist.
+  assertArrayEquals("No elements with name thisNmaeDoesNotExist",
+                    [], wpt.contentScript.findDomElements_(
+                        root, "name'thisIdDoesNotExist"));
+
+  // Find the one element whose name matches.
+  var actual = wpt.contentScript.findDomElements_(
+      root, "name'thisNameExists");
+  assertEquals("One element with name thisNameExists",
+               1, actual.length);
+  assertEquals("Should be an anchor tag.",
+               "Anchor with name", actual[0].innerText);
+
+  // If more than one name matches, the elemnts should be returned in DOM order.
+  var actual = wpt.contentScript.findDomElements_(
+      root, "name'thitNameHasMultipleMatches");
+  assertEquals("Two elements with name thisNameHasMultipleMatches",
+               2, actual.length);
+  assertEquals("Should be an anchor tag.",
+               "First named anchor", actual[0].innerText);
+  assertEquals("Should be an anchor tag.",
+               "Second named anchor", actual[1].innerText);
+}
--- agent/browser/chrome/extension/wpt/script.js
+++ agent/browser/chrome/extension/wpt/script.js
@@ -1,10 +1,22 @@
 
 // Closure compiler needs to see a goog.provide, but we don't load
 // closure library outside tests.  Without closure, |goog| does not exist.
+// The goog.provide call is not indented, because the closure library
+// dependency generation scrips use a regexp to find calls to goog.provide,
+// and it will not be matched if there is whitespace on the line.
 // TODO(skerner): Make the non-test content script flow have base.js,
 // so that goog.provide exists.
-if (window['goog'] != undefined)
-  goog.provide('wpt.contentscript');
+if (window['goog'])
+goog.provide('wpt.contentScript');
+
+// If the namespace is not set up by goog.provide, define the objects
+// it would set up.  We could avoid this sort of hackery by injecting
+// base.js before injecting this script, but the script injection
+// this would do has the potential to be slow enough to change the
+// measured load time.
+// TODO(skerner): Measure the timing difference.
+window['wpt'] = window['wpt'] || {};
+window.wpt['contentScript'] = window.wpt['contentScript'] || {};
 
 // This script is automatically injected into every page before it loads.
 // We need to use it to register for the earliest onLoad callback
@@ -24,3 +36,60 @@ window.addEventListener("load", function() {
   WPTExtensionConnection.postMessage(
       {message: 'wptLoad', load_time: WPT_load_time});
 }, false);
+
+
+/**
+ * WebPageTest's scripting language has several commands that act on
+ * DOM nodes.  These commands specify the DOM nodes using an attribute
+ * and a value, separated by a single quote.  A matching DOM node has
+ * an attribute with the corresponding value.
+ *
+ * If there is more than one match, all matches will be returned,
+ * in DOM order.  Commands that use this function should use the
+ * first element.
+ *
+ * @param {DOMElement} root The Element to search under. Usualy window.document.
+ * @param {string} target The pattern to match.
+ */
+wpt.contentScript.findDomElements_ = function(root, target) {
+  var delimiterIndex = target.indexOf("'");
+  if (delimiterIndex == -1)
+    throw ("Invalid target \"" + target + "\": delimter \' is missing.");
+
+  var attribute = target.substring(0, delimiterIndex);
+  var value = target.substring(delimiterIndex + 1);
+
+  if (!attribute)
+    throw ("Invalid target \"" + target +
+           "\": The attribute to search for can not be empty.");
+
+  var matchingNodeList = [];
+  switch (attribute) {
+    case "id": {
+      var element = document.getElementById(value);
+      if (element)
+        matchingNodeList.push(element);
+      break;
+    }
+    case "name": {
+      // |elements| is a DOM NodeList, not a Javascript array, so we must
+      // move the elements into an array.
+      var elements = document.getElementsByName(value);
+      for (var i = 0, ie = elements.length; i < ie; ++i) {
+        matchingNodeList.push(elements[i]);
+      }
+      break;
+    }
+    default: {
+      var fullNodeList = root.getElementsByTagName('*');
+      for (var i = 0, ie = fullNodeList.length; i < ie; ++i) {
+        if (fullNodeList[i].getAttribute(attribute) !== value)
+          continue;
+
+        matchingNodeList.push(fullNodeList[i]);
+      }
+    }
+  }
+
+  return matchingNodeList;
+};
