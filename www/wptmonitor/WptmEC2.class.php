05bb987e655bb11d17ee95f74875e14c2064bba3
tonyperkins <perkins.tony@gmail.com>
2011-09-24 18:47:53 +0000

    EC2 Control

--- www/wptmonitor/WptmEC2.class.php
+++ www/wptmonitor/WptmEC2.class.php
@@ -12,8 +12,7 @@ define('AWS_CERTIFICATE_AUTHORITY', true);
 
 class WptmEC2
 {
-  function __construct()
-  {
+  function __construct(){
     if (!defined('AWS_KEY')){
       $ec2Creds = parse_ini_file("/etc/wptm-ec2.ini");
       define ('AWS_KEY', $ec2Creds['AWS_KEY']);
@@ -21,16 +20,61 @@ class WptmEC2
     }
   }
 
+  public function deleteAutoScalingGroup($name, $opts = null){
+    $as = new AmazonAS();
+    $response = $as->delete_auto_scaling_group($name,$opts);
+    return $response;
+  }
+  public function createAutoScalingGroup($name, $launch_config_name, $min_size, $max_size, $zones, $opts= null){
+    $as = new AmazonAS();
+    $response = $as->create_auto_scaling_group($name, $launch_config_name, $min_size, $max_size, $zones, $opts);
+    return $response;
+  }
+  
+  public function deleteLaunchConfiguration($region, $name, $opts= null){
+    $as = new AmazonAS();
+//    $as->set_region($region);
+    $response= $as->delete_launch_configuration($name, $opts);
+    return $response;
+  }
+  public function createLaunchConfiguration($region, $name, $image_id, $instance_type, $opts=null){
+    $as = new AmazonAS();
+//    $as->set_region($region);
+    $response = $as->create_launch_configuration($name, $image_id, $instance_type, $opts);
+    // Success?
+//    var_dump($response->isOK());
+    return $response;
+  }
+  public function getLaunchConfigurations($opts = null){
+    $as = new AmazonAS();
+     $response = $as->describe_launch_configurations($opts);
+    // Success?
+    var_dump($response->isOK());
+    return $response;
+  }
+  /**
+   * Request spot instance(s)
+   *
+   * @param $region Region in which to fire up the instance
+   * @param $price maximum price to pay for spot instance
+   * @param $opts EC2 options
+   * @return CFResponse
+   */
   public function requestSpotInstances($region, $price, $opts)
-  {
-    $ec2 = new AmazonEC2();
+  {$ec2 = new AmazonEC2();
     $ec2->set_region($region);
     $response = $ec2->request_spot_instances($price, $opts);
     return $response;
   }
-  
-  public function terminateInstance($region, $id)
-  {
+
+  /**
+   * Terminate an EC2 Instance
+   *
+   * @param $region Region in which to terminate instance
+   * @param $id Id of instance to terminate
+   * @return CFResponse
+   */
+  public function terminateInstance($region, $id){
     $ec2 = new AmazonEC2();
     $ec2->set_region($region);
     $response = $ec2->terminate_instances($id);
@@ -38,6 +82,7 @@ class WptmEC2
   }
   /**
    * Get instances by region and optionally AMI
+   *
    * @param $region
    * @param null $ami
    * @return CFResponse
@@ -50,13 +95,16 @@ class WptmEC2
       $opts=array('Filter' => array(array('Name' => 'image-id', 'Value' => $ami)));
     }
     $response = $ec2->describe_instances($opts);
-    if ( $response->status != "200" ){
-     logOutput("[ERROR] [WptmEC2.class] EC2 Error [".$response->body->Errors->Error->Message."] while getting instances");
-    }
     return $response;
   }
-  public function getInstanceCount($region, $ami)
-  {
+
+  /**
+   * Get the number of instance of a particular AMI running in a give region.
+   * @param $region
+   * @param $ami
+   * @return int Number of instances of AMI $ami running in region $region
+   */
+  public function getInstanceCount($region, $ami){
     $ec2 = new AmazonEC2();
     $ec2->set_region($region);
     $response = $ec2->describe_instances(array(
@@ -66,7 +114,6 @@ class WptmEC2
     if ( $response->status != "200" ){
      logOutput("[ERROR] [WptmEC2.class] EC2 Error [".$response->body->Errors->Error->Message."] while getting instance count");
     }
-
     return sizeof($response->body->reservationSet->item);
   }
 }
