87d874e6018b574c5e4cd12b23b3f5d3cc28d30c
tonyperkins <perkins.tony@gmail.com>
2011-10-19 01:09:18 +0000

    Fix missing host label due to missing relation in doctrine query.

--- www/wptmonitor/listLocations.php
+++ www/wptmonitor/listLocations.php
@@ -4,11 +4,12 @@ include 'monitor.inc';
 displayErrorIfNotAdmin();
 try
 {
-    $q = Doctrine_Query::create()->from('WPTLocation l')->setHydrationMode(Doctrine_Core::HYDRATE_ARRAY);
+    $q = Doctrine_Query::create()->from('WPTLocation l, l.WPTHost h')->setHydrationMode(Doctrine_Core::HYDRATE_ARRAY);
     $locations = $q->execute();
     $q->free(true);
     $smarty->assign('result',$locations);
 }
+
 catch (Exception $e)
 {
   error_log("[WPTMonitor] Failed while Listing Locations: " . $e->getMessage());
