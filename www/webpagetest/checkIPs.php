f7b52cc1cd1c3a456ee0a29ee96bed7940eacc1f
pmeenan <pmeenan@webpagetest.org>
2011-12-12 16:24:46 +0000

    Updated the IP check to include the owner of the API key for any tests that were submitted using the API

--- www/webpagetest/checkIPs.php
+++ www/webpagetest/checkIPs.php
@@ -16,6 +16,7 @@ foreach( $wl as &$w )
 
 $counts = array();
 $dayCounts = array();
+$ip_keys = array();
 
 $targetDate = new DateTime($from, new DateTimeZone('GMT'));
 for($offset = 0; $offset <= $days; $offset++)
@@ -36,15 +37,20 @@ for($offset = 0; $offset <= $days; $offset++)
             if( isset($parts[1]) )
             {
                 $ip = trim($parts[1]);
-                if( isset($counts[$ip]) )
-                    $counts[$ip]++;
-                else
-                    $counts[$ip] = 1;
+                if( strlen($ip) ) {
+                    $key = trim($parts[13]);
+                    if( strlen($key) )
+                        $ip_keys[$ip] = $key;
+                    if( isset($counts[$ip]) )
+                        $counts[$ip]++;
+                    else
+                        $counts[$ip] = 1;
 
-                if( isset($dayCount[$ip]) )
-                    $dayCount[$ip]++;
-                else
-                    $dayCount[$ip] = 1;
+                    if( isset($dayCount[$ip]) )
+                        $dayCount[$ip]++;
+                    else
+                        $dayCount[$ip] = 1;
+                }
             }
         }
     }
@@ -58,28 +64,40 @@ for($offset = 0; $offset <= $days; $offset++)
 // sort the counts descending
 arsort($counts);
 
+// load the API keys
+$keys = parse_ini_file('./settings/keys.ini', true);
+
+echo '<html><head></head><body><table><tr><th>Total</th>';
+foreach( $dayCounts as $index => &$dayCount ) {
+    echo "<th>Day $index</th>";
+}
+echo '<th>API Key</th><th>Key Limit</th><th>IP Address</th></tr>';
+
 foreach($counts as $ip => $count)
 {
     if( $count > 50 )
     {
-        $countStr = "$count (";
+        echo "<tr><td>$count</td>";
         foreach( $dayCounts as $index => &$dayCount )
         {
             $c = 0;
             if( isset($dayCount[$ip]) )
                 $c = $dayCount[$ip];
-            if( $index )
-                $countStr .= ' ';
-            $countStr .= $c;
+            echo "<td>$c</td>";
         }
-        $countStr .= ')';
+
+        $owner = '';
+        $limit = '';            
+        $key = $ip_keys[$ip];
+        if( strlen($key) ) {
+            $owner = $keys[$key]['contact'];
+            $limit = $keys[$key]['limit'];
+        }
         
-        if( isset($whitelist[$ip]) )
-            echo "<b>$countStr - $ip</b> (whitelisted - {$whitelist[$ip]})<br>\n";
-        else
-            echo "$countStr - $ip<br>\n";
+        echo "<td>$owner</td><td>$limit</td><td>$ip</td></tr>\n";
     }
     else
         break;
 }
+echo "</table></body></html>";
 ?>
