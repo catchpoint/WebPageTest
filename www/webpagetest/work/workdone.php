f53d76155c49f6b7c772506ff0779bdff410cd53
skerner <samkerner.github@gmail.com>
2012-03-29 16:47:51 +0000

    Whitespace and remove unneeded includes.

--- www/webpagetest/work/workdone.php
+++ www/webpagetest/work/workdone.php
@@ -508,9 +508,6 @@ function ExecPcap2Har($pcapPath, $harPath, $useLatestPCap2Har,
  */
 function ProcessPCAP($testPath, $pcapFile)
 {
-    require_once('./lib/pcltar.lib.php3');
-    require_once('./lib/pclerror.lib.php3');
-    require_once('./lib/pcltrace.lib.php3');
     global $runNumber;
     global $cacheWarmed;
 
