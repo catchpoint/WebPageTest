fb891ac0f18abf16073439d94aef813341d7d166
pmeenan <pmeenan@webpagetest.org>
2012-05-16 13:59:54 +0000

    Added the Speed Index and Run number to the CSV output (augmented the on-disk data so the csv isn't just a passthrough anymore)

--- www/webpagetest/csv.php
+++ www/webpagetest/csv.php
@@ -1,17 +1,22 @@
 <?php
 include 'common.inc';
+require_once('./video/visualProgress.inc.php');
 
 // make sure the test has finished, otherwise return a 404
 if( isset($test['test']) && (isset($test['test']['completeTime']) || $test['test']['batch']) )
 {
     $fileType = 'IEWPG.txt';
     $filename = "{$id}_summary.csv";
-    $header = '"Date","Time","Event Name","URL","Load Time (ms)","Time to First Byte (ms)","unused","Bytes Out","Bytes In","DNS Lookups","Connections","Requests","OK Responses","Redirects","Not Modified","Not Found","Other Responses","Error Code","Time to Start Render (ms)","Segments Transmitted","Segments Retransmitted","Packet Loss (out)","Activity Time(ms)","Descriptor","Lab ID","Dialer ID","Connection Type","Cached","Event URL","Pagetest Build","Measurement Type","Experimental","Doc Complete Time (ms)","Event GUID","Time to DOM Element (ms)","Includes Object Data","Cache Score","Static CDN Score","One CDN Score","GZIP Score","Cookie Score","Keep-Alive Score","DOCTYPE Score","Minify Score","Combine Score","Bytes Out (Doc)","Bytes In (Doc)","DNS Lookups (Doc)","Connections (Doc)","Requests (Doc)","OK Responses (Doc)","Redirects (Doc)","Not Modified (Doc)","Not Found (Doc)","Other Responses (Doc)","Compression Score","Host","IP Address","ETag Score","Flagged Requests","Flagged Connections","Max Simultaneous Flagged Connections","Time to Base Page Complete (ms)","Base Page Result","Gzip Total Bytes","Gzip Savings","Minify Total Bytes","Minify Savings","Image Total Bytes","Image Savings","Base Page Redirects","Optimization Checked","AFT (ms)","DOM Elements","Page Speed Version","Page Title","Time to Title","Load Event Start","Load Event End","DOM Content Ready Start","DOM Content Ready End","Visually Complete (ms)"';
+    $header = '"Date","Time","Event Name","URL","Load Time (ms)","Time to First Byte (ms)","unused","Bytes Out","Bytes In","DNS Lookups","Connections","Requests","OK Responses","Redirects","Not Modified","Not Found","Other Responses","Error Code","Time to Start Render (ms)","Segments Transmitted","Segments Retransmitted","Packet Loss (out)","Activity Time(ms)","Descriptor","Lab ID","Dialer ID","Connection Type","Cached","Event URL","Pagetest Build","Measurement Type","Experimental","Doc Complete Time (ms)","Event GUID","Time to DOM Element (ms)","Includes Object Data","Cache Score","Static CDN Score","One CDN Score","GZIP Score","Cookie Score","Keep-Alive Score","DOCTYPE Score","Minify Score","Combine Score","Bytes Out (Doc)","Bytes In (Doc)","DNS Lookups (Doc)","Connections (Doc)","Requests (Doc)","OK Responses (Doc)","Redirects (Doc)","Not Modified (Doc)","Not Found (Doc)","Other Responses (Doc)","Compression Score","Host","IP Address","ETag Score","Flagged Requests","Flagged Connections","Max Simultaneous Flagged Connections","Time to Base Page Complete (ms)","Base Page Result","Gzip Total Bytes","Gzip Savings","Minify Total Bytes","Minify Savings","Image Total Bytes","Image Savings","Base Page Redirects","Optimization Checked","AFT (ms)","DOM Elements","Page Speed Version","Page Title","Time to Title","Load Event Start","Load Event End","DOM Content Ready Start","DOM Content Ready End","Visually Complete (ms)","Browser Name","Browser Version"';
+    $column_count = 84;
+    $is_requests = false;
     if( $_GET['requests'] )
     {
         $fileType = 'IEWTR.txt';
-        $header = '"Date","Time","Event Name","IP Address","Action","Host","URL","Response Code","Time to Load (ms)","Time to First Byte (ms)","Start Time (ms)","Bytes Out","Bytes In","Object Size","Cookie Size (out)","Cookie Count(out)","Expires","Cache Control","Content Type","Content Encoding","Transaction Type","Socket ID","Document ID","End Time (ms)","Descriptor","Lab ID","Dialer ID","Connection Type","Cached","Event URL","Pagetest Build","Measurement Type","Experimental","Event GUID","Sequence Number","Cache Score","Static CDN Score","GZIP Score","Cookie Score","Keep-Alive Score","DOCTYPE Score","Minify Score","Combine Score","Compression Score","ETag Score","Flagged","Secure","DNS Time","Connect Time","SSL Time","Gzip Total Bytes","Gzip Savings","Minify Total Bytes","Minify Savings","Image Total Bytes","Image Savings","Cache Time (sec)","Real Start Time (ms)","Full Time to Load (ms)","Optimization Checked","CDN Provider","DNS Start","DNS End","Connect Start","Connect End","SSL Negotiation Start","SSL Negotiation End"';
+        $header = '"Date","Time","Event Name","IP Address","Action","Host","URL","Response Code","Time to Load (ms)","Time to First Byte (ms)","Start Time (ms)","Bytes Out","Bytes In","Object Size","Cookie Size (out)","Cookie Count(out)","Expires","Cache Control","Content Type","Content Encoding","Transaction Type","Socket ID","Document ID","End Time (ms)","Descriptor","Lab ID","Dialer ID","Connection Type","Cached","Event URL","Pagetest Build","Measurement Type","Experimental","Event GUID","Sequence Number","Cache Score","Static CDN Score","GZIP Score","Cookie Score","Keep-Alive Score","DOCTYPE Score","Minify Score","Combine Score","Compression Score","ETag Score","Flagged","Secure","DNS Time","Connect Time","SSL Time","Gzip Total Bytes","Gzip Savings","Minify Total Bytes","Minify Savings","Image Total Bytes","Image Savings","Cache Time (sec)","Real Start Time (ms)","Full Time to Load (ms)","Optimization Checked","CDN Provider","DNS Start","DNS End","Connect Start","Connect End","SSL Negotiation Start","SSL Negotiation End","Initiator","Initiator Line","Initiator Column"';
+        $column_count = 70;
         $filename = "{$id}_details.csv";
+        $is_requests = true;
     }
     header("Content-disposition: attachment; filename=$filename");
     header ("Content-type: text/csv");
@@ -19,7 +24,11 @@ if( isset($test['test']) && (isset($test['test']['completeTime']) || $test['test
     
     if( $test['test']['batch'] )
     {
-        echo "\"Test\",$header\r\n";
+        echo "\"Test\",$header,\"Run\"";
+        if (!$is_requests) {
+            echo ',"Speed Index"';
+        }
+        echo "\r\n";
         $tests = null;
         if( gz_is_file("$testPath/tests.json") )
         {
@@ -42,8 +51,10 @@ if( isset($test['test']) && (isset($test['test']['completeTime']) || $test['test
                 $path = './' . GetTestPath($testData['id']);
                 for( $i = 1; $i <= $test['test']['runs']; $i++ )
                 {
-                    csvFile("$path/{$i}_$fileType", $label);
-                    csvFile("$path/{$i}_Cached_$fileType", $label);
+                    $additional = array($i, SpeedIndex($path, $i, 0));
+                    csvFile("$path/{$i}_$fileType", $label, $column_count, $additional);
+                    $additional = array($i, SpeedIndex($path, $i, 1));
+                    csvFile("$path/{$i}_Cached_$fileType", $label, $column_count, $additional);
                 }
                 
                 foreach( $testData['v'] as $variationIndex => $variationId )
@@ -51,8 +62,10 @@ if( isset($test['test']) && (isset($test['test']['completeTime']) || $test['test
                     $path = './' . GetTestPath($variationId);
                     for( $i = 1; $i <= $test['test']['runs']; $i++ )
                     {
-                        csvFile("$path/{$i}_$fileType", "$label - {$tests['variations'][$variationIndex]['l']}");
-                        csvFile("$path/{$i}_Cached_$fileType", "$label - {$tests['variations'][$variationIndex]['l']}");
+                        $additional = array($i, SpeedIndex($path, $i, 0));
+                        csvFile("$path/{$i}_$fileType", "$label - {$tests['variations'][$variationIndex]['l']}", $column_count, $additional);
+                        $additional = array($i, SpeedIndex($path, $i, 1));
+                        csvFile("$path/{$i}_Cached_$fileType", "$label - {$tests['variations'][$variationIndex]['l']}", $column_count, $additional);
                     }
                 }
             }
@@ -60,12 +73,18 @@ if( isset($test['test']) && (isset($test['test']['completeTime']) || $test['test
     }
     else
     {
-        echo "$header\r\n";
+        echo "$header,\"Run\"";
+        if (!$is_requests) {
+            echo ',"Speed Index"';
+        }
+        echo "\r\n";
         // loop through all  of the results files (one per run) - both cached and uncached
         for( $i = 1; $i <= $test['test']['runs']; $i++ )
         {
-            csvFile("$testPath/{$i}_$fileType");
-            csvFile("$testPath/{$i}_Cached_$fileType");
+            $additional = array($i, SpeedIndex($testPath, $i, 0));
+            csvFile("$testPath/{$i}_$fileType", null, $column_count, $additional);
+            $additional = array($i, SpeedIndex($testPath, $i, 1));
+            csvFile("$testPath/{$i}_Cached_$fileType", null, $column_count, $additional);
         }
     }
 }
@@ -74,13 +93,22 @@ else
     header("HTTP/1.0 404 Not Found");
 }
 
+function SpeedIndex($testPath, $run, $cached) {
+    $speed_index = '';
+    $progress = GetVisualProgress($testPath, $run, $cached);
+    if (isset($progress) && is_array($progress) && array_key_exists('FLI', $progress)) {
+        $speed_index = $progress['FLI'];
+    }
+    return $speed_index;
+}
+
 /**
 * Take a tab-separated file, convert it to csv and spit it out
 * 
 * @param mixed $fileName
 * @param mixed $includeHeader
 */
-function csvFile($fileName, $label = null)
+function csvFile($fileName, $label, $column_count, &$additional_columns)
 {
     $lines = gz_file($fileName);
     if( $lines)
@@ -93,15 +121,28 @@ function csvFile($fileName, $label = null)
                 $line = trim($line, "\r\n");
                 if( strlen($line) )
                 {
-                    $line = str_replace('"', '""', $line);
-                    $line = str_replace('"', '""', $line);
-                    $line = str_replace("\t", '","', $line);
-                    if( isset($label) )
-                    {
-                        $label = str_replace('"', '', $label);
-                        echo "\"$label\",";
+                    $parseLine = str_replace("\t", "\t ", $line);
+                    $fields = explode("\t", $parseLine);
+                    if (count($fields)) {
+                        if( isset($label) ) {
+                            $label = str_replace('"', '', $label);
+                            echo "\"$label\",";
+                        }
+                        for ($i = 0; $i < $column_count; $i++) {
+                            $value = '';
+                            if (array_key_exists($i, $fields)) {
+                                $value = str_replace('"', '""', trim($fields[$i]));
+                            }
+                            echo "\"$value\",";
+                        }
+                        if (isset($additional_columns) && count($additional_columns)) {
+                            foreach ($additional_columns as $value) {
+                                $value = str_replace('"', '""', trim($value));
+                                echo "\"$value\",";
+                            }
+                        }
+                        echo "\r\n";
                     }
-                    echo '"' . $line . '"' . "\r\n";
                 }
             }
         }
