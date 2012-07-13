0ccabed4a5536f283a856ec91f18c25e9f9fe9e9
pmeenan <pmeenan@webpagetest.org>
2012-07-13 16:59:38 +0000

    Fixed an arbitrary file upload vulnerability reported here - http://packetstormsecurity.org/files/114717

--- www/webpagetest/work/resultimage.php
+++ www/webpagetest/work/resultimage.php
@@ -21,27 +21,50 @@ if( (!strlen($locKey) || !strcmp($key, $locKey)) || !strcmp($_SERVER['REMOTE_ADD
     if( isset($_FILES['file']) )
     {
         $fileName = $_FILES['file']['name'];
-        $path = './' . GetTestPath($id);
-        
-        // put each run of video data in it's own directory
-        if( strpos($fileName, 'progress') )
-        {
-            $parts = explode('_', $fileName);
-            if( count($parts) )
-            {
-                $runNum = $parts[0];
-                $fileBase = $parts[count($parts) - 1];
-                $cached = '';
-                if( strpos($fileName, '_Cached') )
-                    $cached = '_cached';
-                $path .= "/video_$runNum$cached";
-                if( !is_dir($path) )
-                    mkdir($path);
-                $fileName = 'frame_' . $fileBase;
+        if (strpos($fileName, '..') === false &&
+            strpos($fileName, '/') === false &&
+            strpos($fileName, '\\') === false) {
+            $path = './' . GetTestPath($id);
+            
+            // make sure the file is an acceptable type
+            $parts = pathinfo($fileName);
+            $ext = strtolower($parts['extension']);
+            $ok = false;
+            if (strpos($ext, 'php') === false &&
+                strpos($ext, 'pl') === false &&
+                strpos($ext, 'py') === false &&
+                strpos($ext, 'cgi') === false &&
+                strpos($ext, 'asp') === false &&
+                strpos($ext, 'js') === false &&
+                strpos($ext, 'rb') === false &&
+                strpos($ext, 'htaccess') === false &&
+                strpos($ext, 'jar') === false) {
+                $ok = true;
+            }
+            
+            if ($ok) {
+                // put each run of video data in it's own directory
+                if( strpos($fileName, 'progress') )
+                {
+                    $parts = explode('_', $fileName);
+                    if( count($parts) )
+                    {
+                        $runNum = $parts[0];
+                        $fileBase = $parts[count($parts) - 1];
+                        $cached = '';
+                        if( strpos($fileName, '_Cached') )
+                            $cached = '_cached';
+                        $path .= "/video_$runNum$cached";
+                        if( !is_dir($path) )
+                            mkdir($path);
+                        $fileName = 'frame_' . $fileBase;
+                    }
+                }
+                logMsg(" Moving uploaded image '{$_FILES['file']['tmp_name']}' to '$path/$fileName'\n");
+                move_uploaded_file($_FILES['file']['tmp_name'], "$path/$fileName");
+                @chmod("$path/$fileName", 0666);
             }
         }
-        logMsg(" Moving uploaded image '{$_FILES['file']['tmp_name']}' to '$path/$fileName'\n");
-        move_uploaded_file($_FILES['file']['tmp_name'], "$path/$fileName");
     }
     else
         logMsg(" no uploaded file attached");
