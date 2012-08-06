930b53e4788d0950119ec60ed134503936c26c6c
pmeenan <pmeenan@webpagetest.org>
2012-08-06 15:34:42 +0000

    Tweaked the logic that selects the default filmstrip interval to default to 100ms for benchmark comparison tests

--- www/webpagetest/video/filmstrip.inc.php
+++ www/webpagetest/video/filmstrip.inc.php
@@ -4,6 +4,7 @@
 require_once('visualProgress.inc.php');
 
 // build up the actual test data (needs to include testID and RUN in the requests)
+$defaultInterval = 0;
 $tests = array();
 $fastest = null;
 $ready = true;
@@ -13,22 +14,18 @@ if( strlen($_REQUEST['end']) )
     $endTime = trim($_REQUEST['end']);
 
 $compTests = explode(',', $_REQUEST['tests']);
-foreach($compTests as $t)
-{
+foreach($compTests as $t) {
     $parts = explode('-', $t);
-    if( count($parts) >= 1 )
-    {
+    if (count($parts) >= 1) {
         $test = array();
         $test['id'] = $parts[0];
         if (ValidateTestId($test['id'])) {
             $test['cached'] = 0;
             $test['end'] = $endTime;
             
-            for( $i = 1; $i < count($parts); $i++ )
-            {
+            for ($i = 1; $i < count($parts); $i++) {
                 $p = explode(':', $parts[$i]);
-                if( count($p) >= 2 )
-                {
+                if (count($p) >= 2) {
                     if( $p[0] == 'r' )
                         $test['run'] = (int)$p[1];
                     if( $p[0] == 'l' )
@@ -44,13 +41,21 @@ foreach($compTests as $t)
             $test['path'] = GetTestPath($test['id']);
             $test['pageData'] = loadAllPageData($test['path']);
             
+            $info = json_decode(gz_file_get_contents("./{$test['path']}/testinfo.json"), true);
+            if (isset($info) && is_array($info)) {
+                if (array_key_exists('view', $info) && 
+                    $info['view'] == 'pss' && 
+                    array_key_exists('priority', $info) &&
+                    $info['priority'] >= 1) {
+                    $defaultInterval = 100;
+                }
+            }
+            
             $testInfo = parse_ini_file("./{$test['path']}/testinfo.ini",true);
-            if( $testInfo !== FALSE )
-            {
+            if ($testInfo !== FALSE) {
                 if (array_key_exists('test', $testInfo) && array_key_exists('location', $testInfo['test']))
                     $test['location'] = $testInfo['test']['location'];
-                if( isset($testInfo['test']) && isset($testInfo['test']['completeTime']) )
-                {
+                if (isset($testInfo['test']) && isset($testInfo['test']['completeTime'])) {
                     $test['done'] = true;
 
                     if( !$test['run'] )
@@ -62,8 +67,7 @@ foreach($compTests as $t)
                         $fastest = $loadTime;
 
                     // figure out the real end time (in ms)
-                    if( isset($test['end']) )
-                    {
+                    if (isset($test['end'])) {
                         if( !strcmp($test['end'], 'doc') )
                             $test['end'] = $test['pageData'][$test['run']][$test['cached']]['docTime'];
                         elseif(!strncasecmp($test['end'], 'doc+', 4))
@@ -72,22 +76,17 @@ foreach($compTests as $t)
                             $test['end'] = 0;
                         elseif( !strcmp($test['end'], 'all') )
                             $test['end'] = -1;
-                        elseif( !strcmp($test['end'], 'aft') )
-                        {
+                        elseif( !strcmp($test['end'], 'aft') ) {
                             $test['end'] = $test['aft'];
                             if( !$test['end'] )
                                 $test['end'] = -1;
-                        }
-                        else
+                        } else
                             $test['end'] = (int)((double)$test['end'] * 1000.0);
-                    }
-                    else
+                    } else
                         $test['end'] = 0;
                     if( !$test['end'] )
                         $test['end'] = $test['pageData'][$test['run']][$test['cached']]['fullyLoaded'];
-                }
-                else
-                {
+                } else {
                     $test['done'] = false;
                     $ready = false;
                     
@@ -104,8 +103,7 @@ foreach($compTests as $t)
 }
 
 $count = count($tests);
-if( $count )
-{
+if( $count ) {
     setcookie('fs', urlencode($_REQUEST['tests']));
     LoadTestData();
 }
@@ -113,8 +111,7 @@ else
     $error = "No valid tests selected.";
 
 $thumbSize = $_REQUEST['thumbSize'];
-if( !isset($thumbSize) || $thumbSize < 50 || $thumbSize > 500 )
-{
+if( !isset($thumbSize) || $thumbSize < 50 || $thumbSize > 500 ) {
     if( $count > 6 )
         $thumbSize = 100;
     elseif( $count > 4 )
@@ -124,13 +121,12 @@ if( !isset($thumbSize) || $thumbSize < 50 || $thumbSize > 500 )
 }
 
 $interval = (int)$_REQUEST['ival'];
-if( !$interval )
-{
-    if( isset($fastest) )
+if( !$interval ) {
+    if ($defaultInterval) {
+        $interval = $defaultInterval;
+    } else if( isset($fastest) )
     {
-        if( $fastest > 10000 )
-            $interval = 1000;
-        elseif( $fastest > 2000 )
+        if( $fastest > 3000 )
             $interval = 500;
         else
             $interval = 100;
@@ -144,27 +140,23 @@ $interval /= 100;
 * Load information about each of the tests (particularly about the video frames)
 * 
 */
-function LoadTestData()
-{
+function LoadTestData() {
     global $tests;
     global $admin;
     global $supportsAuth;
 
-    foreach( $tests as &$test )
-    {
+    foreach( $tests as &$test ) {
         $testPath = GetTestPath($test['id']);
         $pageData = loadAllPageData($testPath);
         $test['url'] = trim($pageData[1][0]['URL']);
         
         if( strlen($test['label']) )
             $test['name'] = $test['label'];
-        else
-        {
+        else {
             $testInfo = json_decode(gz_file_get_contents("./$testPath/testinfo.json"), true);
             $test['name'] = trim($testInfo['label']);
         }
-        if( !strlen($test['name']) )
-        {
+        if( !strlen($test['name']) ) {
             $test['name'] = $test['url'];
             $test['name'] = str_replace('http://', '', $test['name']);
             $test['name'] = str_replace('https://', '', $test['name']);
@@ -175,8 +167,7 @@ function LoadTestData()
             $videoPath .= '_cached';
             
         $test['video'] = array();
-        if( is_dir($videoPath) )
-        {
+        if( is_dir($videoPath) ) {
             $test['video']['start'] = 20000;
             $test['video']['end'] = 0;
             $test['video']['frames'] = array();
@@ -184,29 +175,23 @@ function LoadTestData()
             
             // get the path to each of the video files
             $dir = opendir($videoPath);
-            if( $dir )
-            {
-                while($file = readdir($dir)) 
-                {
+            if( $dir ) {
+                while($file = readdir($dir)) {
                     $path = $videoPath  . "/$file";
-                    if( is_file($path) && !strncmp($file, 'frame_', 6) && strpos($file, '.thm') === false && strpos($file, '.hist') === false )
-                    {
+                    if( is_file($path) && !strncmp($file, 'frame_', 6) && strpos($file, '.thm') === false && strpos($file, '.hist') === false ) {
                         $parts = explode('_', $file);
-                        if( count($parts) >= 2 )
-                        {
+                        if( count($parts) >= 2 ) {
                             $index = (int)$parts[1];
                             $ms = $index * 100;
                             
-                            if( !$test['end'] || $test['end'] == -1 || $ms <= $test['end'] )
-                            {
+                            if( !$test['end'] || $test['end'] == -1 || $ms <= $test['end'] ) {
                                 if( $index < $test['video']['start'] )
                                     $test['video']['start'] = $index;
                                 if( $index > $test['video']['end'] )
                                     $test['video']['end'] = $index;
                                 
                                 // figure out the dimensions of the source image
-                                if( !$test['video']['width'] || !$test['video']['height'] )
-                                {
+                                if( !$test['video']['width'] || !$test['video']['height'] ) {
                                     $size = getimagesize($path);
                                     $test['video']['width'] = $size[0];
                                     $test['video']['height'] = $size[1];
