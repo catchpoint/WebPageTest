d5b23d5b1c7a53efe5eebd085e84e851107dfc9d
pmeenan <pmeenan@webpagetest.org>
2012-10-23 16:30:43 +0000

    Fixed issue #122 - http://code.google.com/p/webpagetest/issues/detail?id=122 Added video playback support for platforms that don't support flash.

--- www/webpagetest/video/view.php
+++ www/webpagetest/video/view.php
@@ -196,7 +196,8 @@ else
                 echo "body {background-color: $bgcolor; margin:0; padding: 0;}";
             ?>
         </style>
-        <script type="text/javascript" src="/video/player/flowplayer-3.2.6.min.js"></script>
+        <link rel="stylesheet" href="/video/video-js.3.2.0/video-js.min.css" type="text/css">
+        <script type="text/javascript" src="/video/video-js.3.2.0/video.min.js"></script>
     </head>
     <body>
         <div class="page">
@@ -224,60 +225,42 @@ else
                     $hasThumb = true;
                     list($width, $height) = getimagesize("./$dir/video.png");
                 }
-                
+
                 if( $_REQUEST['width'] )
                     $width = (int)$_REQUEST['width'];
                 if( $_REQUEST['height'] )
                     $height = (int)$_REQUEST['height'];
-                    
-                echo '<div';
-                echo " style=\"display:block; width:{$width}px; height:{$height}px\"";
-                echo " id=\"player\">\n";
-                echo "</div>\n";
 
-                // embed the actual player
-                ?>
-                <script>
-                    flowplayer("player", 
-                                    {
-                                        src: "/video/player/flowplayer-3.2.7.swf",
-                                        cachebusting: false,
-                                        version: [9, 115]
-                                    } , 
-                                    { 
-                                        clip:  { 
-                                            scaling: "fit"
-                                        } ,
-                                        playlist: [
-                                            <?php
-                                            if( $hasThumb )
-                                            {
-                                                echo "{ url: '/$dir/video.png'} ,\n";
-                                                echo "{ url: '/$dir/video.mp4', autoPlay: $autoplay, autoBuffering: false}\n";
-                                            }
-                                            else
-                                                echo "{ url: '/$dir/video.mp4', autoPlay: $autoplay, autoBuffering: true}\n";
-                                            ?>
-                                        ],
-                                        plugins: {
-                                            controls: {
-                                                volume:false,
-                                                mute:false,
-                                                stop:true,
-                                                tooltips: { 
-                                                    buttons: true, 
-                                                    fullscreen: 'Enter fullscreen mode' 
-                                                } 
-                                            }
-                                        } ,
-                                        canvas:  { 
-                                            backgroundColor: '#000000', 
-                                            backgroundGradient: 'none'
-                                        }
-                                    }
-                                ); 
-                </script>
-                <?php                
+                echo "<script>\n";
+                echo "_V_.options.techOrder = ['flash', 'html5'];\n";
+                echo "_V_.options.flash.swf = '/video/player/flowplayer-3.2.7.swf';\n";
+                echo "_V_.options.flash.flashVars = {config:\"{";
+                echo "'clip':{'scaling':'fit'},";
+                echo "'plugins':{'controls':{'volume':false,'mute':false,'stop':true,'tooltips':{'buttons':true,'fullscreen':'Enter fullscreen mode'}}},";
+                echo "'canvas':{'backgroundColor':'#000000','backgroundGradient':'none'},";
+                if ($hasThumb) {
+                    echo "'playlist':[{'url':'/$dir/video.png'},{'url':'/$dir/video.mp4','autoPlay':$autoplay,'autoBuffering':false}]";
+                } else {
+                    echo "'playlist':[{'url':'/$dir/video.mp4','autoPlay':$autoplay,'autoBuffering':true}]";
+                }
+                echo "}\"};\n";
+                echo "_V_.options.flash.params = {
+                       allowfullscreen: 'true',
+                       wmode: 'transparent',
+                       allowscriptaccess: 'always'
+                   };
+                   _V_.options.flash.attributes={};\n";
+                echo "</script>\n";
+                    
+                echo "<video id=\"player\" class=\"video-js vjs-default-skin\" controls
+                  preload=\"auto\" width=\"$width\" height=\"$height\"";
+                if ($hasThumb) {
+                    echo " poster=\"/$dir/video.png\"";
+                }
+                echo "data-setup=\"{}\">
+                    <source src=\"/$dir/video.mp4\" type='video/mp4'>
+                </video>";
+
                 if(!$embed)
                     echo "<br><a class=\"link\" href=\"/video/download.php?id=$id\">Click here to download the video file...</a>\n";
             }
