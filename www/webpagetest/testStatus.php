354fda5074ca63ec9503bc6a3ccfc28a7c4a2316
pmeenan <pmeenan@webpagetest.org>
2013-01-22 21:00:32 +0000

    Switched the bulk testing comparison UI to use ajax progress updates.

--- www/webpagetest/testStatus.php
+++ www/webpagetest/testStatus.php
@@ -14,9 +14,53 @@ if( $_REQUEST['pos'] )
 // see if we are dealing with multiple tests or a single test
 if( isset($_REQUEST['tests']) && strlen($_REQUEST['tests']) )
 {
-}
-else
-{
+    $tests = explode(',', $_REQUEST['tests']);
+    $ret['data'] = array('testsExpected' => 0, 'testsCompleted' => 0, 'tests' => array());
+    foreach ($tests as $id) {
+        if (ValidateTestId($id)) {
+            $status = GetTestStatus($id, $position);
+            $status['id'] = $id;
+            $ret['tests'][] = $status;
+            if ($status['statusCode'] < $ret['statusCode']) {
+                $ret['statusCode'] = $status['statusCode'];
+                $ret['statusText'] = $status['statusText'];
+                if (array_key_exists('elapsed', $status))
+                    $ret['data']['elapsed'] = $status['elapsed'];
+                elseif (array_key_exists('elapsed', $ret['data']))
+                    unset ($ret['data']['elapsed']);
+                if (array_key_exists('behindCount', $status))
+                    $ret['data']['behindCount'] = $status['behindCount'];
+                elseif (array_key_exists('behindCount', $ret['data']))
+                    unset ($ret['data']['behindCount']);
+            } elseif ($status['statusCode'] < $ret['statusCode']) {
+                if ($status['statusCode'] == 100 &&
+                    array_key_exists('elapsed', $status)) {
+                    if (!array_key_exists('elapsed', $ret['data']) || 
+                        $status['elapsed'] > $ret['data']['elapsed']) {
+                        $ret['statusText'] = $status['statusText'];
+                        $ret['data']['elapsed'] = $status['elapsed'];
+                    }
+                } elseif ($status['statusCode'] == 101 &&
+                          array_key_exists('behindCount', $status)) {
+                    if (!array_key_exists('behindCount', $ret['data']) || 
+                        $status['behindCount'] < $ret['data']['behindCount']) {
+                        $ret['statusText'] = $status['statusText'];
+                        $ret['data']['behindCount'] = $status['behindCount'];
+                    }
+                }
+            }
+            if (array_key_exists('testsExpected', $status))
+                $ret['data']['testsExpected'] += $status['testsExpected'];
+            if (array_key_exists('testsCompleted', $status))
+                $ret['data']['testsCompleted'] += $status['testsCompleted'];
+        }
+    }
+    if ($ret['statusCode'] >= 100 && $ret['statusCode'] < 200 &&
+        $ret['data']['testsCompleted'] > 0 && $ret['data']['testsExpected'] > 1) {
+        $ret['statusCode'] = 100;
+        $ret['statusText'] = "Completed {$ret['data']['testsCompleted']} of {$ret['data']['testsExpected']} tests";
+    }
+} else {
     $ret['data'] = GetTestStatus($id, $position);
     $ret['statusCode'] = $ret['data']['statusCode'];
     $ret['statusText'] = $ret['data']['statusText'];
