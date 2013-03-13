2baf9271724c96c40de13824f6541d9ee2f43879
pmeenan <pmeenan@webpagetest.org>
2013-03-13 14:47:19 +0000

    Added a default error code for failed resources when dev tools didn't have an error code included.  Also forced failed requests to always be flagged as coming from the net.

--- www/webpagetest/devtools.inc.php
+++ www/webpagetest/devtools.inc.php
@@ -512,6 +512,8 @@ function DevToolsFilterNetRequests($events, &$requests, &$pageData) {
                         $rawRequests[$id]['endTime'] = $event['timestamp'];
                 }
                 if ($event['method'] == 'Network.loadingFailed') {
+                    $rawRequests[$id]['fromNet'] = true;
+                    $rawRequests[$id]['errorCode'] = 12999;
                     if (!array_key_exists('endTime', $rawRequests[$id]) || 
                         $event['timestamp'] > $rawRequests[$id]['endTime'])
                         $rawRequests[$id]['endTime'] = $event['timestamp'];
