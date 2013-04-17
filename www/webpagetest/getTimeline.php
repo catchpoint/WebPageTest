9168b6cfc7b386ae1463d02059a67632749ee291
pmeenan <pmeenan@webpagetest.org>
2013-04-17 12:23:33 +0000

    Improvements to the memory use on extracting timelines from dev tools traces

--- www/webpagetest/getTimeline.php
+++ www/webpagetest/getTimeline.php
@@ -1,7 +1,8 @@
 <?php
 include 'common.inc';
-header ("Content-type: application/json");
+$ok = false;
 if (gz_is_file("$testPath/$run{$cachedText}_timeline.json")) {
+  $ok = true;
   header("Content-disposition: attachment; filename=timeline.json");
   header ("Content-type: application/json");
   gz_readfile_chunked("$testPath/$run{$cachedText}_timeline.json");
@@ -9,27 +10,30 @@ if (gz_is_file("$testPath/$run{$cachedText}_timeline.json")) {
   $devTools = json_decode(gz_file_get_contents("$testPath/$run{$cachedText}_devtools.json"), true);
   if (isset($devTools) && is_array($devTools) && count($devTools)) {
     $timeline = array();
-    foreach ($devTools as $entry) {
+    foreach ($devTools as &$entry) {
       if (isset($entry) &&
           is_array($entry) &&
           array_key_exists('method', $entry) &&
           $entry['method'] == 'Timeline.eventRecorded' &&
           array_key_exists('params', $entry) &&
           is_array($entry['params']) &&
-          array_key_exists('record', $entry['params']))
-      $timeline[] = $entry['params']['record'];
+          array_key_exists('record', $entry['params']) &&
+          is_array($entry['params']['record']) &&
+          count($entry['params']['record'])) {
+        if (!$ok) {
+          $ok = true;
+          header("Content-disposition: attachment; filename=timeline.json");
+          header ("Content-type: application/json");
+          echo '[';
+        } else
+          echo ',';
+        echo json_encode($entry['params']['record']);
+      }
     }
-    if (count($timeline)) {
-      header("Content-disposition: attachment; filename=timeline.json");
-      header ("Content-type: application/json");
-      echo json_encode($timeline);
-    } else {
-      header("HTTP/1.0 404 Not Found");
-    }
-  } else {
-    header("HTTP/1.0 404 Not Found");
+    if ($ok)
+      echo ']';
   }
-} else {
-  header("HTTP/1.0 404 Not Found");
 }
+if (!$ok)
+  header("HTTP/1.0 404 Not Found");
 ?>
