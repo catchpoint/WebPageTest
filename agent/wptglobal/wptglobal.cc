f469108f6d76f391286954c2131f38f66e32ec03
pmeenan <pmeenan@webpagetest.org>
2013-05-29 13:26:48 +0000

    Switched the hooking to use an appinit dll so that we can hook before DirectX is initialized and added the starting point for DirectX hooking support.

--- agent/wptglobal/wptglobal.cc
+++ agent/wptglobal/wptglobal.cc
@@ -5,38 +5,60 @@
 #include "wptglobal_dll.h"
 #include "wptglobal.h"
 #include "hook_angle.h"
+#include "hook_dx9.h"
 
 HINSTANCE global_dll_handle = NULL; // DLL handle
-HHOOK     global_hook = NULL;
 AngleHook * g_angle_hook = NULL;
+Dx9Hook * g_dx9_hook = NULL;
 
 /*-----------------------------------------------------------------------------
-  Dummy hook proc (pass-through) since we don't care about actually hooking
-  messages.
------------------------------------------------------------------------------*/
-LRESULT CALLBACK HookProc(int code, WPARAM wParam, LPARAM lParam) {
-  return CallNextHookEx(global_hook, code, wParam, lParam);
-}
-
-/*-----------------------------------------------------------------------------
-  Install the global message hook
+  Install the global hook
 -----------------------------------------------------------------------------*/
 BOOL WINAPI InstallGlobalHook() {
   BOOL ret = FALSE;
-  if (!global_hook)
-    global_hook = SetWindowsHookEx(WH_CBT, HookProc,
-                                   global_dll_handle, 0);
-  return (global_hook != NULL);
+  TCHAR path[MAX_PATH];
+  if (GetModuleFileName(global_dll_handle, path, _countof(path))) {
+    TCHAR short_path[MAX_PATH];
+    if (GetShortPathName(path, short_path, _countof(short_path))) {
+      HKEY hKey;
+		  if (RegCreateKeyEx(HKEY_LOCAL_MACHINE,
+                         _T("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion")
+                         _T("\\Windows"),
+                         0, 0, 0, KEY_WRITE, 0, &hKey, 0) == ERROR_SUCCESS ) {
+			  DWORD val = 1;
+			  RegSetValueEx(hKey, _T("LoadAppInit_DLLs"), 0, REG_DWORD,
+                      (const LPBYTE)&val, sizeof(val));
+			  val = 0;
+			  RegSetValueEx(hKey, _T("RequireSignedAppInit_DLLs"), 0, REG_DWORD,
+                      (const LPBYTE)&val, sizeof(val));
+			  RegSetValueEx(hKey, _T("AppInit_DLLs"), 0, REG_SZ,
+                      (const LPBYTE)short_path,
+                      (lstrlen(short_path) + 1) * sizeof(TCHAR));
+        RegCloseKey(hKey);
+        ret = TRUE;
+      }
+    }
+  }
+  return ret;
 }
 
 /*-----------------------------------------------------------------------------
-  Remove the global message hook
+  Remove the global hook
 -----------------------------------------------------------------------------*/
 BOOL WINAPI RemoveGlobalHook() {
   BOOL ret = FALSE;
-  if (global_hook)
-    ret = UnhookWindowsHookEx(global_hook);
-  global_hook = NULL;
+  HKEY hKey;
+	if (RegCreateKeyEx(HKEY_LOCAL_MACHINE,
+                      _T("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion")
+                      _T("\\Windows"),
+                      0, 0, 0, KEY_WRITE, 0, &hKey, 0) == ERROR_SUCCESS ) {
+    const TCHAR *short_path = _T("");
+		RegSetValueEx(hKey, _T("AppInit_DLLs"), 0, REG_SZ,
+                  (const LPBYTE)short_path,
+                  (lstrlen(short_path) + 1) * sizeof(TCHAR));
+    RegCloseKey(hKey);
+    ret = TRUE;
+  }
   return ret;
 }
 
@@ -56,6 +78,10 @@ void Initialize() {
         g_angle_hook = new AngleHook;
         g_angle_hook->Init();
       }
+      if (!g_dx9_hook) {
+        g_dx9_hook = new Dx9Hook;
+        g_dx9_hook->Init();
+      }
     }
   }
 }
@@ -68,5 +94,10 @@ void Unload() {
     g_angle_hook->Unload();
     delete g_angle_hook;
     g_angle_hook = NULL;
+  }
+  if (g_dx9_hook) {
+    g_dx9_hook->Unload();
+    delete g_dx9_hook;
+    g_dx9_hook = NULL;
   }
 }
