d854ff8952bbc3c2503113251965677c52567751
pmeenan <pmeenan@webpagetest.org>
2014-02-01 13:21:27 -0500

    Updated windows batch file for node agent so it works with the current agent code (should be the same as the shell script except for iOS and osx support)

--- agent/js/wptdriver.bat
+++ agent/js/wptdriver.bat
@@ -7,49 +7,92 @@ rem TODO update to match wptdriver.sh
 set DP0=%~dp0
 set WPT_SERVER=http://localhost:8888
 set LOCATION=Test
-set WPT_VERBOSE=false
-set WPT_MAX_LOGLEVEL=5
-set WPT_DEBUG=false
+set BROWSER=chrome
+set "WPT_MAX_LOGLEVEL=error"
+set "WPT_VERBOSE=true"
+set "OPT_ARGS="
+set "BATCH_NAME=%0"
+shift
 
 :loop
-if NOT "%1"=="" (
-    if "%1"=="-s" (
-        set WPT_SERVER=%2
+if NOT "%0"=="" (
+    set "MATCHED="
+    set "TRUE="
+    if "%0"=="-s" set TRUE=1
+    if "%0"=="--serverUrl" set TRUE=1
+    if defined TRUE (
+        set MATCHED=1
+        set WPT_SERVER=%1
         shift
         shift
-        goto :loop
     )
-    if "%1"=="-l" (
-        set LOCATION=%2
+    set "TRUE="
+    if "%0"=="-l" set TRUE=1
+    if "%0"=="--location" set TRUE=1
+    if defined TRUE (
+        set MATCHED=1
+        set LOCATION=%1
         shift
         shift
-        goto :loop
     )
-    if "%1"=="-v" (
-        set WPT_VERBOSE=true
+    set "TRUE="
+    if "%0"=="-b" set TRUE=1
+    if "%0"=="-c" set TRUE=1
+    if "%0"=="--browser" set TRUE=1
+    if defined TRUE (
+        set MATCHED=1
+        set BROWSER=%1
         shift
-        goto :loop
+        shift
     )
-    if "%1"=="-m" (
-        set WPT_MAX_LOGLEVEL=%2
+    set "TRUE="
+    if "%0"=="-q" set TRUE=1
+    if "%0"=="--quiet" set TRUE=1
+    if defined TRUE (
+        set MATCHED=1
+        set WPT_VERBOSE=false
+        shift
+    )
+    set "TRUE="
+    if "%0"=="-m" set TRUE=1
+    if "%0"=="--max_log" set TRUE=1
+    if defined TRUE (
+        set MATCHED=1
+        set "WPT_MAX_LOGLEVEL=%1"
         shift
         shift
-        goto :loop
     )
-    if "%1"=="-d" (
-        set WPT_DEBUG=true
-        goto :loop
+    set "TRUE="
+    if "%0"=="-h" set TRUE=1
+    if "%0"=="--help" set TRUE=1
+    if defined TRUE (
+      set MATCHED=1
+      echo Usage: %BATCH_NAME% [options]...
+      echo.
+      echo         -s, --serverUrl URL  WebPagetest server URL.
+      echo                              Defaults to 'http://localhost:8888'.
+      echo.
+      echo         -l, --location NAME  Location name for this WebPagetest device.
+      echo                              Defaults to 'Test'.
+      echo.
+      echo         -b, --browser VALUE  Browser type, which must be one of:
+      echo                              chrome       # Local Chrome browser
+      echo                              android:DID  # Android device id
+      echo                              Defaults to 'chrome'.
+      echo         -q, --quiet          Disable verbose logging to stdout.
+      echo.
+      echo         -m, --max_log LEVEL  Sets the maximum loglevel that will be saved, where
+      echo                              value can either be a number ^(0-8^) or the name of
+      echo                              a loglevel such as critical, warning, or debug.
+      echo                              Defaults to 'info'.
+      cd %DP0%
+      goto :eof
     )
-    echo Usage: %0 [-s server] [-l location] [-v] [-d] [-m]
-    echo         -s    server       WebPagetest server
-    echo         -l    location     location name of the WebPagetest server
-    echo         -v    verbose      mirrors all logs to stdout
-    echo         -d    debug        sets all debug and custom loglevels to -1 so that
-    echo                            they are guaranteed to display
-    echo         -m    max loglevel sets the maximum loglevel that will be saved
-    echo                            the value can either be a number or the name
-    echo                            of a loglevel such as critical, warning, or debug
-    goto :EOF
+    if NOT defined MATCHED (
+      set "OPT_ARGS=%OPT_ARGS% %0"
+      shift
+    )
+    goto :loop
 )
 
 set WPT_ROOT=%~dp0
@@ -77,10 +120,32 @@ set AGENT=%WPT_ROOT%\agent\js
 rem Find the latest version of WD server jar, WDJS, platform-specific chromedriver
 for %%J in (%WPT_ROOT%\lib\webdriver\java\selenium-standalone-*.jar) do set SELENIUM_JAR=%%J
 
-for /D %%D in (%WPT_ROOT%\lib\webdriver\javascript\node-*) do set WDJS_DIR=%%D
-
 for %%E in (%WPT_ROOT%\lib\webdriver\chromedriver\Win32\chromedriver-*.exe) do set CHROMEDRIVER=%%E
+if defined CHROMEDRIVER set "CHROMEDRIVER_ARGS= --chromedriver ^"%CHROMEDRIVER%^""
 
-set NODE_PATH="%AGENT%;%AGENT%\src;%WDJS_DIR%
+set "NODE_PATH=%AGENT%;%AGENT%\src
 
-node src\agent_main --serverUrl %WPT_SERVER% --location %LOCATION% --chromedriver %CHROMEDRIVER% --seleniumJar %SELENIUM_JAR%
+rem First, split the browser string into it's browser:device components
+set "DEVICE_SERIAL=%BROWSER:*:=%"
+set "BROWSER=%BROWSER::="^&REM #%
+
+rem Configure the browser options
+set "KNOWN_BROWSER="
+if "%BROWSER%"=="chrome" (
+  set KNOWN_BROWSER=1
+  set "BROWSER_ARGS= --browser browser_local_chrome.BrowserLocalChrome%CHROMEDRIVER_ARGS%"
+)
+if "%BROWSER%"=="android" (
+  set KNOWN_BROWSER=1
+  set "BROWSER_ARGS= --browser browser_android_chrome.BrowserAndroidChrome --deviceSerial %DEVICE_SERIAL% --captureDir ^"%WPT_ROOT%\lib\capture^"%CHROMEDRIVER_ARGS%"
+)
+if NOT defined KNOWN_BROWSER (
+  echo Unknown browser %BROWSER%
+  cd %DP0%
+  goto :eof
+)
+
+cd %AGENT%
+set "CMD=node src\agent_main --serverUrl %WPT_SERVER% --location %LOCATION%%BROWSER_ARGS%%OPT_ARGS%"
+echo %CMD%
+%CMD%
