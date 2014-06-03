e49d4aae5f06393e459abde878052c66cf21a251
Matthew Lee <mgl@google.com>
2014-06-02 19:29:45 -0700

    Support multiple tests and options for which views and median lines to show in graph_page_data.php

--- www/graph_page_data.php
+++ www/graph_page_data.php
@@ -1,11 +1,65 @@
 <?php 
+$tests = (isset($_REQUEST['tests'])) ? $_REQUEST['tests'] : $_REQUEST['test'];
+$testsId = explode(',', $tests);
+if (count($testsId) == 1) {
+  $_GET['test'] = $testsId[0];
+  $_REQUEST['test'] = $testsId[0];
+}
 include 'common.inc';
 require_once('page_data.inc');
 require_once('graph_page_data.inc');
-$page_keywords = array('Graph Page Data','Webpagetest','Website Speed Test','Page Speed');
-$page_description = "Graph Page Data.";
+$page_keywords = array('Graph Page Data','Webpagetest','Website Speed Test','Page Speed', 'comparison');
+$page_description = "Graph Page Data Comparison.";
 $chartData = array();
-$pageData = loadAllPageData($testPath);
+
+# We intend to change to "?tests" but also allow "?test" so as to not break existing links.
+# TODO(mgl): Support -l:<label> after the test IDs as in video/compare.php
+$testsPath = array_map("GetTestPath", $testsId);
+$testsInfo = array_map("GetTestInfo", $testsPath);
+$pagesData = array_map("loadAllPageData", $testsPath);
+
+# Whether to show first and/or repeat views.
+# Default to showing first view if no views are indicated in the URL.
+$views = array();
+$rv = (isset($_REQUEST['rv'])) ? $_REQUEST['rv'] : 0;
+$fv = (isset($_REQUEST['fv'])) ? $_REQUEST['fv'] : (1 - $rv);
+if ($fv) {
+  $views[] = 0;
+}
+if ($rv) {
+  $views[] = 1;
+}
+
+# Whether to show median run and/or median value
+$median_run = (isset($_REQUEST['median_run'])) ? $_REQUEST['median_run'] : 0;
+$median_value = (isset($_REQUEST['median_value'])) ? $_REQUEST['median_value']  : 0;
+
+
+# Remove speed index if none of the runs have video.
+$removeSpeedIndex = true;
+foreach ( $testsInfo as $testInfo ) {
+  if ($testInfo && $testInfo['video']) {
+    $removeSpeedIndex = false;
+    break;
+  }
+}
+
+# Color palette taken from benchmarks/view.php
+# TODO(mgl): Combine this with the colors in benchmarks/view.php
+# TODO(mgl): Support more than 8 tests
+$colors = array('#ed2d2e', '#008c47', '#1859a9', '#662c91', '#f37d22', '#a11d20', '#b33893', '#010101');
+$light_colors = array_map("lighten", $colors);
+
+# Figure out what characteristics will be common to all lines in each graph.
+$common_labels = array();
+if (count($testsId) == 1) {
+  $common_labels[] = $testsId[0];
+}
+if (count($views) == 1) {
+  $common_labels[] = (($views[0] == '1') ? 'Repeat View' : 'First View');
+}
+$common_label = implode(" ", $common_labels);
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -24,12 +78,38 @@ $pageData = loadAllPageData($testPath);
     <body>
         <div class="page">
             <?php
-            $tab = 'Test Result';
+            if (count($testsId) == 1) {
+              $tab = 'Test Result';
+            }
             include 'header.inc';
             ?>
-            
+            <div style="float: right;">
+                <form name="cached" method="get" action="graph_page_data.php">
+                    <?php
+                    echo "<input type=\"hidden\" name=\"tests\" value=\"$tests\">";
+                    echo "<input type=\"hidden\" name=\"medianMetric\" value=\"$median_metric\">";
+                    ?>
+                    View: <input type="checkbox" name="fv" value="1"
+                        <?php if ($fv == '1') echo "checked"; ?> >First
+                    <input type="checkbox" name="rv" value="1"
+                        <?php if ($rv == '1') echo "checked"; ?> >Repeat<br>
+                    Median: <input type="checkbox" name="median_value" value="1"
+                        <?php if ($median_value == '1') echo "checked"; ?> >Of plotted metric
+                    <input type="checkbox" name="median_run" value="1"
+                        <?php if ($median_run == '1') echo "checked"; ?> >Run with median
+                        <?php echo $median_metric; ?> <br>
+                    <br>
+                    <input type="submit">
+                </form>
+            </div>
+
             <div id="result">
             <h1>Test Result Data Plots</h1>
+            <?php
+            if (count($common_labels) > 0) {
+              echo "<h2 style='text-align: center'>${common_label}</h2>";
+            }
+            ?>
             <?php
             $metrics = array('docTime' => 'Load Time (onload - ms)', 
                             'SpeedIndex' => 'Speed Index',
@@ -51,40 +131,44 @@ $pageData = loadAllPageData($testPath);
             $customMetrics = null;
             $csiMetrics = null;
             $userTimings = null;
-            foreach ($pageData as &$pageRun)
-              foreach ($pageRun as &$data) {
-                if (array_key_exists('userTime', $data))
-                  $metrics['userTime'] = 'Last User Timing Mark';
-                if (array_key_exists('custom', $data) && is_array($data['custom']) && count($data['custom'])) {
-                  if (!isset($customMetrics))
-                    $customMetrics = array();
-                  foreach ($data['custom'] as $metric) {
-                    if (!array_key_exists($metric, $customMetrics))
-                      $customMetrics[$metric] = "Custom metric - $metric";
+            foreach ($pagesData as &$pageData) {
+              foreach ($pageData as &$pageRun)
+                foreach ($pageRun as &$data) {
+                  if (array_key_exists('userTime', $data))
+                    $metrics['userTime'] = 'Last User Timing Mark';
+                  if (array_key_exists('custom', $data) && is_array($data['custom']) && count($data['custom'])) {
+                    if (!isset($customMetrics))
+                      $customMetrics = array();
+                    foreach ($data['custom'] as $metric) {
+                      if (!array_key_exists($metric, $customMetrics))
+                        $customMetrics[$metric] = "Custom metric - $metric";
+                    }
                   }
-                }
-                foreach($data as $metric => $value) {
-                  if (substr($metric, 0, 9) == 'userTime.') {
-                    if (!isset($userTimings))
-                      $userTimings = array();
-                    $userTimings[$metric] = 'User Timing - ' . substr($metric, 9);
+                  foreach($data as $metric => $value) {
+                    if (substr($metric, 0, 9) == 'userTime.') {
+                      if (!isset($userTimings))
+                        $userTimings = array();
+                      $userTimings[$metric] = 'User Timing - ' . substr($metric, 9);
+                    }
                   }
-                }
-                $timingCount = count($userTimings);
-                if (array_key_exists('CSI', $data) && is_array($data['CSI']) && count($data['CSI'])) {
-                  if (!isset($csiMetrics))
-                    $csiMetrics = array();
-                  foreach ($data['CSI'] as $metric) {
-                    if (preg_match('/^[0-9\.]+$/', $data["CSI.$metric"]) &&
-                        !array_key_exists($metric, $csiMetrics)) {
-                      $csiMetrics[$metric] = "CSI - $metric";
+                  $timingCount = count($userTimings);
+                  if (array_key_exists('CSI', $data) && is_array($data['CSI']) && count($data['CSI'])) {
+                    if (!isset($csiMetrics))
+                      $csiMetrics = array();
+                    foreach ($data['CSI'] as $metric) {
+                      if (preg_match('/^[0-9\.]+$/', $data["CSI.$metric"]) &&
+                          !array_key_exists($metric, $csiMetrics)) {
+                        $csiMetrics[$metric] = "CSI - $metric";
+                      }
                     }
                   }
                 }
-              }
-            if (array_key_exists('testinfo', $test) && !$test['testinfo']['video']) {
-                unset($metrics['SpeedIndex']);
             }
+
+            if ($removeSpeedIndex) {
+              unset($metrics['SpeedIndex']);
+            }
+
             foreach($metrics as $metric => $label) {
                 InsertChart($metric, $label);
             }
@@ -108,18 +192,13 @@ $pageData = loadAllPageData($testPath);
             }
             ?>
             </div>
-            
+
             <?php include('footer.inc'); ?>
             <script type="text/javascript" src="//www.google.com/jsapi"></script>
             <script type="text/javascript">
                 <?php
-                    $runs = $test['testinfo']['runs'];
-                    if (array_key_exists('discard', $test['testinfo'])) {
-                        $runs -= $test['testinfo']['discard'];
-                    }
                     echo "var chartData = " . json_encode($chartData) . ";\n";
-                    echo "var runs = $runs;\n";
-                    echo "var fvonly = {$test['testinfo']['fvonly']};\n";
+                    echo "var runs = $num_runs;\n";
                 ?>
                 google.load("visualization", "1", {packages:["corechart"]});
                 google.setOnLoadCallback(drawChart);
@@ -158,11 +237,13 @@ $pageData = loadAllPageData($testPath);
                         data.addRow(row);
                       }
                       var options = {
-                          width: 800,
-                          height: 400,
+                          legend: (series.length == 1) ? 'none' : 'right',
+                          width: 950,
+                          height: Math.max(500, series.length * 45),
                           lineWidth: 1,
-                          hAxis: {gridlines: {count: runs}},
-                          series: series
+                          hAxis: {minValue: 1, maxValue: runs, gridlines: {count: runs}},
+                          series: series,
+                          chartArea: { width: "60%", left: 70, height: "85%" }
                       }
                       var chart = new google.visualization.LineChart(
                           document.getElementById(chartData[metric].div));
@@ -184,32 +265,41 @@ $pageData = loadAllPageData($testPath);
  * @param string $label Label corresponding to metric
  */
 function InsertChart($metric, $label) {
-  global $pageData;
   global $chartData;
-  global $test;
+  global $num_runs;
+  global $views;
+  global $colors;
+  global $light_colors;
   global $median_metric;
-  if (array_key_exists('testinfo', $test)) {
-    // Write HTML for chart
-    $div = "{$metric}Chart";
-    $runs = $test['testinfo']['runs'];
-    if (array_key_exists('discard', $test['testinfo']))
-      $runs -= $test['testinfo']['discard'];
-    echo "<h2 id=\"$metric\">" . htmlspecialchars($label) . "</h2>";
-    echo "<div id=\"$div\" class=\"chart\"></div>\n";
-
-    // Add data in Chart object to $chartData for later chart construction.
-    $chartColumns = array();
-    $chartColumns[] = ChartColumn::runs($runs);
-    $chartColumnsFirstView = ChartColumn::dataMedianColumns(
-      $pageData, 0, $metric, $median_metric, 'blue', 'lightblue', 'First View');
-    $fvonly = $test['testinfo']['fvonly'];
-    if ($fvonly) {
-      $chartColumns = array_merge($chartColumns, $chartColumnsFirstView);
-    } else {
-      $chartColumnsRepeatView = ChartColumn::dataMedianColumns(
-        $pageData, 1, $metric, $median_metric, 'red', 'pink', 'Repeat View');
-      $chartColumns = array_merge(
-        $chartColumns, $chartColumnsFirstView, $chartColumnsRepeatView);
+  global $pagesData;
+  global $testsInfo;
+  global $testsId;
+  global $median_value;
+  global $median_run;
+  // Write HTML for chart
+  $div = "{$metric}Chart";
+  $num_runs = max(array_map("numRunsFromTestInfo", $testsInfo));
+  echo "<h2 id=\"$metric\">" . htmlspecialchars($label) . "</h2>";
+  echo "<div id=\"$div\" class=\"chart\"></div>\n";
+  if ($testsInfo) {
+    $chartColumns = array(ChartColumn::runs($num_runs));
+    $view_num = 0;
+    foreach ($views as $cached) {
+      foreach ($pagesData as $key=>$pageData) {
+        $labels = array();
+        if (count($pagesData) > 1) {
+          $labels[] = $testsId[$key];
+        }
+        if (count($views) > 1) {
+          $labels[] = ($cached == '1') ? 'Repeat View' : 'First View';
+        }
+        // Prepare Chart object and add to $chartData for later chart construction.
+        $chartColumnsAdd = ChartColumn::dataMedianColumns($pageData, $cached,
+          $metric, $median_metric, $colors[$view_num], $light_colors[$view_num], $labels,
+          $num_runs, $median_run, $median_value);
+        $chartColumns = array_merge($chartColumns, $chartColumnsAdd);
+        $view_num++;
+      }
     }
     $chart = new Chart($div, $chartColumns);
     $chartData[$metric] = $chart;
