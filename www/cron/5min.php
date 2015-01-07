7e05a9c396dd60919e0120093f8a4a2678e6283a
pmeenan <pmeenan@webpagetest.org>
2015-01-07 16:59:18 -0500

    Added some cleanup for extraneous files on the EC2 instances

--- www/cron/5min.php
+++ www/cron/5min.php
@@ -10,6 +10,9 @@ $lock = Lock("cron-5", false, 1200);
 if (!isset($lock))
   exit(0);
 
+if (GEtSetting('ec2_initialized'))
+  CleanGetWork();
+
 require_once('./ec2/ec2.inc.php');
 if (GetSetting('ec2_key')) {
   EC2_TerminateIdleInstances();
@@ -19,4 +22,18 @@ if (GetSetting('ec2_key')) {
 
 if (GetSetting('sbl_api_key'))
   SBL_Update();
+  
+/**
+* Clean up extraneous getwork.php.xxx files that may be left behind
+* from using wget in a cron job.  The server AMI was supposed to run wget
+* silently but there have been reports of the files getting created
+* 
+*/
+function CleanGetWork() {
+  $files = glob('/var/www/getwork.php.*');
+  foreach($files as $file) {
+    if (is_file($file))
+      unlink($file);
+  }
+}
 ?>
