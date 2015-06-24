7c34a602301de935ffa15bf2b04a7c53debafe6f
pmeenan <pmeenan@webpagetest.org>
2015-06-24 08:45:49 -0400

    Fixed #453 - Allow periods in the label names

--- www/runtest.php
+++ www/runtest.php
@@ -104,7 +104,7 @@
             $test['notify'] = trim($req_notify);
             $test['video'] = $req_video;
             $test['continuousVideo'] = isset($req_continuousVideo) && $req_continuousVideo ? 1 : 0;
-            $test['label'] = preg_replace('/[^a-zA-Z0-9 \-_]/', '', trim($req_label));
+            $test['label'] = preg_replace('/[^a-zA-Z0-9 \-_\.]/', '', trim($req_label));
             $test['industry'] = trim($req_ig);
             $test['industry_page'] = trim($req_ip);
             $test['median_video'] = (int)$req_mv;
