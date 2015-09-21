a2fad8a1ab2464578982a0850e123d995e8a7708
BirgerK <Birger.Kamp@iteratec.de>
2015-09-21 15:01:49 +0200

    moved static code for creating and viewing waterfall to accordion-template

--- www/details.php
+++ www/details.php
@@ -406,46 +406,8 @@ $page_description = "Website performance test details$testLabel";
 			<a name="waterfall_view<?= getEventNameID($eventName); ?>"></a>
 			<h3 name="waterfall_view<?= getEventNameID($eventName); ?>" id="slide_opener_waterfall_view<?= getEventNameID($eventName); ?>" data-slideid="waterfall_view<?= getEventNameID($eventName); ?>" class="slide_opener close_accordeon">Waterfall View - <?= $eventName ?> </h3>
 			<div id="waterfall_view<?= getEventNameID($eventName); ?>" class="hide_on_load" data-waterfallcontainer="waterfallcontainer-<?= $data['eventNumber'] ?>" data-eventname="<?= $eventName ?>">
-				<a href="#quicklinks">Back to Quicklinks</a>
-			<table border="1" bordercolor="silver" cellpadding="2px" cellspacing="0" style="width:auto; font-size:11px; margin-left:auto; margin-right:auto;">
-				<tr>
-					<td><table><tr><td><div class="bar" style="width:15px; background-color:#1f7c83"></div></td><td>DNS Lookup</td></tr></table></td>
-					<td><table><tr><td><div class="bar" style="width:15px; background-color:#e58226"></div></td><td>Initial Connection</td></tr></table></td>
-					<?php if($secure) { ?>
-						<td><table><tr><td><div class="bar" style="width:15px; background-color:#c141cd"></div></td><td>SSL Negotiation</td></tr></table></td>
-					<?php } ?>
-					<td><table><tr><td><div class="bar" style="width:15px; background-color:#1fe11f"></div></td><td>Time to First Byte</td></tr></table></td>
-					<td><table><tr><td><div class="bar" style="width:15px; background-color:#1977dd"></div></td><td>Content Download</td></tr></table></td>
-					<td style="vertical-align:middle; padding: 4px;"><div style="background-color:#ffff60">&nbsp;3xx response&nbsp;</div></td>
-					<td style="vertical-align:middle; padding: 4px;"><div style="background-color:#ff6060">&nbsp;4xx+ response&nbsp;</div></td>
-				</tr>
-			</table>
-			<table border="1" bordercolor="silver" cellpadding="2px" cellspacing="0" style="width:auto; font-size:11px; margin-left:auto; margin-right:auto; margin-top:11px;">
-				<tr>
-					<td><table><tr><td><div class="bar" style="width:2px; background-color:#28BC00"></div></td><td>Start Render</td></tr></table></td>
-					<?php
-					if (array_key_exists('aft', $data) && $data['aft'] )
-						echo '<td><table><tr><td><div class="bar" style="width:2px; background-color:#FF0000"></div></td><td>Above the Fold</td></tr></table></td>';
-					if (array_key_exists('domTime', $data) && (float)$data['domTime'] > 0.0 )
-						echo '<td><table><tr><td><div class="bar" style="width:2px; background-color:#F28300"></div></td><td>DOM Element</td></tr></table></td>';
-					if(array_key_exists('firstPaint', $data) && (float)$data['firstPaint'] > 0.0 )
-						echo '<td><table><tr><td><div class="bar" style="width:2px; background-color:#8FBC83"></div></td><td>msFirstPaint</td></tr></table></td>';
-					if(array_key_exists('domContentLoadedEventStart', $data) && (float)$data['domContentLoadedEventStart'] > 0.0 )
-						echo '<td><table><tr><td><div class="bar" style="width:15px; background-color:#D888DF"></div></td><td>DOM Content Loaded</td></tr></table></td>';
-					if(array_key_exists('loadEventStart', $data) && (float)$data['loadEventStart'] > 0.0 )
-						echo '<td><table><tr><td><div class="bar" style="width:15px; background-color:#C0C0FF"></div></td><td>On Load</td></tr></table></td>';
-					echo '<td><table><tr><td><div class="bar" style="width:2px; background-color:#0000FF"></div></td><td>Document Complete</td></tr></table></td>';
-					if(array_key_exists('userTime', $data) || (array_key_exists('enable_google_csi', $settings) && $settings['enable_google_csi']))
-						echo '<td><table><tr><td><div class="arrow-down"></div></td><td>User Timings</td></tr></table></td>';
-					?>
-				</tr>
-			</table>
-			<br>
 				<div id="waterfallcontainer-<?= $data['eventNumber'] ?>" style="width:930px"></div>
 			<?php
-			echo "<br><a href=\"/customWaterfall.php?width=930&test=$id&run=$run&cached=$cached&eventName=$eventName\">customize waterfall</a> &#8226; ";
-			echo "<a id=\"view-images\" href=\"/pageimages.php?test=$id&run=$run&cached=$cached&eventName=$eventName\">View all Images (for event name $eventName)</a>";
-			echo "<br/><br/><br/>";
 			echo "</div>";
 			}
 			?>
--- www/template_create_waterfall.php
+++ www/template_create_waterfall.php
@@ -19,6 +19,45 @@ $type = "waterfall";
 $file = generateViewImagePath($testPath, $eventName, $run, $cached, $type);
 $dataArray = loadPageRunData($testPath, $run, $cached, array('SpeedIndex' => true, 'allEvents' => true));
 $requests = getRequests($id, $testPath, $run, $cached, $secure, $haveLocations, true, true, true);
+?>
+
+<a href="#quicklinks">Back to Quicklinks</a>
+			<table border="1" bordercolor="silver" cellpadding="2px" cellspacing="0" style="width:auto; font-size:11px; margin-left:auto; margin-right:auto;">
+				<tr>
+					<td><table><tr><td><div class="bar" style="width:15px; background-color:#1f7c83"></div></td><td>DNS Lookup</td></tr></table></td>
+					<td><table><tr><td><div class="bar" style="width:15px; background-color:#e58226"></div></td><td>Initial Connection</td></tr></table></td>
+					<?php if($secure) { ?>
+    <td><table><tr><td><div class="bar" style="width:15px; background-color:#c141cd"></div></td><td>SSL Negotiation</td></tr></table></td>
+<?php } ?>
+<td><table><tr><td><div class="bar" style="width:15px; background-color:#1fe11f"></div></td><td>Time to First Byte</td></tr></table></td>
+<td><table><tr><td><div class="bar" style="width:15px; background-color:#1977dd"></div></td><td>Content Download</td></tr></table></td>
+<td style="vertical-align:middle; padding: 4px;"><div style="background-color:#ffff60">&nbsp;3xx response&nbsp;</div></td>
+<td style="vertical-align:middle; padding: 4px;"><div style="background-color:#ff6060">&nbsp;4xx+ response&nbsp;</div></td>
+</tr>
+</table>
+<table border="1" bordercolor="silver" cellpadding="2px" cellspacing="0" style="width:auto; font-size:11px; margin-left:auto; margin-right:auto; margin-top:11px;">
+    <tr>
+        <td><table><tr><td><div class="bar" style="width:2px; background-color:#28BC00"></div></td><td>Start Render</td></tr></table></td>
+        <?php
+        if (array_key_exists('aft', $data) && $data['aft'] )
+            echo '<td><table><tr><td><div class="bar" style="width:2px; background-color:#FF0000"></div></td><td>Above the Fold</td></tr></table></td>';
+        if (array_key_exists('domTime', $data) && (float)$data['domTime'] > 0.0 )
+            echo '<td><table><tr><td><div class="bar" style="width:2px; background-color:#F28300"></div></td><td>DOM Element</td></tr></table></td>';
+        if(array_key_exists('firstPaint', $data) && (float)$data['firstPaint'] > 0.0 )
+            echo '<td><table><tr><td><div class="bar" style="width:2px; background-color:#8FBC83"></div></td><td>msFirstPaint</td></tr></table></td>';
+        if(array_key_exists('domContentLoadedEventStart', $data) && (float)$data['domContentLoadedEventStart'] > 0.0 )
+            echo '<td><table><tr><td><div class="bar" style="width:15px; background-color:#D888DF"></div></td><td>DOM Content Loaded</td></tr></table></td>';
+        if(array_key_exists('loadEventStart', $data) && (float)$data['loadEventStart'] > 0.0 )
+            echo '<td><table><tr><td><div class="bar" style="width:15px; background-color:#C0C0FF"></div></td><td>On Load</td></tr></table></td>';
+        echo '<td><table><tr><td><div class="bar" style="width:2px; background-color:#0000FF"></div></td><td>Document Complete</td></tr></table></td>';
+        if(array_key_exists('userTime', $data) || (array_key_exists('enable_google_csi', $settings) && $settings['enable_google_csi']))
+            echo '<td><table><tr><td><div class="arrow-down"></div></td><td>User Timings</td></tr></table></td>';
+        ?>
+    </tr>
+</table>
+<br>
+
+<?php
 if($test_info['imageCaching']){
     if(!file_exists($file)){
         createImageAndSave($id, $testPath, $test_info, $eventName, $run, $cached, $dataArray[$eventName][$run][$cached], $type);
@@ -27,4 +66,8 @@ if($test_info['imageCaching']){
 } else {
     InsertWaterfall($url, $requests, $id, $run, $cached, '', $eventName);
 }
+
+echo "<br><a href=\"/customWaterfall.php?width=930&test=$id&run=$run&cached=$cached&eventName=$eventName\">customize waterfall</a> &#8226; ";
+echo "<a id=\"view-images\" href=\"/pageimages.php?test=$id&run=$run&cached=$cached&eventName=$eventName\">View all Images (for event name $eventName)</a>";
+echo "<br/><br/><br/>";
 ?>
