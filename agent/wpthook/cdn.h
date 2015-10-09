940fc7f84691aad59faadd32a1276ef8c2cc367f
eceres <ericceres@yahoo.com>
2015-10-09 08:19:12 -0700

    Add Aryaka to CDN list

--- agent/wpthook/cdn.h
+++ agent/wpthook/cdn.h
@@ -142,6 +142,9 @@ CDN_PROVIDER cdnList[] = {
   {".cdninstagram.com", "Facebook"},
   {".rlcdn.com", "Reapleaf"},
   {".wp.com", "WordPress Jetpack"},
+  {".aads1.net", "Aryaka"},
+  {".aadscn.net", "Aryaka"},
+  {".aadscng.net", "Aryaka"},
   {"END_MARKER", "END_MARKER"}
 };
 
@@ -172,5 +175,6 @@ CDN_PROVIDER_HEADER cdnHeaderList[] = {
   {"server", "Golfe2", "Google"},
   {"server", "tsa_b", "Twitter"},
   {"X-CDN", "Incapsula", "Incapsula"},
-  {"X-Iinfo", "", "Incapsula"}
+  {"X-Iinfo", "", "Incapsula"},
+  {"X-Ar-Debug", "", "Aryaka"}
 };
