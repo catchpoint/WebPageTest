0e075dfa8e584319ec7111479e2dfddd74c9ad8e
Patrick Meenan <pmeenan@chromium.org>
2015-10-23 09:06:36 -0400

    Fixed #512 - Added support for persisting a default metric to use for calculation the median run on a per-test basis

--- www/runtest.php
+++ www/runtest.php
@@ -171,6 +171,8 @@
             $test['clearcerts'] = array_key_exists('clearcerts', $_REQUEST) && $_REQUEST['clearcerts'] ? 1 : 0;
             $test['orientation'] = array_key_exists('orientation', $_REQUEST) ? trim($_REQUEST['orientation']) : 'default';
             $test['responsive'] = array_key_exists('responsive', $_REQUEST) && $_REQUEST['responsive'] ? 1 : 0;
+            if (isset($_REQUEST['medianMetric']))
+              $test['medianMetric'] = $_REQUEST['medianMetric'];
 
             if (array_key_exists('tsview_id', $_REQUEST)){
               $test['tsview_id'] = $_REQUEST['tsview_id'];
