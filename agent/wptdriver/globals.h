75607bac8da15d9d5b083d1df03666806d1b1ea3
Karthik Krishnamurthy <karthik3186@gmail.com>
2015-11-02 21:20:21 -0800

    Fix: handle timeouts robustly

---
+++ agent/wptdriver/globals.h
@@ -0,0 +1,10 @@
+#pragma once
+
+extern const TCHAR * FLASH_CACHE_DIR;
+extern const TCHAR * SILVERLIGHT_CACHE_DIR;
+extern const TCHAR * GLOBAL_TESTING_MUTEX;
+extern const TCHAR * BROWSER_STARTED_EVENT;
+extern const TCHAR * BROWSER_DONE_EVENT;
+
+extern int const BROWSER_STARTED_EVENT_TIMEOUT;
+extern int const RESULTS_PROCESSING_GRACE_PERIOD;
