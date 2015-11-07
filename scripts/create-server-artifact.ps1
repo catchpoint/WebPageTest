7b8d0778516c909bcb2331300cef8bb2ce96d3c4
Olivier Crameri <olivier.crameri@appdynamics.com>
2015-11-07 10:18:59 -0800

    Fix directory structure of the wptserver artifact

--- scripts/create-server-artifact.ps1
+++ scripts/create-server-artifact.ps1
@@ -6,14 +6,22 @@ Build an artifact with the webpagetest sources
 #>
 
 $wptroot = $PSScriptRoot + "\.."
+$outdir = ($wptroot + "\_dist-www")
+
+if (Test-Path $outdir) {
+    Remove-Item -Recurse -Force $outdir
+}
+mkdir $outdir | Out-Null
 
 $wptserverdir = Get-ChildItem $wptroot www
+Copy-Item $wptserverdir ($outdir + "\www") -recurse
+
 $wptserverzip = ($wptroot + "\wptserver.zip")
 if (Test-Path $wptserverzip) {
     Remove-Item $wptserverzip
 }
 Add-Type -Assembly System.IO.Compression.FileSystem
-[System.IO.Compression.ZipFile]::CreateFromDirectory($wptserverdir.fullname,
+[System.IO.Compression.ZipFile]::CreateFromDirectory($outdir,
         $wptserverzip)
 
 "-> $wptserverzip created"
