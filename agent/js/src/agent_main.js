27e80a1ed1d828d2ebb86bb983b082169123b868
Patrick Meenan <pmeenan@chromium.org>
2016-02-11 18:31:20 -0500

    Added exception handling around the tempdir cleanup

--- agent/js/src/agent_main.js
+++ agent/js/src/agent_main.js
@@ -126,17 +126,19 @@ Agent.prototype.scheduleNoFault_ = function(description, f) {
 };
 
 var deleteFolderRecursive = function(path) {
-  if (fs.existsSync(path)) {
-    fs.readdirSync(path).forEach(function(file,index){
-      var curPath = path + "/" + file;
-      if(fs.lstatSync(curPath).isDirectory()) { // recurse
-        deleteFolderRecursive(curPath);
-      } else { // delete file
-        fs.unlinkSync(curPath);
-      }
-    });
-    fs.rmdirSync(path);
-  }
+  try {
+    if (fs.existsSync(path)) {
+      fs.readdirSync(path).forEach(function(file,index){
+        var curPath = path + "/" + file;
+        if(fs.lstatSync(curPath).isDirectory()) { // recurse
+          deleteFolderRecursive(curPath);
+        } else { // delete file
+          fs.unlinkSync(curPath);
+        }
+      });
+      fs.rmdirSync(path);
+    }
+  } catch(e) {}
 };
 
 /**
