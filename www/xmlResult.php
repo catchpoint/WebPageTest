67cb6a3d933ca24c4a1d0d7d21f478fa55190341
Stefan Burnicki <stefan.burnicki@iteratec.de>
2016-06-08 23:14:09 +0200

    refactor: xmlResult run XML generation more flexible
    The code to generate the XML result for a run was moved along
    with the related functions into XmlResultGenerator.inc.
    
    Then the code is now encapsuled in a class and more dynamic,
    so it can generate the XML for first and repeat view.
    
    Dynamic generation of URLs and file paths was included.
    
    This will make upcoming multi-step changes much easier.
    
    The generated XML is very similar to the old XML.
    There are some minor changes for the cached view of each run:
    
      - new-line between `<rawData>` and `<headers>` tag
      - missing link for `optimization_report` as it seems to be deprecated
        and points to a file which is not even included in the git project
      - added `/cached/` suffix for `breakdown` and `domains`. These URLs
        aren't necessary, but supported by current rewrite rules and the
        target files.

--- www/xmlResult.php
+++ www/xmlResult.php
@@ -21,6 +21,8 @@ require_once('breakdown.inc');
 require_once('devtools.inc.php');
 require_once('archive.inc');
 
+require_once 'XmlResultGenerator.inc';
+
 // see if we are sending abbreviated results
 $pagespeed = 0;
 if (array_key_exists('pagespeed', $_REQUEST))
@@ -223,6 +225,7 @@ else
             echo "</median>\n";
         }
 
+        $xmlGenerator = new XmlResultGenerator($test, $pageData, $id, $testPath, "$protocol://$host$uri", $pagespeed);
         // spit out the raw data for each run
         for( $i = 1; $i <= $runs; $i++ )
         {
@@ -232,198 +235,16 @@ else
             // first view
             if( isset( $pageData[$i] ) )
             {
-                if( isset( $pageData[$i][0] ) )
-                {
+                if (isset($pageData[$i][0])) {
                     echo "<firstView>\n";
-                    if (array_key_exists('testinfo', $test)) {
-                      $tester = null;
-                      if (array_key_exists('tester', $test['testinfo']))
-                        $tester = $test['testinfo']['tester'];
-                      if (array_key_exists('test_runs', $test['testinfo']) &&
-                          array_key_exists($i, $test['testinfo']['test_runs']) &&
-                          array_key_exists('tester', $test['testinfo']['test_runs'][$i]))
-                        $tester = $test['testinfo']['test_runs'][$i]['tester'] . '<br>';
-                      if (isset($tester))
-                        echo "<tester>" . xml_entities($tester) . "</tester>\n";
-                    }
-                    echo "<results>\n";
-                    echo ArrayToXML($pageData[$i][0]);
-                    if( $pagespeed )
-                    {
-                        $score = GetPageSpeedScore("$testPath/{$i}_pagespeed.txt");
-                        if( strlen($score) )
-                            echo "<PageSpeedScore>$score</PageSpeedScore>\n";
-                    }
-                    echo "</results>\n";
-
-                    // links to the relevant pages
-                    echo "<pages>\n";
-                    if( FRIENDLY_URLS )
-                    {
-                        echo "<details>$protocol://$host$uri/result/$id/$i/details/</details>\n";
-                        echo "<checklist>$protocol://$host$uri/result/$id/$i/performance_optimization/</checklist>\n";
-                        echo "<breakdown>$protocol://$host$uri/result/$id/$i/breakdown/</breakdown>\n";
-                        echo "<domains>$protocol://$host$uri/result/$id/$i/domains/</domains>\n";
-                        echo "<screenShot>$protocol://$host$uri/result/$id/$i/screen_shot/</screenShot>\n";
-                    }
-                    else
-                    {
-                        echo "<details>$protocol://$host$uri/details.php?test=$id&amp;run=$i</details>\n";
-                        echo "<checklist>$protocol://$host$uri/performance_optimization.php?test=$id&amp;run=$i</checklist>\n";
-                        echo "<breakdown>$protocol://$host$uri/breakdown.php?test=$id&amp;run=$i</breakdown>\n";
-                        echo "<domains>$protocol://$host$uri/domains.php?test=$id&amp;run=$i</domains>\n";
-                        echo "<screenShot>$protocol://$host$uri/screen_shot.php?test=$id&amp;run=$i</screenShot>\n";
-                    }
-                    echo "</pages>\n";
-                    
-                    // urls for the relevant images
-                    echo "<thumbnails>\n";
-                    echo "<waterfall>$protocol://$host$uri/result/$id/{$i}_waterfall_thumb.png</waterfall>\n";
-                    echo "<checklist>$protocol://$host$uri/result/$id/{$i}_optimization_thumb.png</checklist>\n";
-                    if( is_file("$testPath/{$i}_screen.jpg") )
-                      echo "<screenShot>$protocol://$host$uri/result/$id/{$i}_screen_thumb.jpg</screenShot>\n";
-                    echo "</thumbnails>\n";
-
-                    echo "<images>\n";
-                    echo "<waterfall>$protocol://$host$uri$path/{$i}_waterfall.png</waterfall>\n";
-                    echo "<connectionView>$protocol://$host$uri$path/{$i}_connection.png</connectionView>\n";
-                    echo "<checklist>$protocol://$host$uri$path/{$i}_optimization.png</checklist>\n";
-                    if( is_file("$testPath/{$i}_screen.jpg") )
-                      echo "<screenShot>$protocol://$host$uri/getfile.php?test=$id&amp;file={$i}_screen.jpg</screenShot>\n";
-                    if( is_file("$testPath/{$i}_screen.png") )
-                        echo "<screenShotPng>$protocol://$host$uri/getfile.php?test=$id&amp;file={$i}_screen.png</screenShotPng>\n";
-                    echo "</images>\n";
-
-                    // raw results
-                    echo "<rawData>";
-                    if (gz_is_file("$testPath/{$i}_report.txt"))
-                      echo "<headers>$protocol://$host$uri$path/{$i}_report.txt</headers>\n";
-                    if (is_file("$testPath/{$i}_bodies.zip"))
-                        echo "<bodies>$protocol://$host$uri$path/{$i}_bodies.zip</bodies>\n";
-                    if (gz_is_file("$testPath/{$i}_IEWPG.txt"))
-                      echo "<pageData>$protocol://$host$uri$path/{$i}_IEWPG.txt</pageData>\n";
-                    if (gz_is_file("$testPath/{$i}_IEWTR.txt"))
-                      echo "<requestsData>$protocol://$host$uri$path/{$i}_IEWTR.txt</requestsData>\n";
-                    if (gz_is_file("$testPath/{$i}_progress.csv"))
-                      echo "<utilization>$protocol://$host$uri$path/{$i}_progress.csv</utilization>\n";
-                    if (gz_is_file("$testPath/{$i}_pagespeed.txt"))
-                      echo "<PageSpeedData>$protocol://$host$uri/result/$id/{$i}_pagespeed.txt</PageSpeedData>\n";
-                    echo "</rawData>\n";
-                    
-                    // video frames
-                    $startOffset = array_key_exists('testStartOffset', $pageData[$i][0]) ? intval(round($pageData[$i][0]['testStartOffset'])) : 0;
-                    $progress = GetVisualProgress($testPath, $i, 0, null, null, $startOffset);
-                    if (array_key_exists('frames', $progress) && is_array($progress['frames']) && count($progress['frames'])) {
-                      echo "<videoFrames>\n";
-                      foreach($progress['frames'] as $ms => $frame) {
-                          echo "<frame>\n";
-                          echo "<time>$ms</time>\n";
-                          echo "<image>$protocol://$host$uri/getfile.php?test=$id&amp;video=video_{$i}&amp;file={$frame['file']}</image>\n";
-                          echo "<VisuallyComplete>{$frame['progress']}</VisuallyComplete>\n";
-                          echo "</frame>\n";
-                      }
-                      echo "</videoFrames>\n";
-                    }
-                    
-                    xmlDomains($id, $testPath, $i, 0);
-                    xmlBreakdown($id, $testPath, $i, 0);
-                    if (array_key_exists('requests', $_REQUEST) && $_REQUEST['requests'] != 'median')
-                      xmlRequests($id, $testPath, $i, 0);
-                    StatusMessages($id, $testPath, $i, 0);
-                    ConsoleLog($id, $testPath, $i, 0);
+                    $xmlGenerator->printRun($i, false);
                     echo "</firstView>\n";
                 }
 
                 // repeat view
-                if( isset( $pageData[$i][1] ) )
-                {
+                if( isset( $pageData[$i][1] ) ) {
                     echo "<repeatView>\n";
-                    if (array_key_exists('testinfo', $test)) {
-                      $tester = null;
-                      if (array_key_exists('tester', $test['testinfo']))
-                        $tester = $test['testinfo']['tester'];
-                      if (array_key_exists('test_runs', $test['testinfo']) &&
-                          array_key_exists($i, $test['testinfo']['test_runs']) &&
-                          array_key_exists('tester', $test['testinfo']['test_runs'][$i]))
-                        $tester = $test['testinfo']['test_runs'][$i]['tester'] . '<br>';
-                      if (isset($tester))
-                        echo "<tester>" . xml_entities($tester) . "</tester>\n";
-                    }
-                    echo "<results>\n";
-                    echo ArrayToXML($pageData[$i][1]);
-                    if( $pagespeed )
-                    {
-                        $score = GetPageSpeedScore("$testPath/{$i}_Cached_pagespeed.txt");
-                        if( strlen($score) )
-                            echo "<PageSpeedScore>$score</PageSpeedScore>\n";
-                    }
-                    echo "</results>\n";
-
-                    // links to the relevant pages
-                    echo "<pages>\n";
-                    echo "<details>$protocol://$host$uri/result/$id/$i/details/cached/</details>\n";
-                    echo "<checklist>$protocol://$host$uri/result/$id/$i/performance_optimization/cached/</checklist>\n";
-                    echo "<report>$protocol://$host$uri/result/$id/$i/optimization_report/cached/</report>\n";
-                    echo "<breakdown>$protocol://$host$uri/result/$id/$i/breakdown/</breakdown>\n";
-                    echo "<domains>$protocol://$host$uri/result/$id/$i/domains/</domains>\n";
-                    echo "<screenShot>$protocol://$host$uri/result/$id/$i/screen_shot/cached/</screenShot>\n";
-                    echo "</pages>\n";
-                    
-                    // urls for the relevant images
-                    echo "<thumbnails>\n";
-                    echo "<waterfall>$protocol://$host$uri/result/$id/{$i}_Cached_waterfall_thumb.png</waterfall>\n";
-                    echo "<checklist>$protocol://$host$uri/result/$id/{$i}_Cached_optimization_thumb.png</checklist>\n";
-                    if( is_file("$testPath/{$i}_Cached_screen.jpg") )
-                      echo "<screenShot>$protocol://$host$uri/result/$id/{$i}_Cached_screen_thumb.jpg</screenShot>\n";
-                    echo "</thumbnails>\n";
-
-                    echo "<images>\n";
-                    echo "<waterfall>$protocol://$host$uri$path/{$i}_Cached_waterfall.png</waterfall>\n";
-                    echo "<connectionView>$protocol://$host$uri$path/{$i}_Cached_connection.png</connectionView>\n";
-                    echo "<checklist>$protocol://$host$uri$path/{$i}_Cached_optimization.png</checklist>\n";
-                    if( is_file("$testPath/{$i}_Cached_screen.jpg") )
-                      echo "<screenShot>$protocol://$host$uri/getfile.php?test=$id&amp;file={$i}_Cached_screen.jpg</screenShot>\n";
-                    if( is_file("$testPath/{$i}_Cached_screen.png") )
-                        echo "<screenShotPng>$protocol://$host$uri/getfile.php?test=$id&amp;file={$i}_Cached_screen.png</screenShotPng>\n";
-                    echo "</images>\n";
-
-                    // raw results
-                    echo "<rawData>\n";
-                    if (gz_is_file("$testPath/{$i}_Cached_report.txt"))
-                      echo "<headers>$protocol://$host$uri$path/{$i}_Cached_report.txt</headers>\n";
-                    if (is_file("$testPath/{$i}_Cached_bodies.zip"))
-                        echo "<bodies>$protocol://$host$uri$path/{$i}_Cached_bodies.zip</bodies>\n";
-                    if (gz_is_file("$testPath/{$i}_Cached_IEWPG.txt"))
-                      echo "<pageData>$protocol://$host$uri$path/{$i}_Cached_IEWPG.txt</pageData>\n";
-                    if (gz_is_file("$testPath/{$i}_Cached_IEWTR.txt"))
-                      echo "<requestsData>$protocol://$host$uri$path/{$i}_Cached_IEWTR.txt</requestsData>\n";
-                    if (gz_is_file("$testPath/{$i}_Cached_progress.csv"))
-                      echo "<utilization>$protocol://$host$uri$path/{$i}_Cached_progress.csv</utilization>\n";
-                    if (gz_is_file("$testPath/{$i}_Cached_pagespeed.txt"))
-                      echo "<PageSpeedData>$protocol://$host$uri/result/$id/{$i}_Cached_pagespeed.txt</PageSpeedData>\n";
-                    echo "</rawData>\n";
-                    
-                    // video frames
-                    $startOffset = array_key_exists('testStartOffset', $pageData[$i][1]) ? intval(round($pageData[$i][1]['testStartOffset'])) : 0;
-                    $progress = GetVisualProgress($testPath, $i, 1, null, null, $startOffset);
-                    if (array_key_exists('frames', $progress) && is_array($progress['frames']) && count($progress['frames'])) {
-                      echo "<videoFrames>\n";
-                      foreach($progress['frames'] as $ms => $frame) {
-                          echo "<frame>\n";
-                          echo "<time>$ms</time>\n";
-                          echo "<image>$protocol://$host$uri/getfile.php?test=$id&amp;video=video_{$i}_cached&amp;file={$frame['file']}</image>\n";
-                          echo "<VisuallyComplete>{$frame['progress']}</VisuallyComplete>\n";
-                          echo "</frame>\n";
-                      }
-                      echo "</videoFrames>\n";
-                    }
-                    
-                    xmlDomains($id, $testPath, $i, 1);
-                    xmlBreakdown($id, $testPath, $i, 1);
-                    if (array_key_exists('requests', $_REQUEST) && $_REQUEST['requests'] != 'median')
-                      xmlRequests($id, $testPath, $i, 1);
-                    StatusMessages($id, $testPath, $i, 1);
-                    ConsoleLog($id, $testPath, $i, 1);
+                    $xmlGenerator->printRun($i, true);
                     echo "</repeatView>\n";
                 }
             }
@@ -475,146 +296,6 @@ else
         }
 
         echo "</response>\n";
-    }
-}
-
-/**
-* Dump a breakdown of the requests and bytes by domain
-*/
-function xmlDomains($id, $testPath, $run, $cached) {
-    if (array_key_exists('domains', $_REQUEST) && $_REQUEST['domains']) {
-        echo "<domains>\n";
-        $requests;
-        $breakdown = getDomainBreakdown($id, $testPath, $run, $cached, $requests);
-        foreach ($breakdown as $domain => &$values) {
-            $domain = $domain;
-            echo "<domain host=\"" . xml_entities($domain) . "\">\n";
-            echo "<requests>{$values['requests']}</requests>\n";
-            echo "<bytes>{$values['bytes']}</bytes>\n";
-            echo "<connections>{$values['connections']}</connections>\n";
-            if (isset($values['cdn_provider']))
-              echo "<cdn_provider>{$values['cdn_provider']}</cdn_provider>\n";
-            echo "</domain>\n";
-        }
-        echo "</domains>\n";
-    }
-}
-
-/**
-* Dump a breakdown of the requests and bytes by mime type
-*/
-function xmlBreakdown($id, $testPath, $run, $cached) {
-    if (array_key_exists('breakdown', $_REQUEST) && $_REQUEST['breakdown']) {
-        echo "<breakdown>\n";
-        $requests;
-        $breakdown = getBreakdown($id, $testPath, $run, $cached, $requests);
-        foreach ($breakdown as $mime => &$values) {
-            $domain = strrev($domain);
-            echo "<$mime>\n";
-            echo "<requests>{$values['requests']}</requests>\n";
-            echo "<bytes>{$values['bytes']}</bytes>\n";
-            echo "</$mime>\n";
-        }
-        echo "</breakdown>\n";
-    }
-}
-
-
-/**
-* Dump information about all of the requests
-*/
-function xmlRequests($id, $testPath, $run, $cached) {
-    if (array_key_exists('requests', $_REQUEST) && $_REQUEST['requests']) {
-        echo "<requests>\n";
-        $secure = false;
-        $haveLocations = false;
-        $requests = getRequests($id, $testPath, $run, $cached, $secure, $haveLocations, false, true);
-        foreach ($requests as &$request) {
-            echo "<request number=\"{$request['number']}\">\n";
-            foreach ($request as $field => $value) {
-                if (!is_array($value))
-                  echo "<$field>" . xml_entities($value) . "</$field>\n";
-            }
-            if (array_key_exists('headers', $request) && is_array($request['headers'])) {
-              echo "<headers>\n";
-              if (array_key_exists('request', $request['headers']) && is_array($request['headers']['request'])) {
-                echo "<request>\n";
-                foreach ($request['headers']['request'] as $value)
-                  echo "<header>" . xml_entities($value) . "</header>\n";
-                echo "</request>\n";
-              }
-              if (array_key_exists('response', $request['headers']) && is_array($request['headers']['response'])) {
-                echo "<response>\n";
-                foreach ($request['headers']['response'] as $value)
-                  echo "<header>" . xml_entities($value) . "</header>\n";
-                echo "</response>\n";
-              }
-              echo "</headers>\n";
-            }
-            echo "</request>\n";
-        }
-        echo "</requests>\n";
-    }
-}
-
-/**
-* Dump any logged browser status messages
-* 
-* @param mixed $id
-* @param mixed $testPath
-* @param mixed $run
-* @param mixed $cached
-*/
-function StatusMessages($id, $testPath, $run, $cached) {
-    $cachedText = '';
-    if ($cached)
-        $cachedText = '_Cached';
-    $statusFile = "$testPath/$run{$cachedText}_status.txt";
-    if (gz_is_file($statusFile)) {
-        echo "<status>\n";
-        $messages = array();
-        $lines = gz_file($statusFile);
-        foreach($lines as $line) {
-            $line = trim($line);
-            if (strlen($line)) {
-                $parts = explode("\t", $line);
-                $time = xml_entities(trim($parts[0]));
-                $message = xml_entities(trim($parts[1]));
-                echo "<entry>\n";
-                echo "<time>$time</time>\n";
-                echo "<message>$message</message>\n";
-                echo "</entry>\n";
-            }
-        }
-        echo "</status>\n";
-    }
-}
-
-/**
-* Dump the console log if we have one
-* 
-* @param mixed $id
-* @param mixed $testPath
-* @param mixed $run
-* @param mixed $cached
-*/
-function ConsoleLog($id, $testPath, $run, $cached) {
-    if(isset($_GET['console']) && $_GET['console'] == 0) {
-        return;
-    }
-    $consoleLog = DevToolsGetConsoleLog($testPath, $run, $cached);
-    if (isset($consoleLog) && is_array($consoleLog) && count($consoleLog)) {
-        echo "<consoleLog>\n";
-        foreach( $consoleLog as &$entry ) {
-            echo "<entry>\n";
-            echo "<source>" . xml_entities($entry['source']) . "</source>\n";
-            echo "<level>" . xml_entities($entry['level']) . "</level>\n";
-            echo "<message>" . xml_entities($entry['text']) . "</message>\n";
-            echo "<url>" . xml_entities($entry['url']) . "</url>\n";
-            echo "<line>" . xml_entities($entry['line']) . "</line>\n";
-            echo "</entry>\n";
-        }
-        echo "</consoleLog>\n";
     }
 }
 
