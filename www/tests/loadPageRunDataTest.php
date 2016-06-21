0771b12f4f4a4fc8240197c00112f9b71c5c54ae
Stefan Burnicki <stefan.burnicki@iteratec.de>
2016-06-21 14:59:35 +0200

    tests: First test for devtools

--- www/tests/loadPageRunDataTest.php
+++ www/tests/loadPageRunDataTest.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../common_lib.inc';
 require_once __DIR__ . '/../page_data.inc';
+require_once __DIR__ . '/TestUtil.php';
 
 require __DIR__ . '/data/singlestepPageRunData.inc.php';
 
@@ -14,59 +15,23 @@ class LoadPageRunDataTest extends PHPUnit_Framework_TestCase {
   public function setUp() {
     global $SINGLESTEP_PAGE_RUN_DATA;
     $this->expectedData = $SINGLESTEP_PAGE_RUN_DATA;
-    $this->extractPath = $this->extractToTemp(__DIR__ . "/data/singlestepResults.zip");
+    $this->extractPath = TestUtil::extractToTemp(__DIR__ . "/data/singlestepResults.zip");
+    if (!$this->extractPath) {
+      $this->fail("Failed to extract results to temp dir");
+    }
     $this->resultPath = $this->extractPath . "/singlestepResults";
     date_default_timezone_set("UTC"); // to make the test consistent with the result
   }
 
   public function tearDown() {
     if (is_dir($this->extractPath)) {
-      //$this->removeDirRecursive($this->extractPath);
+      TestUtil::removeDirRecursive($this->extractPath);
     }
   }
-
-  /*
-   * Tests
-   */
 
   public function testLoadPageRunData() {
     echo $this->resultPath . "\n";
     $pageRunData = loadPageRunData($this->resultPath, 1, 0);
     $this->assertEquals($this->expectedData, $pageRunData);
-  }
-
-
-  /*
-   * Helper methods
-   */
-
-  private function extractToTemp($zipFile) {
-    $zipFile = realpath($zipFile);
-    $zip = new ZipArchive();
-    $destPath = $this->getTempPath("wptTest_");
-    if ($zip->open($zipFile) !== TRUE) {
-      $this->fail("Couldn't extract $zipFile to $destPath");
-    }
-    $zip->extractTo($destPath);
-    $zip->close();
-    return $destPath;
-  }
-
-  private function getTempPath($prefix) {
-    $path = tempnam(null, $prefix);
-    if (is_file($path)) {
-      unlink($path);
-    }
-    mkdir($path);
-    return $path;
-  }
-
-  private function removeDirRecursive($path) {
-    $files = glob($path . '/*');
-    foreach ($files as $file) {
-      is_dir($file) ? $this->removeDirRecursive($file) : unlink($file);
-    }
-    rmdir($path);
-    return;
   }
 }
