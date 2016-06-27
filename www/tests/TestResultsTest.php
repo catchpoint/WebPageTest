9936e6258042cacca8ff0cbde549fac149a82b9e
Stefan Burnicki <stefan.burnicki@iteratec.de>
2016-06-27 13:54:22 +0200

    tests: More TestResults tests

--- www/tests/TestResultsTest.php
+++ www/tests/TestResultsTest.php
@@ -15,7 +15,7 @@ class TestResultsTest extends PHPUnit_Framework_TestCase {
     $this->assertEquals(3, $results->getMedianRunNumber("loadTime", false));
     $this->assertEquals(1, $results->getMedianRunNumber("TTFB", false));
     $this->assertEquals(4, $results->getMedianRunNumber("loadTime", true));
-    $this->assertEquals(3, $results->getMedianRunNumber("TTFB", true));
+    $this->assertEquals(4, $results->getMedianRunNumber("TTFB", true));
   }
 
   public function testGetFirstViewAverage() {
@@ -26,16 +26,35 @@ class TestResultsTest extends PHPUnit_Framework_TestCase {
     $this->assertEquals(3, $fvAverages["avgRun"]);
   }
 
+  public function testGetRepeatViewAverage() {
+    $results = $this->testResultsFromPageData();
+    $fvAverages = $results->getRepeatViewAverage();
+    $this->assertEquals(400, $fvAverages["TTFB"]);
+    $this->assertEquals(4000, $fvAverages["loadTime"]);
+    $this->assertEquals(4, $fvAverages["avgRun"]);
+  }
+
+  public function testGetStandardDeviation() {
+    $results = $this->testResultsFromPageData();
+
+    $this->assertEquals(2160, $results->getStandardDeviation("loadTime", false));
+    $this->assertEquals(163, $results->getStandardDeviation("TTFB", false));
+    $this->assertEquals(2160, $results->getStandardDeviation("loadTime", true));
+    $this->assertEquals(81, $results->getStandardDeviation("TTFB", true));
+  }
+
   public function testResultsFromPageData() {
     $run1 = array('result' => 0, 'TTFB' => 300, 'loadTime' => 6000);
     $run2 = array('result' => 404, 'TTFB' => 200, 'loadTime' => 3000);
     $run3 = array('result' => 0, 'TTFB' => 100, 'loadTime' => 2000);
     $run4 = array('result' => 99999, 'TTFB' => 500, 'loadTime' => 1000);
+    $run5 = array('result' => 0, 'TTFB' => 400, 'loadTime' => 5000);
+
     $pageData = array(
       1 => array($run1), // missing repeat view
       2 => array($run2, $run4), // failed first view
       3 => array($run3, $run1),
-      4 => array($run4, $run3)
+      4 => array($run4, $run5)
     );
     foreach (array_keys($pageData) as $key) {
       $pageData[$key][0]["cached"] = 0;
