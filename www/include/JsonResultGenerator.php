3bd040d55c3f70e1ccefcca309b064d441c372ef
Stefan Burnicki <stefan.burnicki@iteratec.de>
2016-06-29 11:06:44 +0200

    tests: Regression test for JSON result array generation (red)

---
+++ www/include/JsonResultGenerator.php
@@ -0,0 +1,11 @@
+<?php
+
+class JsonResultGenerator {
+
+  public function __construct($testInfo, $baseUrl, $fileHandler) {
+  }
+
+  public function resultArray($testResults, $medianMetric = "loadTime") {
+
+  }
+}
