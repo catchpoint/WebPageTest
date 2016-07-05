da1db82dadfd2a2519e566df5cc5b05fc30b6fe0
Stefan Burnicki <stefan.burnicki@iteratec.de>
2016-07-05 15:29:34 +0200

    Refactor: Use early return to save indention

--- www/include/JsonResultGenerator.php
+++ www/include/JsonResultGenerator.php
@@ -159,114 +159,114 @@ class JsonResultGenerator {
    */
   private function GetSingleRunData($id, $testPath, $run, $cached, &$pageData, $testInfo) {
     $ret = null;
-    if (array_key_exists($run, $pageData) &&
-      is_array($pageData[$run]) &&
-      array_key_exists($cached, $pageData[$run]) &&
-      is_array($pageData[$run][$cached])
+    if (!array_key_exists($run, $pageData) ||
+      !is_array($pageData[$run]) ||
+      !array_key_exists($cached, $pageData[$run]) ||
+      !is_array($pageData[$run][$cached])
     ) {
-      $ret = $pageData[$run][$cached];
-      $ret['run'] = $run;
-      $localPaths = new TestPaths($testPath, $run, $cached);
-      $nameOnlyPaths = new TestPaths("", $run, $cached);
-      $urlGenerator = UrlGenerator::create(false, $this->urlStart, $this->testInfo->getId(), $run, $cached);
-      $friendlyUrlGenerator = UrlGenerator::create(true, $this->urlStart, $this->testInfo->getId(), $run, $cached);
-      $urlPaths = new TestPaths($this->urlStart . substr($testPath, 1), $run, $cached, 1);
+      return null;
+    }
+    $ret = $pageData[$run][$cached];
+    $ret['run'] = $run;
+    $localPaths = new TestPaths($testPath, $run, $cached);
+    $nameOnlyPaths = new TestPaths("", $run, $cached);
+    $urlGenerator = UrlGenerator::create(false, $this->urlStart, $this->testInfo->getId(), $run, $cached);
+    $friendlyUrlGenerator = UrlGenerator::create(true, $this->urlStart, $this->testInfo->getId(), $run, $cached);
+    $urlPaths = new TestPaths($this->urlStart . substr($testPath, 1), $run, $cached, 1);
 
-      if (isset($testInfo)) {
-        if (array_key_exists('tester', $testInfo))
-          $ret['tester'] = $testInfo['tester'];
-        if (array_key_exists('test_runs', $testInfo) &&
-          array_key_exists($run, $testInfo['test_runs']) &&
-          array_key_exists('tester', $testInfo['test_runs'][$run])
-        )
-          $ret['tester'] = $testInfo['test_runs'][$run]['tester'];
-      }
+    if (isset($testInfo)) {
+      if (array_key_exists('tester', $testInfo))
+        $ret['tester'] = $testInfo['tester'];
+      if (array_key_exists('test_runs', $testInfo) &&
+        array_key_exists($run, $testInfo['test_runs']) &&
+        array_key_exists('tester', $testInfo['test_runs'][$run])
+      )
+        $ret['tester'] = $testInfo['test_runs'][$run]['tester'];
+    }
 
-      $basic_results = $this->hasInfoFlag(self::BASIC_INFO_ONLY);
+    $basic_results = $this->hasInfoFlag(self::BASIC_INFO_ONLY);
 
-      if (!$basic_results && $this->fileHandler->gzFileExists($localPaths->pageSpeedFile())) {
-        $ret['PageSpeedScore'] = GetPageSpeedScore($localPaths->pageSpeedFile());
-        $ret['PageSpeedData'] = $urlGenerator->getGZip($nameOnlyPaths->pageSpeedFile());
-      }
+    if (!$basic_results && $this->fileHandler->gzFileExists($localPaths->pageSpeedFile())) {
+      $ret['PageSpeedScore'] = GetPageSpeedScore($localPaths->pageSpeedFile());
+      $ret['PageSpeedData'] = $urlGenerator->getGZip($nameOnlyPaths->pageSpeedFile());
+    }
 
-      $ret['pages'] = array();
-      $ret['pages']['details'] = $urlGenerator->resultPage("details");
-      $ret['pages']['checklist'] = $urlGenerator->resultPage("performance_optimization");
-      $ret['pages']['breakdown'] = $urlGenerator->resultPage("breakdown");
-      $ret['pages']['domains'] = $urlGenerator->resultPage("domains");
-      $ret['pages']['screenShot'] = $urlGenerator->resultPage("screen_shot");
+    $ret['pages'] = array();
+    $ret['pages']['details'] = $urlGenerator->resultPage("details");
+    $ret['pages']['checklist'] = $urlGenerator->resultPage("performance_optimization");
+    $ret['pages']['breakdown'] = $urlGenerator->resultPage("breakdown");
+    $ret['pages']['domains'] = $urlGenerator->resultPage("domains");
+    $ret['pages']['screenShot'] = $urlGenerator->resultPage("screen_shot");
 
-      $ret['thumbnails'] = array();
-      $ret['thumbnails']['waterfall'] = $friendlyUrlGenerator->thumbnail("waterfall.png");
-      $ret['thumbnails']['checklist'] = $friendlyUrlGenerator->thumbnail("optimization.png");
-      $ret['thumbnails']['screenShot'] = $friendlyUrlGenerator->thumbnail("screen.png");
+    $ret['thumbnails'] = array();
+    $ret['thumbnails']['waterfall'] = $friendlyUrlGenerator->thumbnail("waterfall.png");
+    $ret['thumbnails']['checklist'] = $friendlyUrlGenerator->thumbnail("optimization.png");
+    $ret['thumbnails']['screenShot'] = $friendlyUrlGenerator->thumbnail("screen.png");
 
-      $ret['images'] = array();
-      $ret['images']['waterfall'] = $friendlyUrlGenerator->generatedImage("waterfall");
-      $ret['images']['connectionView'] = $friendlyUrlGenerator->generatedImage("connection");
-      $ret['images']['checklist'] = $friendlyUrlGenerator->generatedImage("optimization");
-      $ret['images']['screenShot'] = $urlGenerator->getFile($nameOnlyPaths->screenShotFile());
-      if ($this->fileHandler->fileExists($localPaths->screenShotPngFile())) {
-        $ret['images']['screenShotPng'] = $urlGenerator->getFile($nameOnlyPaths->screenShotPngFile());
-      }
+    $ret['images'] = array();
+    $ret['images']['waterfall'] = $friendlyUrlGenerator->generatedImage("waterfall");
+    $ret['images']['connectionView'] = $friendlyUrlGenerator->generatedImage("connection");
+    $ret['images']['checklist'] = $friendlyUrlGenerator->generatedImage("optimization");
+    $ret['images']['screenShot'] = $urlGenerator->getFile($nameOnlyPaths->screenShotFile());
+    if ($this->fileHandler->fileExists($localPaths->screenShotPngFile())) {
+      $ret['images']['screenShotPng'] = $urlGenerator->getFile($nameOnlyPaths->screenShotPngFile());
+    }
 
-      $ret['rawData'] = array();
-      $ret['rawData']['headers'] = $urlPaths->headersFile();
-      $ret['rawData']['pageData'] = $urlPaths->pageDataFile();
-      $ret['rawData']['requestsData'] = $urlPaths->requestDataFile();
-      $ret['rawData']['utilization'] = $urlPaths->utilizationFile();
-      if ($this->fileHandler->fileExists($localPaths->bodiesFile())) {
-        $ret['rawData']['bodies'] = $urlPaths->bodiesFile();
-      }
-      if ($this->fileHandler->gzFileExists($localPaths->devtoolsTraceFile())) {
-        $ret['rawData']['trace'] = $urlGenerator->getGZip($nameOnlyPaths->devtoolsTraceFile() . ".gz");
-      }
+    $ret['rawData'] = array();
+    $ret['rawData']['headers'] = $urlPaths->headersFile();
+    $ret['rawData']['pageData'] = $urlPaths->pageDataFile();
+    $ret['rawData']['requestsData'] = $urlPaths->requestDataFile();
+    $ret['rawData']['utilization'] = $urlPaths->utilizationFile();
+    if ($this->fileHandler->fileExists($localPaths->bodiesFile())) {
+      $ret['rawData']['bodies'] = $urlPaths->bodiesFile();
+    }
+    if ($this->fileHandler->gzFileExists($localPaths->devtoolsTraceFile())) {
+      $ret['rawData']['trace'] = $urlGenerator->getGZip($nameOnlyPaths->devtoolsTraceFile() . ".gz");
+    }
 
-      if (!$basic_results) {
-        $startOffset = array_key_exists('testStartOffset', $ret) ? intval(round($ret['testStartOffset'])) : 0;
-        $progress = GetVisualProgress($testPath, $run, $cached, null, null, $startOffset);
-        if (array_key_exists('frames', $progress) && is_array($progress['frames']) && count($progress['frames'])) {
-          $ret['videoFrames'] = array();
-          foreach ($progress['frames'] as $ms => $frame) {
-            $videoFrame = array('time' => $ms);
-            $videoFrame['image'] = $urlGenerator->getFile($frame['file'], $nameOnlyPaths->videoDir());
-            $videoFrame['VisuallyComplete'] = $frame['progress'];
-            $ret['videoFrames'][] = $videoFrame;
-          }
+    if (!$basic_results) {
+      $startOffset = array_key_exists('testStartOffset', $ret) ? intval(round($ret['testStartOffset'])) : 0;
+      $progress = GetVisualProgress($testPath, $run, $cached, null, null, $startOffset);
+      if (array_key_exists('frames', $progress) && is_array($progress['frames']) && count($progress['frames'])) {
+        $ret['videoFrames'] = array();
+        foreach ($progress['frames'] as $ms => $frame) {
+          $videoFrame = array('time' => $ms);
+          $videoFrame['image'] = $urlGenerator->getFile($frame['file'], $nameOnlyPaths->videoDir());
+          $videoFrame['VisuallyComplete'] = $frame['progress'];
+          $ret['videoFrames'][] = $videoFrame;
         }
+      }
 
-        $requests = getRequests($id, $testPath, $run, $cached, $secure, $haveLocations, false, true);
-        $ret['domains'] = getDomainBreakdown($id, $testPath, $run, $cached, $requests);
-        $ret['breakdown'] = getBreakdown($id, $testPath, $run, $cached, $requests);
+      $requests = getRequests($id, $testPath, $run, $cached, $secure, $haveLocations, false, true);
+      $ret['domains'] = getDomainBreakdown($id, $testPath, $run, $cached, $requests);
+      $ret['breakdown'] = getBreakdown($id, $testPath, $run, $cached, $requests);
 
-        // add requests
-        if (!$this->hasInfoFlag(self::WITHOUT_REQUESTS)) {
-          $ret['requests'] = $requests;
+      // add requests
+      if (!$this->hasInfoFlag(self::WITHOUT_REQUESTS)) {
+        $ret['requests'] = $requests;
+      }
+
+      // Check to see if we're adding the console log
+      if (!$this->hasInfoFlag(self::WITHOUT_CONSOLE)) {
+        $console_log = DevToolsGetConsoleLog($testPath, $run, $cached);
+        if (isset($console_log)) {
+          $ret['consoleLog'] = $console_log;
         }
+      }
 
-        // Check to see if we're adding the console log
-        if (!$this->hasInfoFlag(self::WITHOUT_CONSOLE)) {
-          $console_log = DevToolsGetConsoleLog($testPath, $run, $cached);
-          if (isset($console_log)) {
-            $ret['consoleLog'] = $console_log;
-          }
-        }
-
-        if ($this->fileHandler->gzFileExists($localPaths->statusFile())) {
-          $ret['status'] = array();
-          $lines = $this->fileHandler->gzReadFile($localPaths->statusFile());
-          foreach ($lines as $line) {
-            $line = trim($line);
-            if (strlen($line)) {
-              list($time, $message) = explode("\t", $line);
-              if (strlen($time) && strlen($message))
-                $ret['status'][] = array('time' => $time, 'message' => $message);
-            }
+      if ($this->fileHandler->gzFileExists($localPaths->statusFile())) {
+        $ret['status'] = array();
+        $lines = $this->fileHandler->gzReadFile($localPaths->statusFile());
+        foreach ($lines as $line) {
+          $line = trim($line);
+          if (strlen($line)) {
+            list($time, $message) = explode("\t", $line);
+            if (strlen($time) && strlen($message))
+              $ret['status'][] = array('time' => $time, 'message' => $message);
           }
         }
       }
     }
-
     return $ret;
   }
 
