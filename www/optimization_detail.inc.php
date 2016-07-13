0ae5b7db21d2688e1ea5b856da18fce0350beb26
Stefan Burnicki <stefan.burnicki@iteratec.de>
2016-07-13 09:33:04 +0200

    whitespace: Fixed indention (0 code lines changed)

--- www/optimization_detail.inc.php
+++ www/optimization_detail.inc.php
@@ -36,97 +36,97 @@ function getOptimizationGrades(&$pageData, &$test, $id, $run)
  */
 function createGradeArray($scores) {
 
-        $opt = array();
+  $opt = array();
 
-        // put them in rank-order
-        $opt['ttfb'] = array();
-        $opt['keep-alive'] = array();
-        $opt['gzip'] = array();
-        $opt['image_compression'] = array();
-        $opt['caching'] = array();
-        $opt['combine'] = array();
-        $opt['cdn'] = array();
-        $opt['cookies'] = array();
-        $opt['minify'] = array();
-        $opt['e-tags'] = array();
+  // put them in rank-order
+  $opt['ttfb'] = array();
+  $opt['keep-alive'] = array();
+  $opt['gzip'] = array();
+  $opt['image_compression'] = array();
+  $opt['caching'] = array();
+  $opt['combine'] = array();
+  $opt['cdn'] = array();
+  $opt['cookies'] = array();
+  $opt['minify'] = array();
+  $opt['e-tags'] = array();
 
-        foreach (array_keys($opt) as $scoreName) {
-          if (array_key_exists($scoreName, $scores) && $scores[$scoreName] >= 0) {
-              $opt[$scoreName]['score'] = $scores[$scoreName];
-          }
-        }
-        if (array_key_exists('progressive_jpeg', $scores) && $scores['progressive_jpeg'] >= 0) {
-          $opt['progressive_jpeg'] = array(
-            'score' => $scores['progressive_jpeg'],
-            'label' => 'Progressive JPEGs',
-            'important' => true
-          );
-        }
+  foreach (array_keys($opt) as $scoreName) {
+    if (array_key_exists($scoreName, $scores) && $scores[$scoreName] >= 0) {
+      $opt[$scoreName]['score'] = $scores[$scoreName];
+    }
+  }
+  if (array_key_exists('progressive_jpeg', $scores) && $scores['progressive_jpeg'] >= 0) {
+    $opt['progressive_jpeg'] = array(
+      'score' => $scores['progressive_jpeg'],
+      'label' => 'Progressive JPEGs',
+      'important' => true
+    );
+  }
 
-        // define the labels for all  of them
-        $opt['ttfb']['label'] = 'First Byte Time';
-        $opt['keep-alive']['label'] = 'Keep-alive Enabled';
-        $opt['gzip']['label'] = 'Compress Transfer';
-        $opt['image_compression']['label'] = 'Compress Images';
-        $opt['caching']['label'] = 'Cache static content';
-        $opt['combine']['label'] = 'Combine js and css files';
-        $opt['cdn']['label'] = 'Effective use of CDN';
-        $opt['cookies']['label'] = 'No cookies on static content';
-        $opt['minify']['label'] = 'Minify javascript';
-        $opt['e-tags']['label'] = 'Disable E-Tags';
+  // define the labels for all  of them
+  $opt['ttfb']['label'] = 'First Byte Time';
+  $opt['keep-alive']['label'] = 'Keep-alive Enabled';
+  $opt['gzip']['label'] = 'Compress Transfer';
+  $opt['image_compression']['label'] = 'Compress Images';
+  $opt['caching']['label'] = 'Cache static content';
+  $opt['combine']['label'] = 'Combine js and css files';
+  $opt['cdn']['label'] = 'Effective use of CDN';
+  $opt['cookies']['label'] = 'No cookies on static content';
+  $opt['minify']['label'] = 'Minify javascript';
+  $opt['e-tags']['label'] = 'Disable E-Tags';
 
-        // flag the important ones
-        $opt['ttfb']['important'] = true;
-        $opt['keep-alive']['important'] = true;
-        $opt['gzip']['important'] = true;
-        $opt['image_compression']['important'] = true;
-        $opt['caching']['important'] = true;
-        $opt['cdn']['important'] = true;
+  // flag the important ones
+  $opt['ttfb']['important'] = true;
+  $opt['keep-alive']['important'] = true;
+  $opt['gzip']['important'] = true;
+  $opt['image_compression']['important'] = true;
+  $opt['caching']['important'] = true;
+  $opt['cdn']['important'] = true;
 
-        // apply grades
-        foreach( $opt as $check => &$item )
-        {
-            $grade = 'N/A';
-            $weight = 0;
-            if( $check == 'cdn' )
-            {
-                if( $item['score'] >= 80 )
-                {
-                    $item['grade'] = "<img src=\"{$GLOBALS['cdnPath']}/images/grade_check.png\" alt=\"yes\">";
-                    $item['class'] = 'A';
-                }
-                else
-                {
-                    $item['grade'] = 'X';
-                    $item['class'] = 'NA';
-                }
-            }
-            else
-            {
-                if( isset($item['score']) )
-                {
-                    $weight = 100;
-                    if( $item['score'] >= 90 )
-                        $grade = 'A';
-                    elseif( $item['score'] >= 80 )
-                        $grade = 'B';
-                    elseif( $item['score'] >= 70 )
-                        $grade = 'C';
-                    elseif( $item['score'] >= 60 )
-                        $grade = 'D';
-                    elseif( $item['score'] >= 0 )
-                        $grade = 'F';
-                    else
-                        $weight = 0;
-                }
-                $item['grade'] = $grade;
-                if( $grade == "N/A" )
-                    $item['class'] = "NA";
-                else
-                    $item['class'] = $grade;
-            }
-            $item['weight'] = $weight;
-        }
+  // apply grades
+  foreach( $opt as $check => &$item )
+  {
+    $grade = 'N/A';
+    $weight = 0;
+    if( $check == 'cdn' )
+    {
+      if( $item['score'] >= 80 )
+      {
+        $item['grade'] = "<img src=\"{$GLOBALS['cdnPath']}/images/grade_check.png\" alt=\"yes\">";
+        $item['class'] = 'A';
+      }
+      else
+      {
+        $item['grade'] = 'X';
+        $item['class'] = 'NA';
+      }
+    }
+    else
+    {
+      if( isset($item['score']) )
+      {
+        $weight = 100;
+        if( $item['score'] >= 90 )
+          $grade = 'A';
+        elseif( $item['score'] >= 80 )
+          $grade = 'B';
+        elseif( $item['score'] >= 70 )
+          $grade = 'C';
+        elseif( $item['score'] >= 60 )
+          $grade = 'D';
+        elseif( $item['score'] >= 0 )
+          $grade = 'F';
+        else
+          $weight = 0;
+      }
+      $item['grade'] = $grade;
+      if( $grade == "N/A" )
+        $item['class'] = "NA";
+      else
+        $item['class'] = $grade;
+    }
+    $item['weight'] = $weight;
+  }
 
     return $opt;
 }
@@ -139,11 +139,11 @@ function createGradeArray($scores) {
 */
 function gradeTTFB(&$pageData, &$test, $id, $run, $cached, &$target)
 {
-    $ttfb = (int)$pageData['TTFB'];
-    $latency = isset($test['testinfo']['latency']) ? $test['testinfo']['latency'] : null;
-    $testPath = './' . GetTestPath($id);
-    $localPaths = new TestPaths($testPath, $id, $run, $cached, 1);
-    return gradeTTFBForStep($ttfb, $latency, $localPaths, $target);
+  $ttfb = (int)$pageData['TTFB'];
+  $latency = isset($test['testinfo']['latency']) ? $test['testinfo']['latency'] : null;
+  $testPath = './' . GetTestPath($id);
+  $localPaths = new TestPaths($testPath, $id, $run, $cached, 1);
+  return gradeTTFBForStep($ttfb, $latency, $localPaths, $target);
 }
 
 /**
@@ -154,28 +154,28 @@ function gradeTTFB(&$pageData, &$test, $id, $run, $cached, &$target)
  * @return int|null The TTFB score or null, if it can't get determined
  */
 function gradeTTFBForStep($ttfb, $latency, $localPaths, &$target) {
-    $score = null;
-    if( $ttfb )
+  $score = null;
+  if( $ttfb )
+  {
+    // see if we can fast-path fail this test without loading the object data
+    if( isset($latency) )
     {
-        // see if we can fast-path fail this test without loading the object data
-        if( isset($latency) )
-        {
-            $rtt = (int)$latency + 100;
-            $worstCase = $rtt * 7 + 1000;  // 7 round trips including dns, socket, request and ssl + 1 second back-end
-            if( $ttfb > $worstCase )
-                $score = 0;
-        } else {
-            $latency = 0;
-        }
-
-        if( !isset($score) )
-        {
-            $target = getTargetTTFBForStep($localPaths, $latency);
-            $score = (int)min(max(100 - (($ttfb - $target) / 10), 0), 100);
-        }
+      $rtt = (int)$latency + 100;
+      $worstCase = $rtt * 7 + 1000;  // 7 round trips including dns, socket, request and ssl + 1 second back-end
+      if( $ttfb > $worstCase )
+        $score = 0;
+    } else {
+      $latency = 0;
     }
 
-    return $score;
+    if( !isset($score) )
+    {
+      $target = getTargetTTFBForStep($localPaths, $latency);
+      $score = (int)min(max(100 - (($ttfb - $target) / 10), 0), 100);
+    }
+  }
+
+  return $score;
 }
 
 /**
@@ -185,50 +185,50 @@ function gradeTTFBForStep($ttfb, $latency, $localPaths, &$target) {
  * @return int|null The target TTFB or null if it can't get determined
  */
 function getTargetTTFBForStep($localPaths, $rtt) {
-    $target = NULL;
+  $target = NULL;
 
-    // load the object data (unavoidable, we need the socket connect time to the first host)
-    require_once('object_detail.inc');
+  // load the object data (unavoidable, we need the socket connect time to the first host)
+  require_once('object_detail.inc');
 
-    $secure = false;
-    $requests = getRequestsForStep($localPaths, null, $secure, $haveLocations, false);
-    if( count($requests) )
-    {
-        // figure out what the RTT is to the server (take the connect time from the first request unless it is over 3 seconds)
-        if (isset($requests[0]['connect_start']) &&
-            $requests[0]['connect_start'] >= 0 &&
-            isset($requests[0]['connect_end']) &&
-            $requests[0]['connect_end'] > $requests[0]['connect_start']) {
-          $rtt = $requests[0]['connect_end'] - $requests[0]['connect_start'];
-        } else {
-          $connect_ms = $requests[0]['connect_ms'];
-          if ($rtt > 0 && (!isset($connect_ms) || $connect_ms > 3000 || $connect_ms < 0))
-            $rtt += 100;    // allow for an additional 100ms to reach the server on top of the traffic-shaped RTT
-          else
-            $rtt = $connect_ms;
-        }
-        
-        // allow for a minimum of 100ms for the RTT
-        $rtt = max($rtt, 100);
-        
-        $ssl_ms = 0;
-        $i = 0;
-        while (isset($requests[$i])) {
-          if (isset($requests[$i]['contentType']) &&
-              (stripos($requests[$i]['contentType'], 'ocsp') !== false ||
-               stripos($requests[$i]['contentType'], 'crl') !== false)) {
-            $i++;
-          } else {
-            if ($requests[$i]['is_secure'])
-              $ssl_ms = $rtt;
-            break;
-          }
-        }
-
-        // RTT's: DNS + Socket Connect + HTTP Request + 100ms allowance
-        $target = ($rtt * 3) + $ssl_ms + 100;
+  $secure = false;
+  $requests = getRequestsForStep($localPaths, null, $secure, $haveLocations, false);
+  if( count($requests) )
+  {
+    // figure out what the RTT is to the server (take the connect time from the first request unless it is over 3 seconds)
+    if (isset($requests[0]['connect_start']) &&
+      $requests[0]['connect_start'] >= 0 &&
+      isset($requests[0]['connect_end']) &&
+      $requests[0]['connect_end'] > $requests[0]['connect_start']) {
+      $rtt = $requests[0]['connect_end'] - $requests[0]['connect_start'];
+    } else {
+      $connect_ms = $requests[0]['connect_ms'];
+      if ($rtt > 0 && (!isset($connect_ms) || $connect_ms > 3000 || $connect_ms < 0))
+        $rtt += 100;    // allow for an additional 100ms to reach the server on top of the traffic-shaped RTT
+      else
+        $rtt = $connect_ms;
     }
 
-    return $target;
+    // allow for a minimum of 100ms for the RTT
+    $rtt = max($rtt, 100);
+
+    $ssl_ms = 0;
+    $i = 0;
+    while (isset($requests[$i])) {
+      if (isset($requests[$i]['contentType']) &&
+        (stripos($requests[$i]['contentType'], 'ocsp') !== false ||
+          stripos($requests[$i]['contentType'], 'crl') !== false)) {
+        $i++;
+      } else {
+        if ($requests[$i]['is_secure'])
+          $ssl_ms = $rtt;
+        break;
+      }
+    }
+
+    // RTT's: DNS + Socket Connect + HTTP Request + 100ms allowance
+    $target = ($rtt * 3) + $ssl_ms + 100;
+  }
+
+  return $target;
 }
 ?>
