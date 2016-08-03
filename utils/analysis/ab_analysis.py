bec3dbc6ce0aafc373e070a9e1e4aa7e95c5e87d
Charlie Harrison <csharrison@chromium.org>
2016-08-02 21:38:41 -0400

    Add ParseTime back to ab_analysis.py

--- utils/analysis/ab_analysis.py
+++ utils/analysis/ab_analysis.py
@@ -34,6 +34,8 @@ from scipy import stats
 _METRICS = {
     'SpeedIndex': lambda x: sanitizeInt(x['SpeedIndex']),
     'TTFP': lambda x: sanitizeInt(x['render']),
+    'ParseTime': lambda x:
+        sanitizeInt(x['domInteractive'], x['domLoading']),
     'SpeedIndexAfterFirstByte':
         lambda x: sanitizeIntDiff(x['SpeedIndex'], x['TTFB'])
 }
