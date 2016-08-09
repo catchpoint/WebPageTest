ce5bcddd2d6681f2769a105245b88abbb83a9bd4
Stefan Burnicki <stefan.burnicki@iteratec.de>
2016-08-09 14:01:53 +0200

    refactor: Moved some parts for the breakdown html snippet in new base class for reuse

--- www/include/MimetypeBreakdownHtmlSnippet.php
+++ www/include/MimetypeBreakdownHtmlSnippet.php
@@ -1,9 +1,9 @@
 <?php
 
+require_once __DIR__ . '/BreakdownHtmlSnippet.php';
 require_once __DIR__ . '/ConnectionViewHtmlSnippet.php';
 
-class MimetypeBreakdownHtmlSnippet {
-  private $breakdownId;
+class MimetypeBreakdownHtmlSnippet extends BreakdownHtmlSnippet {
   private $connectionView;
   private $stepResult;
 
@@ -13,45 +13,19 @@ class MimetypeBreakdownHtmlSnippet {
    * @param TestStepResult $stepResult
    */
   public function __construct($testInfo, $stepResult) {
+    parent::__construct($stepResult);
     $this->stepResult = $stepResult;
-    $this->breakdownId = "breakdown_" . ($stepResult->isCachedRun() ? "rv" : "fv") . "_step_" . $stepResult->getStepNumber();
     $this->connectionView = new ConnectionViewHtmlSnippet($testInfo, $stepResult);
   }
 
   public function create() {
-    $out = $this->_createChartMarkup();
+    $out = $this->createChartMarkup();
     $out .= "<div style=\"text-align:center;\">\n";
     $out .= "<h3 name=\"connection\">Connection View</h3>\n";
     $out .= $this->connectionView->create();
     $out .= "</div>\n";
-    $out .= $this->_createJavascript();
+    $out .= $this->createJavascript("wptBreakdownData", $this->_getJSONBreakdown());
     return $out;
-  }
-
-  public function _createChartMarkup() {
-    $id = $this->breakdownId;
-    return <<<EOT
-      <div id="$id">
-      <table align="center" data-breakdown-id="$id" class="breakdownFrame">
-          <tr class="breakdownFramePies">
-              <td>
-                  <div class="pieRequests" style="width:450px; height:300px;"></div>
-              </td>
-              <td>
-                  <div class="pieBytes" style="width:450px; height:300px;"></div>
-              </td>
-          </tr>
-          <tr>
-              <td>
-                  <div class="tableRequests" style="width: 100%;"></div>
-              </td>
-              <td>
-                  <div class="tableBytes" style="width: 100%;"></div>
-              </td>
-          </tr>
-      </table>
-      </div>
-EOT;
   }
 
   private function _getJSONBreakdown() {
@@ -67,20 +41,5 @@ EOT;
       );
     }
     return json_encode($jsFriendly);
-  }
-
-
-  public function _createJavascript() {
-    $jsonData = $this->_getJSONBreakdown();
-    $id = $this->breakdownId;
-    $out = "<script type=\"text/javascript\">";
-    $out .= "if (typeof wptBreakdownData == 'undefined') var wptBreakdownData = {};";
-    $out .= "wptBreakdownData['$id'] = $jsonData;";
-    $out .= "</script>";
-    return $out;
-  }
-
-  public function getBreakdownId() {
-    return $this->breakdownId;
   }
 }
