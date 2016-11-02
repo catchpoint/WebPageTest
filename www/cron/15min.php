38be1ebc447de0b9a2f30e2d5e620c1a8fc1ab53
Patrick Meenan <pmeenan@chromium.org>
2016-11-02 18:55:12 -0400

    Added logging to the feed updates

--- www/cron/15min.php
+++ www/cron/15min.php
@@ -10,10 +10,16 @@ $lock = Lock("cron-15", false, 1200);
 if (!isset($lock))
   exit(0);
 
+header("Content-type: text/plain");
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+
 // Update the safe browsing black list
+echo "Updating SBL\n";
 SBL_Update();
 
 // update the feeds
+echo "Updating Feeds\n";
 require_once('updateFeeds.php');
 UpdateFeeds();
 ?>
