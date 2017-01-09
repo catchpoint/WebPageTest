b1bfaa33d10be15744c1be9bd4d8593bb87d6ce5
Patrick Meenan <pmeenan@chromium.org>
2017-01-09 15:22:39 -0500

    Allow API tests to explicitly request non-private even if they include auth.  Fixed #787

--- www/runtest.php
+++ www/runtest.php
@@ -381,6 +381,10 @@
                 $test['private'] = 1;
               }
             }
+            
+            // If API requests explicitly mark tests as not-private, allow it
+            if (($_SERVER['REQUEST_METHOD'] == 'GET' || $xml || $json) && isset($_REQUEST['private']) && !$_REQUEST['private'])
+              $test['private'] = 0;
 
             // default batch and API requests to a lower priority
             if( !isset($req_priority) )
