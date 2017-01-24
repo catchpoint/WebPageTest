fcaebc257bbf6f172d43eaadef40fd234fc13ab6
Patrick Meenan <patmeenan@gmail.com>
2017-01-24 10:00:16 -0500

    Added v8 callstats when capturing timelines (mobile)

--- agent/js/src/wd_server.js
+++ agent/js/src/wd_server.js
@@ -964,9 +964,9 @@ WebDriverServer.prototype.scheduleStartTracingIfRequested_ = function() {
       message.params.categories = '-*,' + message.params.categories;
     }
     if (1 === this.task_.timeline) {
-      message.params.categories = message.params.categories + ',toplevel,v8.execute,disabled-by-default-devtools.timeline.frame,devtools.timeline.frame';
+      message.params.categories = message.params.categories + ',toplevel,v8.execute,disabled-by-default-devtools.timeline.frame,devtools.timeline.frame,disabled-by-default-v8.runtime_stats';
       if (this.task_.timelineStackDepth) {
-        message.params.categories = message.params.categories + ',disabled-by-default-devtools.timeline.stack,devtools.timeline.stack';
+        message.params.categories = message.params.categories + ',disabled-by-default-v8.cpu_profiler,disabled-by-default-v8.runtime_stats_sampling';
       }
     }
     this.devToolsCommand_(message).then(function() {
