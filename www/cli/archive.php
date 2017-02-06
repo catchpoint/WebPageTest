15ed65e60c9dcac553ec97759ab6af9b9e50b65e
Patrick Meenan <pmeenan@chromium.org>
2017-02-06 09:28:44 -0500

    Added more logging to the archive logic

--- www/cli/archive.php
+++ www/cli/archive.php
@@ -54,18 +54,17 @@ if ((isset($archive_dir) && strlen($archive_dir)) ||
 
   // Archive the actual tests
   $years = scandir('./results');
-  foreach( $years as $year ) {
-    mkdir('./logs/archived', 0777, true);
+  foreach ($years as $year) {
     $yearDir = "./results/$year";
-    if( is_numeric($year) && is_dir($yearDir) && $year != '.' && $year != '..' ) {
+    if (is_numeric($year) && is_dir($yearDir) && $year != '.' && $year != '..') {
       $months = scandir($yearDir);
-      foreach( $months as $month ) {
+      foreach ($months as $month) {
         $monthDir = "$yearDir/$month";
-        if( is_dir($monthDir) && $month != '.' && $month != '..' ) {
+        if (is_dir($monthDir) && $month != '.' && $month != '..') {
           $days = scandir($monthDir);
           foreach( $days as $day ) {
             $dayDir = "$monthDir/$day";
-            if( is_dir($dayDir) && $day != '.' && $day != '..' ) {
+            if (is_dir($dayDir) && $day != '.' && $day != '..') {
               $elapsedDays = ElapsedDays($year, $month, $day);
               $forced_only = $elapsedDays < ($MIN_DAYS - 1);
               CheckDay($dayDir, "$year$month$day", $elapsedDays, $forced_only);
@@ -91,79 +90,62 @@ Unlock($lock);
 * 
 */
 function CheckRelay() {
-    $dirs = scandir('./results/relay');
-    $keys = parse_ini_file('./settings/keys.ini');
-    foreach($dirs as $key) {
-        if ($key != '.' && $key != '..') {
-            $keydir = "./results/relay/$key";
-            if (is_dir($keydir)) {
-                if (array_key_exists($key, $keys)) {
-                    echo "\rChecking relay tests for $key";
-                    $years = scandir($keydir);
-                    foreach( $years as $year ) {
-                        if ($year != '.' && $year != '..') {
-                            $yearDir = "$keydir/$year";
-                            if (is_numeric($year)) {
-                                if (ElapsedDays($year, '01', '01') < 10) {
-                                    $months = scandir($yearDir);
-                                    foreach( $months as $month ) {
-                                        if ($month != '.' && $month != '..') {
-                                            $monthDir = "$yearDir/$month";
-                                            if (is_numeric($month)) {
-                                                if (ElapsedDays($year, $month, '01') < 10) {
-                                                    $days = scandir($monthDir);
-                                                    foreach( $days as $day ) {
-                                                        if ($day != '.' && $day != '..') {
-                                                            $dayDir = "$monthDir/$day";
-                                                            if (is_numeric($day)) {
-                                                                if (ElapsedDays($year, $month, $day) >= 10) {
-                                                                    delTree($dayDir);
-                                                                }
-                                                            } else {
-                                                                if (is_file($dayDir)) {
-                                                                    unlink($dayDir);
-                                                                } else {
-                                                                    delTree($dayDir);
-                                                                }
-                                                            }
-                                                            @rmdir($dayDir);
-                                                        }
-                                                    }
-                                                } else {
-                                                    delTree($monthDir);
-                                                }
-                                            } else {
-                                                if (is_file($monthDir)) {
-                                                    unlink($monthDir);
-                                                } else {
-                                                    delTree($monthDir);
-                                                }
-                                            }
-                                            @rmdir($monthDir);
-                                        }
-                                    }
-                                } else {
-                                    delTree($yearDir);
-                                }
-                            } else {
-                                if (is_file($yearDir)) {
-                                    unlink($yearDir);
-                                } else {
-                                    delTree($yearDir);
-                                }
+  $dirs = scandir('./results/relay');
+  $keys = parse_ini_file('./settings/keys.ini');
+  foreach($dirs as $key) {
+    if ($key != '.' && $key != '..') {
+      $keydir = "./results/relay/$key";
+      if (is_dir($keydir)) {
+        if (array_key_exists($key, $keys)) {
+          echo "\rChecking relay tests for $key";
+          $years = scandir($keydir);
+          foreach( $years as $year ) {
+            if ($year != '.' && $year != '..') {
+              $yearDir = "$keydir/$year";
+              if (is_numeric($year)) {
+                if (ElapsedDays($year, '01', '01') < 10) {
+                  $months = scandir($yearDir);
+                  foreach( $months as $month ) {
+                    if ($month != '.' && $month != '..') {
+                      $monthDir = "$yearDir/$month";
+                      if (is_numeric($month)) {
+                        if (ElapsedDays($year, $month, '01') < 10) {
+                          $days = scandir($monthDir);
+                          foreach( $days as $day ) {
+                            if ($day != '.' && $day != '..') {
+                              $dayDir = "$monthDir/$day";
+                              if (!is_numeric($day))
+                                delTree($dayDir);
+                              @rmdir($dayDir);
                             }
-                            @rmdir($yearDir);
+                          }
+                        } else {
+                          delTree($monthDir);
                         }
+                      } else {
+                        delTree($monthDir);
+                      }
+                      @rmdir($monthDir);
                     }
+                  }
                 } else {
-                    delTree($keydir);
+                  delTree($yearDir);
                 }
-                @rmdir($keydir);
-            } else {
-                unlink($keydir);
+              } else {
+                delTree($yearDir);
+              }
+              @rmdir($yearDir);
             }
+          }
+        } else {
+          delTree($keydir);
         }
+        @rmdir($keydir);
+      } else {
+        unlink($keydir);
+      }
     }
+  }
 }
 
 /**
@@ -172,17 +154,17 @@ function CheckRelay() {
 * @param mixed $path
 */
 function CheckOldDir($path) {
-    $oldDirs = scandir($path);
-    foreach( $oldDirs as $oldDir ) {
-        if( $oldDir != '.' && $oldDir != '..' ) {
-            // see if it is a test or a higher-level directory
-            if( is_file("$path/$oldDir/testinfo.ini") )
-                CheckTest("$path/$oldDir", $oldDir, 1000);
-            else
-                CheckOldDir("$path/$oldDir");
-        }
+  $oldDirs = scandir($path);
+  foreach( $oldDirs as $oldDir ) {
+    if( $oldDir != '.' && $oldDir != '..' ) {
+      // see if it is a test or a higher-level directory
+      if( is_file("$path/$oldDir/testinfo.ini") )
+        CheckTest("$path/$oldDir", $oldDir, 1000);
+      else
+        CheckOldDir("$path/$oldDir");
     }
-    rmdir($path);
+  }
+  @rmdir($path);
 }
 
 /**
@@ -205,7 +187,7 @@ function CheckDay($dir, $baseID, $elapsedDays, $forced_only) {
               is_dir("$dir/$test/video_1")) {
             CheckTest("$dir/$test", "{$baseID}_$test", $elapsedDays, $forced_only);
           } else {
-            // check for bogus stray test directories
+            // We're likely looking at a shard directory, loop through the actual tests
             CheckDay("$dir/$test", "{$baseID}_$test", $elapsedDays, $forced_only);
           }
         }
@@ -228,7 +210,7 @@ function CheckTest($testPath, $id, $elapsedDays, $forced_only) {
   global $log;
   global $MIN_DAYS;
   global $MAX_DAYS;
-  $logLine = "$id : ";
+  $logLine = "$id ($elapsedDays): ";
 
   echo "\rArc:$archiveCount, Del:$deleted, Kept:$kept, Checking:" . str_pad($id,45);
 
@@ -247,10 +229,11 @@ function CheckTest($testPath, $id, $elapsedDays, $forced_only) {
     } elseif (!$forced_only) {
       $elapsed = TestLastAccessed($id);
       if (isset($elapsed)) {
-        if ($elapsed >= $MIN_DAYS)
+        $logLine .= "Last Accessed $elapsed days";
+        if ($elapsed >= $MIN_DAYS) {
           $needs_archive = true;
-        else
-          $logLine .= "Last Accessed $elapsed days";
+          $logLine .= " Archiving";
+        }
       } else {
         $delete = true;
       }
@@ -262,6 +245,7 @@ function CheckTest($testPath, $id, $elapsedDays, $forced_only) {
         $delete = true;
       } else if ($elapsed < 60) {
         $status = GetTestStatus($id, false);
+        $logLine .= " status {$status['statusCode']}";
         if ($status['statusCode'] >= 400 ||
             ($status['statusCode'] == 102 &&
              $status['remote'] &&
@@ -292,12 +276,12 @@ function CheckTest($testPath, $id, $elapsedDays, $forced_only) {
 * Calculate how many days have passed since the given day
 */
 function ElapsedDays($year, $month, $day) {
-    global $now;
-    global $UTC;
-    $date = DateTime::createFromFormat('ymd', "$year$month$day", $UTC);
-    $daytime = $date->getTimestamp();
-    $elapsed = max($now - $daytime, 0) / 86400;
-    return $elapsed;
+  global $now;
+  global $UTC;
+  $date = DateTime::createFromFormat('ymd', "$year$month$day", $UTC);
+  $daytime = $date->getTimestamp();
+  $elapsed = max($now - $daytime, 0) / 86400;
+  return $elapsed;
 }
 
 ?>
