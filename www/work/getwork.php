1ec1f6d93a128c49e927749ec4cd985e9df8e5b1
Patrick Meenan <pmeenan@chromium.org>
2017-02-21 18:52:00 -0500

    Cache the agent updates for 60 seconds

--- www/work/getwork.php
+++ www/work/getwork.php
@@ -44,8 +44,8 @@ if (isset($locations) && is_array($locations) && count($locations) &&
     (!array_key_exists('freedisk', $_GET) || (float)$_GET['freedisk'] > 0.1)) {
   shuffle($locations);
   $location = trim($locations[0]);
-  if (!$is_done && array_key_exists('reboot', $_GET))
-    $is_done = GetReboot();
+  //if (!$is_done && array_key_exists('reboot', $_GET))
+  //  $is_done = GetReboot();
   if (!$is_done && array_key_exists('ver', $_GET))
     $is_done = GetUpdate();
   foreach ($locations as $loc) {
@@ -356,42 +356,35 @@ function GetUpdate() {
     if( isset($_GET['software']) && strlen($_GET['software']) )
       $fileBase = trim($_GET['software']);
     
-    $updateDir = './work/update';
-    if( is_dir("$updateDir/$location") )
-      $updateDir = "$updateDir/$location";
-        
-    // see if we have any software updates
-    if (is_file("$updateDir/{$fileBase}update.ini") && is_file("$updateDir/{$fileBase}update.zip")) {
-      $update = parse_ini_file("$updateDir/{$fileBase}update.ini");
-
-      // Check for inequality allows both upgrade and quick downgrade
-      if ($update['ver'] && intval($update['ver']) !== intval($_GET['ver'])) {
-        header('Content-Type: application/zip');
-        header("Cache-Control: no-cache, must-revalidate");
-        header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
-
-        readfile_chunked("$updateDir/{$fileBase}update.zip");
-        $ret = true;
-      }
+    $update = null;
+    if (function_exists('apcu_fetch')) {
+      $update = apcu_fetch("update-$fileBase");
+      if ($update === FALSE)
+        unset($update);
     }
     
-    // Keep track of the number of times in a row we sent down an update
-    if (function_exists('apcu_fetch') && function_exists('apcu_store')) {
-      $updateCount = apcu_fetch("uc-$location-$tester");
-      if (!$updateCount)
-        $updateCount = 0;
-      $oldCount = $updateCount;
-      $updateCount = $ret ? $updateCount + 1 : 0;
-      if ($updateCount != $oldCount)
-        apcu_store("uc-$location-$tester", $updateCount, 3600);
-    } elseif (function_exists('apc_fetch') && function_exists('apc_store')) {
-      $updateCount = apc_fetch("uc-$location-$tester");
-      if (!$updateCount)
-        $updateCount = 0;
-      $oldCount = $updateCount;
-      $updateCount = $ret ? $updateCount + 1 : 0;
-      if ($updateCount != $oldCount)
-        apc_store("uc-$location-$tester", $updateCount, 3600);
+    if (!isset($update)) {
+      $updateDir = './work/update';
+      if (is_dir("$updateDir/$location"))
+        $updateDir = "$updateDir/$location";
+          
+      // see if we have any software updates
+      if (is_file("$updateDir/{$fileBase}update.ini") && is_file("$updateDir/{$fileBase}update.zip")) {
+        $update = parse_ini_file("$updateDir/{$fileBase}update.ini");
+
+        // Check for inequality allows both upgrade and quick downgrade
+        if ($update['ver'] && intval($update['ver']) !== intval($_GET['ver'])) {
+          header('Content-Type: application/zip');
+          header("Cache-Control: no-cache, must-revalidate");
+          header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+
+          readfile_chunked("$updateDir/{$fileBase}update.zip");
+          $ret = true;
+        }
+      }
+      if (isset($update) && function_exists('apcu_store')) {
+        apcu_store("update-$fileBase", $update, 60);
+      }
     }
   }
   
@@ -557,23 +550,6 @@ function GetReboot() {
     }
   }
 
-  // If we sent down more than 3 updates sequentially, reboot the tester
-  if (!$reboot && function_exists('apcu_fetch') && function_exists('apcu_store')) {
-    $updateCount = apcu_fetch("uc-$location-$tester");
-    if ($updateCount && $updateCount >= 3) {
-      $reboot = true;
-      $updateCount = 0;
-      apcu_store("uc-$location-$tester", $updateCount, 3600);
-    }
-  } elseif (!$reboot && function_exists('apc_fetch') && function_exists('apc_store')) {
-    $updateCount = apc_fetch("uc-$location-$tester");
-    if ($updateCount && $updateCount >= 3) {
-      $reboot = true;
-      $updateCount = 0;
-      apc_store("uc-$location-$tester", $updateCount, 3600);
-    }
-  }
-  
   if ($reboot) {
     header('Content-type: text/plain');
     header("Cache-Control: no-cache, must-revalidate");
