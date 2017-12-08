5aff6054532bb6d42e6876935c93b81d9f6de43d
Jirawat <paoji@icloud.com>
2017-12-08 23:47:42 +0700

    add function to download

--- www/video/compare.php
+++ www/video/compare.php
@@ -271,7 +271,7 @@ else
             </style>
         </head>
         <body>
-            <div class="page-wide">
+            <div class="page-wide" id="page-wide">
                 <?php
                 $tab = 'Test Result';
                 $nosubheader = true;
@@ -294,6 +294,7 @@ else
 
                 <?php include('footer.inc'); ?>
             </div>
+            <a  href="javascript:genPDF()"><button class="button5">Dowload PDF</button></a>
 
             <script type="text/javascript">
                 function ShowAdvanced()
@@ -651,6 +652,20 @@ function ScreenShotTable()
         // scroll the table to show the first thumbnail change
         $scrollPos = $firstFrame * ($maxThumbWidth + 6);
         ?>
+        <script type="text/javascript" src="jspdf.min.js"></script>
+           <script type="text/javascript" src="html2canvas.js"></script>
+            <script type="text/javascript">
+           function genPDF(){
+            html2canvas(document.getElementById("page-wide"),{
+            onrendered: function (canvas) {
+            var img=canvas.toDataURL("image/png");
+            var doc=new jsPDF();
+            doc.addImage(img,'JPEG',20,20);
+            doc.save('test.pdf');
+            }
+            });
+            }
+         </script>
         <script language="javascript">
             var thumbWidth = <?php echo "$maxThumbWidth + 4;"; ?>
             var scrollPos = <?php echo "$scrollPos;"; ?>
