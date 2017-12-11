c979375d56b1e82064fc7db1471e664a6c706cfb
JIRAWAT JARUTLUECHAI <34392290+pao0012@users.noreply.github.com>
2017-12-11 15:51:00 +0700

    Update customWaterfall.php

--- www/customWaterfall.php
+++ www/customWaterfall.php
@@ -52,9 +52,38 @@ $page_description = "Website speed test custom waterfall$testLabel";
         function genPDF(){
             html2canvas(document.getElementById("testdiv"),{
             onrendered: function (canvas) {
-                var img=canvas.toDataURL("image/png");
-                var doc=new jsPDF();
-                doc.addImage(img,'JPEG',20,20);
+                var contentWidth = canvas.width;
+	      var contentHeight = canvas.height;
+	
+	      //The height of the canvas which one pdf page can show;
+	      var pageHeight = contentWidth / 592.28 * 841.89;
+	      //the height of canvas that haven't render to pdf
+	      var leftHeight = contentHeight;
+	      //addImage y-axial offset
+	      var position = 0;
+	      //a4 format [595.28,841.89]	      
+              var imgWidth = 595.28;
+	      var imgHeight = 592.28/contentWidth * contentHeight;
+	
+	      var pageData = canvas.toDataURL('image/jpeg', 1.0);
+	
+	      var pdf = new jsPDF('', 'pt', 'a4');
+	
+	     if (leftHeight < pageHeight) {
+	          pdf.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight );
+	      } else {
+	          while(leftHeight > 0) {
+	              pdf.addImage(pageData, 'JPEG', 0, position, imgWidth, imgHeight)
+	              leftHeight -= pageHeight;
+	              position -= 841.89;
+	              //avoid blank page
+	              if(leftHeight > 0) {
+	                  pdf.addPage();
+	              }
+	          }
+	      }
+	
+	      pdf.save('test.pdf');20);
                 doc.save('test.pdf');
         }
       });
