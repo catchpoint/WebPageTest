4e32badf2ecc1aa6faf976ce6d031ab53f7a080c
fook_supachai <fookfik1551@gmail.com>
2017-12-13 02:08:31 +0700

    Update index.php

--- www/video/index.php
+++ www/video/index.php
@@ -64,8 +64,8 @@ if (is_file(__DIR__ . '/../settings/profiles.ini'))
                             }
                             ?>
                             <div id="urldiv1" class="urldiv">
-                                Label: <input id="label1" type="text" name="label[1]" style="width:10em"> 
-                                URL: <input id="url1" type="text" name="url[1]" style="width:30em"> 
+                                Label: <input id="label1" type="search" name="label[1]" style="width:10em"> 
+                                URL: <input id="url1" type="search" name="url[1]" style="width:30em"> 
                                 <a href='#' onClick='return RemoveUrl("#urldiv1");'>Remove</a>
                             </div>
                         </div>
