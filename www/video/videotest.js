ab7bc107f3f1183afd18c285f524cee99b6c64e9
qweasdzxc789 <34521536+qweasdzxc789@users.noreply.github.com>
2017-12-14 02:53:14 +0700

    Update videotest.js

--- www/video/videotest.js
+++ www/video/videotest.js
@@ -1,3 +1,4 @@
+var urlCount = 1;
 function ValidateInput(form)
 {
     return true;
@@ -5,12 +6,13 @@ function ValidateInput(form)
 
 function AddUrl()
 {
+    urlCount += 1;
     var id = parseInt($("#nextid").val(), 10);
     
     var html = "<div id='urldiv" + id + "' class='urldiv'>";
     html += "Label: <input id='label" + id +"' type='text' name='label[" + id + "]' style='width:10em'> ";
     html += "URL: <input id='url" + id +"' type='text' name='url[" + id + "]' style='width:30em' onkeypress='if (event.keyCode == 32) {return false;}'> ";
-    html += "<a href='#' onClick='return RemoveUrl(\"#urldiv" + id + "\");'>Remove</a>";
+    html += "<a href='#' onClick='return RemoveUrl(\"#urldiv" + id + "\");' id='removeLink'>Remove</a>";
     html += "</div>";
     $("#urls").append(html);
     
@@ -21,6 +23,16 @@ function AddUrl()
 
 function RemoveUrl(id) 
 {
+    urlCount -= 1;
     $(id).remove();
     return false;
 }
+function showRemoveLink(){
+    if (urlCount == 1){
+        $(#removeLink).hide();
+    }
+    else {
+        $(#removeLink).show();
+    }
+}
+showRemoveLink();
