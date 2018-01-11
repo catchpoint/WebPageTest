b0aed6f1f7773ee71ddf31a924b1bc623a80edcb
Patrick Meenan <pmeenan@chromium.org>
2018-01-11 08:53:27 -0500

    Change the netlog extension to json to make it easier to import into Chrome.

--- www/getgzip.php
+++ www/getgzip.php
@@ -10,11 +10,14 @@ if( isset($_GET['file']) &&
     strpos($_GET['file'], 'testinfo') === false &&
     gz_is_file($file) )
 {
-    header("Content-disposition: attachment; filename={$_GET['file']}");
-    if( strpos($_GET['file'], 'pagespeed') !== false || 
-        strpos($_GET['file'], '.json') !== false ) {
+    $file_name = $_GET['file'];
+    if (substr($file_name, -10) === 'netlog.txt')
+      $file_name = str_replace('netlog.txt', 'netlog.json', $file_name);
+    header("Content-disposition: attachment; filename=$file_name");
+    if( strpos($file_name, 'pagespeed') !== false || 
+        strpos($file_name, '.json') !== false ) {
         header ("Content-type: application/json");
-    } elseif (strpos($_GET['file'], '.log') !== false) {
+    } elseif (strpos($file_name, '.log') !== false) {
         header ("Content-type: text/plain");
     } else {
         header ("Content-type: application/octet-stream");
