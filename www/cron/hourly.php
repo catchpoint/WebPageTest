aba9af5fbe3166e35ffbb6f923fdb3ebe0252920
Patrick Meenan <pmeenan@chromium.org>
2018-07-12 08:08:19 -0400

    Remove the centralized agent updates since those agents are no longer supported (new servers will still do one update before getting the fix).

--- www/cron/hourly.php
+++ www/cron/hourly.php
@@ -22,7 +22,6 @@ if (GetSetting('ec2_key')) {
 }
 
 GitUpdate();
-AgentUpdate();
 ApkUpdate();
 
 Unlock($cron_lock);
@@ -49,59 +48,6 @@ function ApkUpdate() {
   if (GetSetting('apkPackages')) {
     echo "Updating APKs from attached device...\n";
     include __DIR__ . '/apkUpdate.php';
-  }
-}
-
-/**
-* Automatically update the agent binaries from the public agents (if configured)
-* 
-*/
-function AgentUpdate() {
-  $updateServer = GetSetting('agentUpdate');
-  echo "\nChecking for agent update...\n";
-  if ($updateServer && strlen($updateServer)) {
-    if (!is_dir('./work/update'))
-      mkdir('./work/update', 0777, true); 
-    $url = $updateServer . 'work/getupdates.php';
-    $updates = json_decode(http_fetch($url), true);
-    if ($updates && is_array($updates) && count($updates)) {
-      foreach($updates as $update) {
-        $needsUpdate = true;
-        $ini = "./work/update/{$update['name']}.ini";
-        $zip = "./work/update/{$update['name']}.zip";
-        if (is_file($ini) && is_file($zip)) {
-          $current = parse_ini_file($ini);
-          if ($current['ver'] == $update['ver'])
-            $needsUpdate = false;
-        }
-        if ($needsUpdate && isset($update['md5'])) {
-          $tmp = "./work/update/{$update['name']}.tmp";
-          if (is_file($tmp))
-            unlink($tmp);
-          $url = $updateServer . str_replace(" ","%20","work/update/{$update['name']}.zip?v={$update['ver']}");
-          echo "Fetching $url\n";
-          if (http_fetch_file($url, $tmp)) {
-            $md5 = md5_file($tmp);
-            if ($md5 == $update['md5']) {
-              if (is_file("$ini.bak"))
-                unlink("$ini.bak");
-              if (is_file($ini))
-                rename($ini, "$ini.bak");
-              if (is_file("$zip.bak"))
-                unlink("$zip.bak");
-              if (is_file($zip))
-                rename($zip, "$zip.bak");
-              rename($tmp, $zip);
-              $z = new ZipArchive;
-              if ($z->open($zip) === TRUE) {
-                $z->extractTo('./work/update', "{$update['name']}.ini");
-                $z->close();
-              }
-            }
-          }
-        }
-      }
-    }
   }
 }
 
