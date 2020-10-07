bf882f53c8b8fab9687e09c096ebdf6e72f74171
Patrick Meenan <PatMeenan@gmail.com>
2020-10-07 19:08:58 +0000

    Changed the profile logging to write to a single file that can be logrotated

--- www/work/profile.php
+++ www/work/profile.php
@@ -1,16 +1,20 @@
 <?php
 // This processes profiler data uploaded from agents as JSON and logs it to the dat directory for further processing
+chdir('..');
+require_once('common.inc');
 
 // Load the profile data, parse it (to make sure it is valid) and then write it out to the log
-$raw = file_get_contents('php://input');
-if ($raw) {
-    $obj = json_decode($raw, true);
-    if ($obj) {
-        $json = json_encode($obj);
-        if (isset($json) && is_string($json) && strlen($json)) {
-            $json .= "\n";
-            $log_file = __DIR__ . '/../dat/' . date('ymd') . '_profile.json';
-            error_log($json, 3, $log_file);
+if (GetSetting('profile_data')) {
+    $raw = file_get_contents('php://input');
+    if ($raw) {
+        $obj = json_decode($raw, true);
+        if ($obj) {
+            $json = json_encode($obj);
+            if (isset($json) && is_string($json) && strlen($json)) {
+                $json .= "\n";
+                $log_file = __DIR__ . '/../dat/profile.log';
+                error_log($json, 3, $log_file);
+            }
         }
     }
 }
