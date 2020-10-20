52df8b26a8eb6d4c0e59664eef4e2ab7c2b58409
Patrick Meenan <PatMeenan@gmail.com>
2020-10-20 14:18:55 +0000

    Force no caching for the slow image

--- www/test_page/slowimage.php
+++ www/test_page/slowimage.php
@@ -1,4 +1,8 @@
 <?php
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 header("Content-Type: image/png");
+
 usleep(1000000);
 readfile(__DIR__ . '/image.png');
