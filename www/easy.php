4c03698b53f2ad0240c5d0022ca246371ed0bfba
Patrick Meenan <PatMeenan@gmail.com>
2021-02-25 15:54:41 +0000

    Eliminated the forced HTTPS redirect in case it was causing cached redirect loops

--- www/easy.php
+++ www/easy.php
@@ -2,7 +2,7 @@
 // Copyright 2020 Catchpoint Systems Inc.
 // Use of this source code is governed by the Polyform Shield 1.0.0 license that can be
 // found in the LICENSE.md file.
-$REDIRECT_HTTPS = true;
+//$REDIRECT_HTTPS = true;
 include 'common.inc';
 
 $headless = false;
