151b005296c2af5e75085027c4ad41451cc873aa
Tim Kadlec <tim@timkadlec.com>
2021-03-22 15:27:30 +0000

    fixing commented out code

--- www/index.php
+++ www/index.php
@@ -197,17 +197,11 @@ $loc = ParseLocations($locations);
                             <label for="url" class="vis-hidden">Enter URL to test</label>
                             <input type="text" name="url" id="url" inputmode="url" value="<?php echo $url; ?>" class="text large" autocorrect="off" autocapitalize="off" onfocus="if (this.value == this.defaultValue) {this.value = '';}" onblur="if (this.value == '') {this.value = this.defaultValue;}" onkeypress="if (event.keyCode == 32) {return false;}">
                         <?php
-<<<<<<< HEAD
                             if (strlen($siteKey)) {
                             echo "<button data-sitekey=\"$siteKey\" data-callback='onRecaptchaSubmit' class=\"g-recaptcha start_test\">Start Test &#8594;</button>";
                             } else {
-=======
-                            //if (strlen($siteKey)) {
-                            //echo "<p><button data-sitekey=\"$siteKey\" data-callback='onRecaptchaSubmit' class=\"g-recaptcha start_test\"></button></p>";
-                            //} else {
->>>>>>> 1880980a6160767f624cd4d4d13f5b537f63146f
                             echo '<input type="submit" name="submit" value="Start Test &#8594;" class="start_test">';
-                            //}
+                            }
                             ?>
                     </li>
                         <li>
