7770045539ffb4699cb9fb15436dc00c80a4ffb0
Patrick Meenan <PatMeenan@gmail.com>
2021-03-24 15:39:05 -0400

    Fix the logout

--- www/saml/logout.php
+++ www/saml/logout.php
@@ -6,7 +6,7 @@ header("Cache-Control: no-store, max-age=0");
 
 // Store the referer page in a session cookie and redirect to the saml login
 $url = getUrlProtocol() . '://' . $_SERVER['HTTP_HOST'] . '/';
-if (isset($_SERVER["HTTP_REFERER"])) {
+if (isset($_SERVER["HTTP_REFERER"]) && strlen($_SERVER["HTTP_REFERER"])) {
     $url = $_SERVER["HTTP_REFERER"];
 }
 // Set the cookie into the past
