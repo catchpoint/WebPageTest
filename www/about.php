993e189233674bccb3c778377a2e604c66807e98
Patrick Meenan <PatMeenan@gmail.com>
2021-04-01 11:53:31 +0000

    Updated about page

--- www/about.php
+++ www/about.php
@@ -26,28 +26,31 @@ $page_description = "More information about WebPageTest website speed testing an
             ?>
             <h1>About WebPageTest</h1>
             <div class="box">
-                <?php
-                if( is_file('settings/server/about.inc') ) {
-                    include('settings/server/about.inc');
-                } elseif( is_file('settings/common/about.inc') ) {
-                    include('settings/common/about.inc');
-                } elseif( is_file('settings/about.inc') ) {
-                    include('settings/about.inc');
-                } else {
-                ?>
-                <a href="https://developers.google.com/speed"><img id="logo" src="images/google.png"></a>
-                <br><p>WebPageTest is an open source project that is primarily being developed and supported by Google as part of our efforts
-                to <a href="http://developers.google.com/speed">make the web faster</a>.</p>
-                <p>WebPageTest is a tool that was originally developed by <a href="http://dev.aol.com/">AOL</a> for use internally and was open-sourced in 2008
-                under a BSD license.  The platform is under active development on <a href="https://github.com/WPO-Foundation/webpagetest">GitHub</a> and
-                is also packaged up periodically and available for <a href="https://www.webpagetest.org/forums/forumdisplay.php?fid=12">download</a> if you would like to run your own
-                instance.</p>
-                <p>The online version at <a href="https://www.webpagetest.org/">www.webpagetest.org</a> is run for the benefit of the
-                performance community.</p>
-                <p>If you are having any problems or just have questions about the site, please feel free to <a href="mailto:info@webpagetest.org">contact us</a>.</p>
-                <?php
-                }
-                ?>
+                <p>Building high quality web experiences for users is at the core of all our efforts. With WebPageTest, 
+                we strive to provide performance products and resources to our global and growing community of developers, 
+                third-party platforms, technical consultants and others. WebPage as a tool was originally developed by 
+                Patrick Meenan while he was at AOL for use internally and was open-sourced in 2008 under a BSD license. 
+                The online version at <a href="https://www.webpagetest.org/">www.webpagetest.org</a> is run for the benefit 
+                of the performance community.</p>
+
+                <p>WebPageTest was acquired in September of 2020 by Catchpoint, the leading Digital Experience Monitoring 
+                platform providing Synthetic Monitoring, Real User Measurement, Network Monitoring, and Endpoint Monitoring products.</p>
+
+                <p>The acquisition starts a new and exciting chapter as we plan to expand WebPageTest’s capabilities and WebPageTest.org’s 
+                geographical performance testing footprint, leveraging Catchpoint’s best-in-class infrastructure, adding capacity and 
+                improving consistency and quality of analytics. You can read more about Catchpoint’s acquisition of 
+                WebPageTest <a href="https://www.catchpoint.com/webpagetest-joins-catchpoint">here</a>.<p>
+
+                <p>The WebPageTest code is free to use under the Polyform Shield license, a source-available license. As 
+                long as you are not creating a product or service that competes with Catchpoint’s offerings then you 
+                are free to do whatever you like with the WebPageTest code, including using it for your own internal 
+                use or creating non-competing commercial products from it. In fact, we encourage using the WebPageTest 
+                code to build your own value-added applications. Read 
+                more <a href="https://github.com/WPO-Foundation/webpagetest/blob/master/LICENSE_FAQ.md">here</a>.</p>
+
+                <p>If you are having any problems or just have questions about the site, please feel free to 
+                <a href="mailto:info@webpagetest.org">contact us</a>. If you are considering sending advertising opportunities, 
+                SEO/SEM solicitations, link sharing, etc....don't. We won't respond and will mark it as spam.</p>
             </div>
             <?php include('footer.inc'); ?>
         </div>
