2260e69931c6454bd4eb046b079a550321ede423
Jeffrey Lembeck <jlembeck@gmail.com>
2021-11-02 16:18:15 -0700

    feat(User): create User class
    This is a User model. It has some attributes on it that are important to
    understand about the current user as we build web pages, such as their
    email, if they are an admin, and whether or not they are a paid user.
    
    Have added this user as $current_user to the exported common.inc file so
    it will be referenced in any page that is used

---
+++ tests/models/UserTest.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+require_once __DIR__ . '/../../www/models/user.php';
+
+use PHPUnit\Framework\TestCase;
+
+final class UserTest extends TestCase {
+  public function testConstructorSetsValues() : void {
+    $user = new User();
+    $this->assertTrue($user->is_anon());
+    $this->assertFalse($user->is_admin());
+    $this->assertFalse($user->is_paid_api_user());
+  }
+
+  public function testSettingEmail () : void {
+    $user = new User();
+    $email = 'foo@bar.com';
+    $user->set_email($email);
+    $this->assertEquals($email, $user->get_email());
+    $this->assertFalse($user->is_anon());
+  }
+
+  public function testSettingOwnerId () : void {
+    $user = new User();
+    $owner_id = 1234;
+    $user->set_owner_id($owner_id);
+    $this->assertEquals($owner_id, $user->get_owner_id());
+    $this->assertTrue($user->is_paid_api_user());
+  }
+
+  public function testSettingAdmin () : void {
+    $user = new User();
+    $this->assertFalse($user->is_admin());
+    $user->set_admin(true);
+    $this->assertTrue($user->is_admin());
+  }
+}
