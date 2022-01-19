5d262399a1863318903864c5a7225d00dea62181
Jeffrey Lembeck <jlembeck@gmail.com>
2022-01-19 10:50:50 -0800

    feat(context): add User instance to $request
    This allows us to use $request->getUser() throughout the code to get the
    current user.
    
    This will be helpful when we're managing logging in locally

--- tests/models/RequestContextTest.php
+++ tests/models/RequestContextTest.php
@@ -2,12 +2,22 @@
 
 use PHPUnit\Framework\TestCase;
 use WebPageTest\RequestContext;
+use WebPageTest\User;
 
 final class RequestContextTest extends TestCase {
   public function testConstructorSetsValues() : void {
     $global_req = [];
     $request = new RequestContext($global_req);
     $this->assertEquals($global_req, $request->getRaw());
+  }
+  public function testSetGetUser() : void {
+    $global_req = [];
+    $email = 'foo@foo.com';
+    $user = new User();
+    $user->setEmail($email);
+    $request = new RequestContext($global_req);
+    $request->setUser($user);
+    $this->assertEquals($email, $request->getUser()->getEmail());
   }
 }
 
--- www/models/RequestContext.php
+++ www/models/RequestContext.php
@@ -2,15 +2,29 @@
 
 namespace WebPageTest;
 
+use WebPageTest\User;
+
 class RequestContext {
   private array $raw;
+  private ?User $user;
 
   function __construct (array $global_request) {
     $this->raw = $global_request;
+    $this->user = null;
   }
 
   function getRaw () : array {
     return $this->raw;
+  }
+
+  function getUser () : ?User {
+    return $this->user;
+  }
+
+  function setUser (?User $user) : void {
+    if (isset($user)) {
+      $this->user = $user;
+    }
   }
 
 }
