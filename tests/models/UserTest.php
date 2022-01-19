1f52882f93a96149dac893daa05c57aebff4b1c9
Jeffrey Lembeck <jlembeck@gmail.com>
2022-01-19 09:36:38 -0800

    chore(standards): follow PSR-1 for method names

--- tests/models/UserTest.php
+++ tests/models/UserTest.php
@@ -6,31 +6,31 @@ use WebPageTest\User;
 final class UserTest extends TestCase {
   public function testConstructorSetsValues() : void {
     $user = new User();
-    $this->assertTrue($user->is_anon());
-    $this->assertFalse($user->is_admin());
-    $this->assertFalse($user->is_paid_api_user());
+    $this->assertTrue($user->isAnon());
+    $this->assertFalse($user->isAdmin());
+    $this->assertFalse($user->isPaidApiUser());
   }
 
   public function testSettingEmail () : void {
     $user = new User();
     $email = 'foo@bar.com';
-    $user->set_email($email);
-    $this->assertEquals($email, $user->get_email());
-    $this->assertFalse($user->is_anon());
+    $user->setEmail($email);
+    $this->assertEquals($email, $user->getEmail());
+    $this->assertFalse($user->isAnon());
   }
 
   public function testSettingOwnerId () : void {
     $user = new User();
     $owner_id = 1234;
-    $user->set_owner_id($owner_id);
-    $this->assertEquals($owner_id, $user->get_owner_id());
-    $this->assertTrue($user->is_paid_api_user());
+    $user->setOwnerId($owner_id);
+    $this->assertEquals($owner_id, $user->getOwnerId());
+    $this->assertTrue($user->isPaidApiUser());
   }
 
   public function testSettingAdmin () : void {
     $user = new User();
-    $this->assertFalse($user->is_admin());
-    $user->set_admin(true);
-    $this->assertTrue($user->is_admin());
+    $this->assertFalse($user->isAdmin());
+    $user->setAdmin(true);
+    $this->assertTrue($user->isAdmin());
   }
 }
--- www/models/User.php
+++ www/models/User.php
@@ -13,43 +13,43 @@ class User {
     $this->owner_id = 2445; // owner id of 2445 is for unpaid users
   }
 
-  public function get_email () : ?string {
+  public function getEmail () : ?string {
     return $this->email;
   }
 
-  public function set_email (?string $email) : void {
+  public function setEmail (?string $email) : void {
     if (isset($email)) {
       $this->email = $email;
     }
   }
 
-  public function set_admin (bool $is_admin) : void {
+  public function setAdmin (bool $is_admin) : void {
     $this->is_admin = $is_admin;
   }
 
-  public function get_owner_id () : int {
+  public function getOwnerId () : int {
     return $this->owner_id;
   }
 
   /**
    * @var string|int $owner_id
    */
-  public function set_owner_id ($owner_id) : void {
+  public function setOwnerId ($owner_id) : void {
     $this->owner_id = intval($owner_id);
   }
 
   /**
    * 2445 is the owner id for any user that is unpaid
    */
-  public function is_paid_api_user () : bool {
+  public function isPaidApiUser () : bool {
     return $this->owner_id != 2445;
   }
 
-  public function is_admin () : bool {
+  public function isAdmin () : bool {
     return $this->is_admin;
   }
 
-  public function is_anon () : bool {
+  public function isAnon () : bool {
     return $this->email == null;
   }
 
