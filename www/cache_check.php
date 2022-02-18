e0e19aa1106549c08c60991cc91f65c49382d63d
Jeffrey Lembeck <jlembeck@gmail.com>
2022-02-18 08:17:34 -0800

    fixup! fixup! fixup! fixup! break out cache_list

--- www/cache_check.php
+++ www/cache_check.php
@@ -16,15 +16,20 @@ use WebPageTest\RequestContext;
     exit();
   }
 
+  /**
   $cache_info = apcu_cache_info();
   $list = $cache_info['cache_list'];
   $infos = array_map(fn($val) => $val['info'], $list);
   $filtered = array_filter($infos, function($k) {
     return preg_match('/rladdr_per_month/', $k);
   });
+  echo var_dump($filtered);
+   */
 
   echo '<pre>';
-  echo var_dump($filtered);
+  foreach (new APCUIterator('/rladdr_per_month/') as $val) {
+    echo "$val[key]: $val[value]\n";
+  }
   echo '</pre>';
   exit();
 
