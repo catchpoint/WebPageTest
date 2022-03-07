06630c500021613113f06ba734050a3e047102e3
Jeffrey Lembeck <jlembeck@gmail.com>
2022-03-07 22:53:51 +0000

    these src props should match markup not dom

--- www/settings/custom_metrics/imgs-no-alt.js
+++ www/settings/custom_metrics/imgs-no-alt.js
@@ -3,7 +3,7 @@ var allImgs = document.querySelectorAll("img");
 for (var i = 0; i < allImgs.length; i++) {
  if ( !allImgs[i].hasAttribute("alt")  ) {
     imgsNoAlt.push({
-      src: allImgs[i].src,
+      src: allImgs[i].getAttribute("src"),
       html: allImgs[i].outerHTML,
       currentSrc: allImgs[i].currentSrc
   });
--- www/settings/custom_metrics/imgs-out-viewport.js
+++ www/settings/custom_metrics/imgs-out-viewport.js
@@ -4,11 +4,11 @@ for (var i = 0; i < allImgs.length; i++) {
  var boundingClientRect = allImgs[i].getBoundingClientRect();
  if ( boundingClientRect.top <= window.innerHeight) {
     imgsOutViewport.push({
-      src: allImgs[i].src,
+      src: allImgs[i].getAttribute("src"),
       html: allImgs[i].outerHTML,
       currentSrc: allImgs[i].currentSrc,
-      srcSet: allImgs[i].srcSet,
-      sizes: allImgs[i].sizes,
+      srcSet: allImgs[i].getAttribute("srcset"),
+      sizes: allImgs[i].getAttribute("sizes"),
       priority: allImgs[i].getAttribute("priority"),
       loading: allImgs[i].getAttribute("loading")
   });
