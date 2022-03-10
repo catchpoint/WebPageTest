d4a4cc42b539f76d45fc8e41cb2b96f450bf01f9
Scott Jehl <scottjehl@gmail.com>
2022-03-10 16:17:38 +0000

    see what undefined gets us

--- www/settings/custom_metrics/meta-viewport.js
+++ www/settings/custom_metrics/meta-viewport.js
@@ -1,5 +1,5 @@
-var metaviewport = undefined;
-var metaTags=document.getElementsByTagName("meta");
+var metaviewport;
+var metaTags = document.getElementsByTagName("meta");
 for (var i = 0; i < metaTags.length; i++) {
     if (metaTags[i].getAttribute("name") == "viewport") {
         metaviewport = metaTags[i].getAttribute("content");
