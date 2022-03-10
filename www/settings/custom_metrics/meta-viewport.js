a118e00a623b5a6e500ecdc2ba5505e95b240af8
Scott Jehl <scottjehl@gmail.com>
2022-03-10 16:40:18 +0000

    explicit return

--- www/settings/custom_metrics/meta-viewport.js
+++ www/settings/custom_metrics/meta-viewport.js
@@ -1,4 +1,4 @@
-var metaviewport = "Not found";
+var metaviewport = null;
 var metaTags = document.getElementsByTagName("meta");
 for (var i = 0; i < metaTags.length; i++) {
     if (metaTags[i].getAttribute("name") == "viewport") {
@@ -6,4 +6,4 @@ for (var i = 0; i < metaTags.length; i++) {
         break;
     }
 }
-return metaviewport;
+return metaviewport === null ? "Not found" : metaviewport;
