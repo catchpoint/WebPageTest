bc4a1b96680f95f97d528e0d897c16d3134077e1
Scott Jehl <scottjehl@gmail.com>
2022-03-22 21:06:22 +0000

    try gen content custom metric

---
+++ www/settings/custom_metrics/generated-content.js
@@ -0,0 +1,12 @@
+let generatedContent = 0;
+const byteSize = str => new Blob([str]).size;
+let htmlInitial = $WPT_BODIES[0].response_body;
+let htmlAfter = document.documentElement.outerHTML;
+
+if (htmlInitial && htmlAfter) {
+    let htmlInitialSize = byteSize(htmlInitial);
+    let htmlAfterSize = byteSize(htmlAfter);
+    generatedContent = (htmlAfterSize - htmlInitialSize) / 1024;
+}
+
+return generatedContent;
