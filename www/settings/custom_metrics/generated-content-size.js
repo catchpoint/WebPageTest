bc752835f04efce7814fece46f7fd026de8c04fb
Scott Jehl <scottjehl@gmail.com>
2022-03-23 15:29:07 +0000

    change gen content metric to include size and make one for percent also

---
+++ www/settings/custom_metrics/generated-content-size.js
@@ -0,0 +1,24 @@
+let generatedContent = 0;
+const byteSize = str => new Blob([str]).size;
+
+function replaceNonContent(markup) {
+    // get content from body start onward
+    markup = markup.replace(/^.*(\<body.+)$/gmi, "$1");
+    //remove script tags
+    markup = markup.replace(/\<script.*\<\/script\>/gmi, "$1");
+    //remove style tags
+    markup = markup.replace(/\<style.*\<\/style\>/gmi, "$1");
+    // remove link tags
+    markup = markup.replace(/\<link.*\<\/link\>/gmi, "$1");
+    return markup;
+}
+let htmlInitial = replaceNonContent($WPT_BODIES[0].response_body);
+let htmlAfter = replaceNonContent(document.documentElement.outerHTML.replace(/^.*(\<body.+)$/gmi, "$1"));
+
+if (htmlInitial && htmlAfter) {
+    let htmlInitialSize = byteSize(htmlInitial);
+    let htmlAfterSize = byteSize(htmlAfter);
+    generatedContent = (htmlAfterSize - htmlInitialSize) / 1024;
+}
+
+return generatedContent;
