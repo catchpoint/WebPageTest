90b081d089674b5b7fd8221f2598db6eaef67577
Scott Jehl <scottjehl@gmail.com>
2022-03-30 18:38:39 +0000

    parse instead, just to see if it impacts any result data

--- www/settings/custom_metrics/generated-content-size.js
+++ www/settings/custom_metrics/generated-content-size.js
@@ -2,16 +2,15 @@ let generatedContent = 0;
 const byteSize = str => new Blob([str]).size;
 
 function replaceNonContent(markup) {
+    let contain = document.createElement("div");
+    contain.innerHTML = markup;
     // get content from body start onward
-    markup = markup.replace(/^.*(\<body.+)$/gmi, "$1");
-    //remove script tags
-    markup = markup.replace(/\<script.*\<\/script\>/gmi, "");
-    //remove style tags
-    markup = markup.replace(/\<style.*\<\/style\>/gmi, "");
-    // remove link tags
-    markup = markup.replace(/\<link.*\<\/link\>/gmi, "");
-    return markup;
+    contain.querySelectorAll("head, script, style, link").forEach(elem => {
+        elem.remove();
+    });
+    return contain.innerHTML;
 }
+
 let htmlInitial = replaceNonContent($WPT_BODIES[0].response_body);
 let htmlAfter = replaceNonContent(document.documentElement.outerHTML.replace(/^.*(\<body.+)$/gmi, "$1"));
 
