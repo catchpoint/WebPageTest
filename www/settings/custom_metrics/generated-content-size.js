55162c343360b08977b43faac0a94720b24ec0b3
Scott Jehl <scottjehl@gmail.com>
2022-03-30 20:36:24 +0000

    try doing compare without stripping non-content

--- www/settings/custom_metrics/generated-content-size.js
+++ www/settings/custom_metrics/generated-content-size.js
@@ -1,22 +1,8 @@
 let generatedContent = 0;
 const byteSize = str => new Blob([str]).size;
 
-function replaceNonContent(markup) {
-    let contain = document.createElement("div");
-    let markupBody = markup.match(/(\<body[\s\>].+$)/gmi);
-    if (markupBody) {
-        markup = markupBody;
-    }
-    contain.innerHTML = markup;
-    // get content from body start onward
-    contain.querySelectorAll("script, style, link").forEach(elem => {
-        elem.remove();
-    });
-    return contain.innerHTML;
-}
-
-let htmlInitial = replaceNonContent($WPT_BODIES[0].response_body);
-let htmlAfter = replaceNonContent(document.documentElement.outerHTML);
+let htmlInitial = $WPT_BODIES[0].response_body;
+let htmlAfter = document.documentElement.outerHTML;
 
 if (htmlInitial && htmlAfter) {
     let htmlInitialSize = byteSize(htmlInitial);
