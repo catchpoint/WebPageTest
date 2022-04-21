abd8a4c64321c1f8494ea26d75e71aa644b76f0a
Scott Jehl <scottjehl@gmail.com>
2022-04-21 17:10:22 +0000

    add natural width and height to these img metrcis

--- www/settings/custom_metrics/imgs-in-viewport.js
+++ www/settings/custom_metrics/imgs-in-viewport.js
@@ -1,18 +1,20 @@
 var imgsInViewport = [];
 var allImgs = document.querySelectorAll("img");
 for (var i = 0; i < allImgs.length; i++) {
- var boundingClientRect = allImgs[i].getBoundingClientRect();
- if ( boundingClientRect.top < window.innerHeight) {
-  imgsInViewport.push({
-      src: allImgs[i].getAttribute("src"),
-      html: allImgs[i].outerHTML,
-      currentSrc: allImgs[i].currentSrc,
-      srcSet: allImgs[i].getAttribute("srcset"),
-      sizes: allImgs[i].getAttribute("sizes"),
-      priority: allImgs[i].getAttribute("priority"),
-      loading: allImgs[i].getAttribute("loading")
-  });
- }
+    var boundingClientRect = allImgs[i].getBoundingClientRect();
+    if (boundingClientRect.top < window.innerHeight) {
+        imgsInViewport.push({
+            src: allImgs[i].getAttribute("src"),
+            html: allImgs[i].outerHTML,
+            currentSrc: allImgs[i].currentSrc,
+            srcSet: allImgs[i].getAttribute("srcset"),
+            sizes: allImgs[i].getAttribute("sizes"),
+            priority: allImgs[i].getAttribute("priority"),
+            loading: allImgs[i].getAttribute("loading"),
+            naturalWidth: allImgs[i].naturalWidth,
+            naturalHeight: allImgs[i].naturalHeight
+        });
+    }
 }
 
 return imgsInViewport;
