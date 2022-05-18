7472c2d3f989e682077d5844e56ec425c16d230a
Tim Kadlec <tim@timkadlec.com>
2022-05-18 15:27:54 +0000

    fixing custom metric for out of viewport images to look for stuff outside instead of inside which it does at the moment

--- www/settings/custom_metrics/imgs-out-viewport.js
+++ www/settings/custom_metrics/imgs-out-viewport.js
@@ -2,7 +2,7 @@ var imgsOutViewport = [];
 var allImgs = document.querySelectorAll("img");
 for (var i = 0; i < allImgs.length; i++) {
     var boundingClientRect = allImgs[i].getBoundingClientRect();
-    if (boundingClientRect.top <= window.innerHeight) {
+    if (boundingClientRect.top >= window.innerHeight) {
         imgsOutViewport.push({
             src: allImgs[i].getAttribute("src"),
             html: allImgs[i].outerHTML,
