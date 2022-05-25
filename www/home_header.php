b80ac205a0012383ed0e9eb7487d54ab63e35762
Scott Jehl <scottjehl@gmail.com>
2022-05-25 00:07:42 +0000

    contain max

--- www/home_header.php
+++ www/home_header.php
@@ -1,34 +1,34 @@
 <div class="home_feature">
     <div class="home_feature_hed_contain">
-		<div class="home_feature_hed home_feature_hed-main">
-            <div class="home_feature_hed_text">
-                <h1 class="attention">Test. Optimize. Repeat!</h1>
-                <p>Instantly test your site’s speed, usability, and resilience in real <b>browsers</b>, <b>devices</b>, and <b>locations</b> around the&nbsp;world. </p>
+		<div class="home_feature_containslides">
+			<div class="home_feature_hed home_feature_hed-main">
+				<div class="home_feature_hed_text">
+					<h1 class="attention">Test. Optimize. Repeat!</h1>
+					<p>Instantly test your site’s speed, usability, and resilience in real <b>browsers</b>, <b>devices</b>, and <b>locations</b> around the&nbsp;world. </p>
 
-            </div>
-            <div class="home_feature_hed_visual">
-                <img src="/images/wpt_home_featureimg.jpg" fetchpriority="high" width="1414" height="843" alt="screenshot of wpt results page">
-            </div>
-        </div>
-
-		<div class="home_feature_hed home_feature_hed-pro">
-			<div class="home_feature_hed_text">
-				<h1 class="attention"><span class="home_feature_hed_text_leadin">Say hello to </span> <img class="home_feature_hed_text_logo" width="105" height="14" src="/images/wpt-logo-pro.svg" alt="WebPageTest Pro"></h1>
-							<p>All of the WebPageTest features you already love, <span class="home_feature_hed_text_line">plus <b>API Access</b> &amp; <b>No-Code Experiments!</b></span></p>
-				<a class="pill" href="#" style="
-				padding: .9em 1.5em;
-			">View Plans &amp; Learn More &gt;&gt;</a>
+				</div>
+				<div class="home_feature_hed_visual">
+					<img src="/images/wpt_home_featureimg.jpg" fetchpriority="high" width="1414" height="843" alt="screenshot of wpt results page">
+				</div>
 			</div>
-			<div class="home_feature_hed_visual">
-					<video width="1152" height="720" playsinline="" id="intro" poster="/images/pro-intro.jpg" preload="none">
-						<source src="/images/pro-intro-1152.mp4" media="(min-width: 800px)">
-						<source src="video/pro-intro-intro-480.mp4">
-						<track default="" kind="captions" srclang="en" src="/images/pro-intro.vtt">
-					</video>
-					<button class="play" id="playbtn">Play/Pause Video</button>
+			<div class="home_feature_hed home_feature_hed-pro">
+				<div class="home_feature_hed_text">
+					<h1 class="attention"><span class="home_feature_hed_text_leadin">Say hello to </span> <img class="home_feature_hed_text_logo" width="105" height="14" src="/images/wpt-logo-pro.svg" alt="WebPageTest Pro"></h1>
+								<p>All of the WebPageTest features you already love, <span class="home_feature_hed_text_line">plus <b>API Access</b> &amp; <b>No-Code Experiments!</b></span></p>
+					<a class="pill" href="#" style="
+					padding: .9em 1.5em;
+				">View Plans &amp; Learn More &gt;&gt;</a>
+				</div>
+				<div class="home_feature_hed_visual">
+						<video width="1152" height="720" playsinline="" id="intro" poster="/images/pro-intro.jpg" preload="none">
+							<source src="/images/pro-intro-1152.mp4" media="(min-width: 800px)">
+							<source src="video/pro-intro-intro-480.mp4">
+							<track default="" kind="captions" srclang="en" src="/images/pro-intro.vtt">
+						</video>
+						<button class="play" id="playbtn">Play/Pause Video</button>
+				</div>
 			</div>
 		</div>
-      
 
 
         
