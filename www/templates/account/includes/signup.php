9aca410920cbc8658dfa271b522d06950b1075b9
Sue Lockwood <deathbearbrown@gmail.com>
2022-06-01 11:21:50 -0700

    Update the account page sign up forms to show the correct billing frequency and the % of savings

--- www/templates/account/includes/signup.php
+++ www/templates/account/includes/signup.php
@@ -1,39 +1,40 @@
 <div class="card subscribe">
-  <div class="card-section">
-    <div class="info">
-      <p style="line-height: 1.5">WebPageTest Pro plans bring full access to the power and depth of WebPageTest's analysis, letting you pull performance data into your existing workflows and processes. It also includes access to No-Code Experiments, the WebPageTest API, bulk testing, and more!</p>
-      <h3>Plan Features Comparison</h3>
-      <img style="max-width: 100%" src="/images/plans-breakdown.png" alt="Pro Plan table breakdown: temporarily an image as we work through launch day hiccups. major apologies! fix coming.">
+    <div class="card-section">
+        <div class="info">
+            <p style="line-height: 1.5">WebPageTest Pro plans bring full access to the power and depth of WebPageTest's analysis, letting you pull performance data into your existing workflows and processes. It also includes access to No-Code Experiments, the WebPageTest API, bulk testing, and more!</p>
+            <h3>Plan Features Comparison</h3>
+            <img style="max-width: 100%" src="/images/plans-breakdown.png" alt="Pro Plan table breakdown: temporarily an image as we work through launch day hiccups. major apologies! fix coming.">
+        </div>
     </div>
-  </div>
 </div>
 <form id="wpt-account-signup" method="post" action="/account">
-  <fieldset class="wpt-plans">
-    <legend class="sr-only">Choose a Plan</legend>
-    <input id="annual-plans" type="radio" name="plan-type" value="annual" checked />
-    <input id="monthly-plans" type="radio" name="plan-type" value="monthly" />
-    <div class="radiobutton-group">
-      <label for="annual-plans">Annual Plans</label>
-      <label for="monthly-plans">Monthly Plans</label>
-    </div>
-    <div class="wpt-plan-set annual-plans">
-      <?php foreach($annual_plans as $plan):
-$plan_block = <<<HTML
+    <h3>Save 20% by paying annually!</h3>
+    <fieldset class="wpt-plans">
+        <legend class="sr-only">Choose a Plan</legend>
+        <input id="annual-plans" type="radio" name="plan-type" value="annual" checked />
+        <input id="monthly-plans" type="radio" name="plan-type" value="monthly" />
+        <div class="radiobutton-group">
+            <label for="annual-plans">Annual Plans</label>
+            <label for="monthly-plans">Monthly Plans</label>
+        </div>
+        <div class="wpt-plan-set annual-plans">
+            <?php foreach ($annual_plans as $plan) :
+                $plan_block = <<<HTML
       <div class="form-wrapper-radio">
         <input type="radio" id="annual-{$plan['id']}" name="plan" value="{$plan['id']}" required />
         <label class="wpt-plan card" for="annual-{$plan['id']}">
           <h5>{$plan['name']}</h5>
-          <div><strong>\${$plan['monthly_price']}</strong>/Month</div>
+          <div><strong>\${$plan['annual_price']}</strong>/Year</div>
           <span aria-hidden="true" class="select-indicator">Select</span>
         </label>
       </div>
 HTML;
-echo $plan_block;
-    endforeach; ?>
-    </div>
-    <div class="wpt-plan-set monthly-plans">
-      <?php foreach($monthly_plans as $plan):
-$plan_block = <<<HTML
+                echo $plan_block;
+            endforeach; ?>
+        </div>
+        <div class="wpt-plan-set monthly-plans">
+            <?php foreach ($monthly_plans as $plan) :
+                $plan_block = <<<HTML
       <div class="form-wrapper-radio">
         <input type="radio" id="monthly-{$plan['id']}" name="plan" value="{$plan['id']}" required />
         <label class="card wpt-plan" for="monthly-{$plan['id']}">
@@ -43,77 +44,77 @@ $plan_block = <<<HTML
         </label>
       </div>
 HTML;
-echo $plan_block;
-    endforeach; ?>
+                echo $plan_block;
+            endforeach; ?>
+        </div>
+    </fieldset>
+    <div class="card">
+        <div>Looking to run more than 20,000 tests in a month?</div>
+        <div><a href="#">Contact Us</a></div>
     </div>
-  </fieldset>
-  <div class="card">
-    <div>Looking to run more than 20,000 tests in a month?</div>
-    <div><a href="#">Contact Us</a></div>
-  </div>
-  <div class="plan-billing-container">
-    <div class="card billing-container">
-      <div id="braintree-container"></div>
-      <div class="billing-info-section">
-        <div class="info-container street-address">
-          <label for="street-address">Street Address</label>
-          <div>
-            <input name="street-address" type="text" required />
-          </div>
-        </div>
-        <div class="info-container city">
-          <label for="city">City</label>
-          <div>
-            <input name="city" type="text" required />
-          </div>
-        </div>
-        <div class="info-container state">
-          <label for="state">State</label>
-          <div>
-            <input name="state" type="text" required />
-          </div>
-        </div>
-        <div class="info-container country">
-          <label for="country">Country</label>
-          <div>
-            <select name="country">
-            <?php foreach($country_list as $country): ?>
-            <option value="<?= $country["key"] ?>"><?= $country["text"]; ?></option>
-            <?php endforeach; ?>
-            </select>
-          </div>
-        </div>
-        <div class="info-container zipcode">
-          <label for="zipcode">Zip Code</label>
-          <div>
-            <input type="text" name="zipcode" required />
-          </div>
-        </div>
-      </div> <!-- /.billing-info-section -->
-    </div> <!-- /.billing-container -->
+    <div class="plan-billing-container">
+        <div class="card billing-container">
+            <div id="braintree-container"></div>
+            <div class="billing-info-section">
+                <div class="info-container street-address">
+                    <label for="street-address">Street Address</label>
+                    <div>
+                        <input name="street-address" type="text" required />
+                    </div>
+                </div>
+                <div class="info-container city">
+                    <label for="city">City</label>
+                    <div>
+                        <input name="city" type="text" required />
+                    </div>
+                </div>
+                <div class="info-container state">
+                    <label for="state">State</label>
+                    <div>
+                        <input name="state" type="text" required />
+                    </div>
+                </div>
+                <div class="info-container country">
+                    <label for="country">Country</label>
+                    <div>
+                        <select name="country">
+                            <?php foreach ($country_list as $country) : ?>
+                                <option value="<?= $country["key"] ?>"><?= $country["text"]; ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+                </div>
+                <div class="info-container zipcode">
+                    <label for="zipcode">Zip Code</label>
+                    <div>
+                        <input type="text" name="zipcode" required />
+                    </div>
+                </div>
+            </div> <!-- /.billing-info-section -->
+        </div> <!-- /.billing-container -->
 
-    <div class="card plan-details-container">
-      <h3>WebPageTest Pro plans include</h3>
-      <ul>
-          <li>Everything in the Starter plan, including real browsers in real locations, custom scripting for page level and user journey measurements, access to 13 months of test history, and the all new Opportunities report to help you zero in on areas of improvement. </li>
-          <li>Access to all new no-code Experiments</li>
-          <li>API access for easier integration into your CI/CD, visualizations, alerting and more </li>
-          <li>High priority tests to help you jump the queue and experience lower wait times </li>
-          <li>Access to new and exclusive, premium-only, test locations </li>
-          <li>Dedicated support to help you get back to work faster </li>
-          <li>Bulk testing to enable testing of many pages at once </li>
-          <li>Private tests for ensuring your private test results stay that way</li>
-      </ul>
-    </div> <!-- /.plan-details-container -->
-  </div> <!-- /.plan-billing-container -->
+        <div class="card plan-details-container">
+            <h3>WebPageTest Pro plans include</h3>
+            <ul>
+                <li>Everything in the Starter plan, including real browsers in real locations, custom scripting for page level and user journey measurements, access to 13 months of test history, and the all new Opportunities report to help you zero in on areas of improvement. </li>
+                <li>Access to all new no-code Experiments</li>
+                <li>API access for easier integration into your CI/CD, visualizations, alerting and more </li>
+                <li>High priority tests to help you jump the queue and experience lower wait times </li>
+                <li>Access to new and exclusive, premium-only, test locations </li>
+                <li>Dedicated support to help you get back to work faster </li>
+                <li>Bulk testing to enable testing of many pages at once </li>
+                <li>Private tests for ensuring your private test results stay that way</li>
+            </ul>
+        </div> <!-- /.plan-details-container -->
+    </div> <!-- /.plan-billing-container -->
 
-  <input type="hidden" name="nonce" id="hidden-nonce-input" required />
-  <input type="hidden" name="type" value="account-signup" required />
-  <input type="hidden" name="csrf_token" value="<?= $csrf_token; ?>" />
+    <input type="hidden" name="nonce" id="hidden-nonce-input" required />
+    <input type="hidden" name="type" value="account-signup" required />
+    <input type="hidden" name="csrf_token" value="<?= $csrf_token; ?>" />
 
-  <div class="add-subscription-button-wrapper">
-    <button type="submit">Add Subscription</button>
-  </div>
+    <div class="add-subscription-button-wrapper">
+        <button type="submit">Add Subscription</button>
+    </div>
 </form>
 
 <script src="https://js.braintreegateway.com/web/3.85.2/js/client.min.js"></script>
@@ -129,22 +130,22 @@ echo $plan_block;
             }
         }
     }, (error, dropinInstance) => {
-      var hiddenNonceInput = document.querySelector('#hidden-nonce-input');
-      var form = document.querySelector("#wpt-account-signup");
+        var hiddenNonceInput = document.querySelector('#hidden-nonce-input');
+        var form = document.querySelector("#wpt-account-signup");
 
-      form.addEventListener('submit', function (event) {
-        event.preventDefault();
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
 
-        dropinInstance.requestPaymentMethod(function (err, payload) {
-          if (err) {
-            // handle error
-            console.error(err);
-            return;
-          }
+            dropinInstance.requestPaymentMethod(function(err, payload) {
+                if (err) {
+                    // handle error
+                    console.error(err);
+                    return;
+                }
 
-          hiddenNonceInput.value = payload.nonce;
-          form.submit();
+                hiddenNonceInput.value = payload.nonce;
+                form.submit();
+            });
         });
-      });
     });
 </script>
