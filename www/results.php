471cbd6db14338bcb088883496bc59558eeda8eb
Jeffrey Lembeck <jlembeck@gmail.com>
2022-05-31 20:37:03 -0700

    fix(privacy): show full page for private tests
    A bug in including common.inc caused some wacky and wild stuff :cue
    Carson: where the result page wouldn't render but also wouldn't throw an
    error.
    
    This also includes a fix to the nginx.conf so our 404's return a 404
    status as well

--- www/results.php
+++ www/results.php
@@ -2,7 +2,7 @@
 // Copyright 2020 Catchpoint Systems Inc.
 // Use of this source code is governed by the Polyform Shield 1.0.0 license that can be
 // found in the LICENSE.md file.
-include 'common.inc';
+require_once __DIR__ . '/common.inc';
 require_once('page_data.inc');
 
 if (array_key_exists('f', $_REQUEST) && $_REQUEST['f'] == 'json') {
