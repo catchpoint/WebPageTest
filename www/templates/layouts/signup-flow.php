194c7a05edfbb23f1c859b77d2afe434da16782c
Sue Lockwood <deathbearbrown@gmail.com>
2022-06-06 09:25:02 -0700

    Add Expiration to alert

--- www/templates/layouts/signup-flow.php
+++ www/templates/layouts/signup-flow.php
@@ -3,56 +3,59 @@
 
 <head>
     <?php
-      require_once __DIR__ . '/../../common.inc';
+    require_once __DIR__ . '/../../common.inc';
 
-      use WebPageTest\Util;
+    use WebPageTest\Util;
 
-      global $USER_EMAIL;
-      global $supportsAuth;
-      global $supportsSaml;
-      global $client_error;
-      global $VER_CSS;
+    global $USER_EMAIL;
+    global $supportsAuth;
+    global $supportsSaml;
+    global $client_error;
+    global $VER_CSS;
 
-      $page_title = $page_title ? $page_title : 'WebPageTest';
-      ?>
+    $page_title = $page_title ? $page_title : 'WebPageTest';
+    ?>
     <title><?php echo $page_title; ?></title>
     <?php require_once __DIR__ . '/head.inc'; ?>
     <link href="<?= "/css/account.css?v={$VER_CSS}" ?>" rel="stylesheet" />
 </head>
 
 <body>
-<?php
-$alert = Util::getSetting('alert');
-if (isset($client_error)) {
-  echo '<div class="error-banner">' . $client_error . '</div>';
-} elseif ($alert) {
-  echo '<div class="alert-banner">' . $alert . '</div>';
-}
-?>
+    <?php
+    $alert = Util::getSetting('alert');
+    $alert_expiration = Util::getSetting('alert_expiration');
+    if (isset($client_error)) {
+        echo '<div class="error-banner">' . $client_error . '</div>';
+    } elseif ($alert && $alert_expiration && new DateTime() < new DateTime($alert_expiration)) {
+        echo '<div class="alert-banner">' . $alert . '</div>';
+    } elseif ($alert && empty($alert_expiration)) {
+        echo '<div class="alert-banner">' . $alert . '</div>';
+    }
+    ?>
     <wpt-header>
         <header>
             <a class="wptheader_logo" href="/">
                 <img src="/images/wpt-logo.svg" alt="WebPageTest, by Catchpoint" />
             </a>
             <?php if ($is_plan_free) : ?>
-            <ol class="free-plan">
+                <ol class="free-plan">
                 <?php else : ?>
-                <ol>
+                    <ol>
                     <?php endif; ?>
                     <?php if ($step == 2) : ?>
-                    <li>Choose Plan</li>
-                    <li class="selected">Account Details</li>
-                    <?php if (!$is_plan_free) : ?>
-                    <li>Payment Details</li>
-                    <?php endif; ?>
+                        <li>Choose Plan</li>
+                        <li class="selected">Account Details</li>
+                        <?php if (!$is_plan_free) : ?>
+                            <li>Payment Details</li>
+                        <?php endif; ?>
                     <?php else : ?>
-                    <li>Choose Plan</li>
-                    <li>Account Details</li>
-                    <?php if (!$is_plan_free) : ?>
-                    <li class="selected">Payment Details</li>
+                        <li>Choose Plan</li>
+                        <li>Account Details</li>
+                        <?php if (!$is_plan_free) : ?>
+                            <li class="selected">Payment Details</li>
+                        <?php endif; ?>
                     <?php endif; ?>
-                    <?php endif; ?>
-                </ol>
+                    </ol>
         </header>
     </wpt-header>
     <main>
