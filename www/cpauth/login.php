5abf54c6b665959382bd4da801e4e7f84b64c668
Jeffrey Lembeck <jlembeck@gmail.com>
2022-06-27 11:03:32 -0700

    feat(login): add ReturnUrl param to login
    This allows us to use CP's oauth login service on multiple allowed boxes
    instead of one static one

--- www/cpauth/login.php
+++ www/cpauth/login.php
@@ -6,9 +6,10 @@ require_once __DIR__ . '/../common.inc';
 
 use WebPageTest\Util;
 
+$host = Util::getSetting('host');
+
 if (!Util::getSetting('cp_auth')) {
     $protocol = $request_context->getUrlProtocol();
-    $host = Util::getSetting('host');
     $route = '/';
     $redirect_uri = "{$protocol}://{$host}{$route}";
 
@@ -17,7 +18,7 @@ if (!Util::getSetting('cp_auth')) {
 }
 
 $auth_host = Util::getSetting('cp_auth_host');
-$redirect_uri = "$auth_host/auth/WptAccount/Login";
+$redirect_uri = "{$auth_host}/auth/WptAccount/Login?ReturnUrl=https://{$host}/cpauth";
 
 header("Location: $redirect_uri");
 exit();
