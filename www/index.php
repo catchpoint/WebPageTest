e9dff4930e23a4e0aeff6271808adbe370727500
Scott Jehl <scottjehl@gmail.com>
2022-06-27 22:00:16 +0000

    use aria disabled to allow button focus to occur

--- www/index.php
+++ www/index.php
@@ -262,7 +262,7 @@ require_once __DIR__ . '/head.inc';
 
                             <div class="test_presets_easy_submit">
                                 <small class="test_runs"><span>45 Runs Left</span> | <a href="/account">Upgrade</a></small>
-                                <input type="submit" name="submit" value="Start Test &#8594;" class="start_test">
+                                <input type="submit" name="submit" value="Start Test &#8594;" class="start_test" aria-disabled>
                             </div>
                         </div>
                             </li>
