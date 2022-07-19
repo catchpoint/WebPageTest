d0348eddd0b60814e8c51397829cecf702435fd9
Stoyan Stefanov <stoyan@yahoo.com>
2022-07-19 14:06:37 +0000

    Formatting

--- www/templates/layouts/default.php
+++ www/templates/layouts/default.php
@@ -1,30 +1,33 @@
 <!DOCTYPE html>
 <html>
-  <head>
-<?php
 
-require_once __DIR__ . '/../../common.inc';
+<head>
+  <?php
 
-global $USER_EMAIL;
-global $supportsAuth;
-global $supportsSaml;
-global $supportsCPAuth;
-global $request_context;
-global $_SESSION;
-global $client_error;
-global $test_is_private;
-global $noanalytics;
+  require_once __DIR__ . '/../../common.inc';
 
-$page_title = $page_title ? $page_title : 'WebPageTest';
-?>
+  global $USER_EMAIL;
+  global $supportsAuth;
+  global $supportsSaml;
+  global $supportsCPAuth;
+  global $request_context;
+  global $_SESSION;
+  global $client_error;
+  global $test_is_private;
+  global $noanalytics;
+
+  $page_title = $page_title ? $page_title : 'WebPageTest';
+    ?>
   <title><?php echo $page_title; ?></title>
   <?php require_once __DIR__ . '/head.inc'; ?>
-  </head>
-  <body>
-    <?php require_once __DIR__ . '/header.inc'; ?>
-    <div id="main">
+</head>
+
+<body>
+  <?php require_once __DIR__ . '/header.inc'; ?>
+  <div id="main">
     <?php require_once __DIR__ . '/main_hed.inc'; ?>
     <?php echo $template_output; ?>
     <?php require_once __DIR__ . '/footer.inc'; ?>
-  </body>
+</body>
+
 </html>
