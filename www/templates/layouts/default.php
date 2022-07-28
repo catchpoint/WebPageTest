c0eb84c8912c7bdd7e260a3af074224775eaa4fa
deathbearbrown <deathbearbrown@gmail.com>
2022-07-28 15:01:21 +0000

    update footer on all account pages

--- www/templates/layouts/default.php
+++ www/templates/layouts/default.php
@@ -2,34 +2,35 @@
 <html>
 
 <head>
-  <?php
+    <?php
 
-  require_once __DIR__ . '/../../common.inc';
+    require_once __DIR__ . '/../../common.inc';
 
-  global $USER_EMAIL;
-  global $supportsAuth;
-  global $supportsSaml;
-  global $supportsCPAuth;
-  global $request_context;
-  global $_SESSION;
-  global $client_error;
-  global $test_is_private;
-  global $noanalytics;
+    global $USER_EMAIL;
+    global $supportsAuth;
+    global $supportsSaml;
+    global $supportsCPAuth;
+    global $request_context;
+    global $_SESSION;
+    global $client_error;
+    global $test_is_private;
+    global $noanalytics;
 
-  $page_title = $page_title ? $page_title : 'WebPageTest';
-  $body_class = $body_class ? ' class="' . $body_class . '"' : '';
+    $page_title = $page_title ? $page_title : 'WebPageTest';
+    $body_class = $body_class ? ' class="' . $body_class . '"' : '';
 
     ?>
-  <title><?php echo $page_title; ?></title>
-  <?php require_once __DIR__ . '/head.inc'; ?>
+    <title><?php echo $page_title; ?></title>
+    <?php require_once __DIR__ . '/head.inc'; ?>
 </head>
 
 <body<?php echo $body_class; ?>>
-  <?php require_once __DIR__ . '/header.inc'; ?>
-  <div id="main">
-    <?php require_once __DIR__ . '/main_hed.inc'; ?>
-    <?php echo $template_output; ?>
-    <?php require_once __DIR__ . '/footer.inc'; ?>
+    <?php require_once __DIR__ . '/header.inc'; ?>
+    <div id="main">
+        <?php require_once __DIR__ . '/main_hed.inc'; ?>
+        <?php echo $template_output; ?>
+        <?php require_once __DIR__ . '/../../footer.inc'; ?>
+    </div>
     </body>
 
 </html>
