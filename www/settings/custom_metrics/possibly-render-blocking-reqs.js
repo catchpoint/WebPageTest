dc657ccde861f092544caabb0fa0e444e9e731cc
Scott Jehl <scottjehl@users.noreply.github.com>
2022-08-02 11:24:54 -0400

    custom metric for script and link elements that could potentially block render.
    This is not a guarantee, as these may have been added dynamically by the time this runs. Still, we can use this list to compare in browsers that don't send a render-blocking flag on requests, so we can be more confident that a request that arrived before start render was actually render-blocking. Browsers such as chromium currently give us that information directly so we do not need this in those browsers.

---
+++ www/settings/custom_metrics/possibly-render-blocking-reqs.js
@@ -0,0 +1,5 @@
+let possibleScripts = Array.from(document.querySelectorAll("script[src]:not([defer]):not([async]):not([type=module]")).map(elem => elem.src );
+let possibleStyleSheets = Array.from(document.querySelectorAll("link[href][rel=stylesheet]:not([rel=alternative])")).map(elem => {
+    return matchMedia(elem.media).matches ? elem.href : '';
+});
+return possibleScripts.concat( possibleStyleSheets );
