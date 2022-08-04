780d668ffd0ddc62de9e9f5509e36d0a536f6c86
deathbearbrown <deathbearbrown@gmail.com>
2022-08-04 16:51:25 +0000

    add name to cancel subscription modal

--- www/templates/account/includes/modals/cancel-subscription.php
+++ www/templates/account/includes/modals/cancel-subscription.php
@@ -1,6 +1,6 @@
 <fg-modal id="subscription-plan-modal" class="subscription-plan-modal fg-modal" data-modal="subscription-plan-modal-confirm">
     <h3 class="modal_title">Subscription Details</h3>
-    <p>Active Plan: <?= "{$braintreeCustomerDetails['wptPlanName']}" ?></p>
+    <p>Active Plan: <?= $wptCustomer->getWptPlanName() ?></p>
     <p>Please <a href="https://support.webpagetest.org">contact support</a> with for any upgrades or other changes to your plan.</p>
     <div class="cancel-subscription-button">
         <button class="pill-button red">Cancel Subscription</button>
@@ -11,7 +11,7 @@
     <form method="POST" action="/account">
         <fieldset>
             <legend class="modal_title">Subscription Details</legend>
-            <p>Active Plan: <?= "{$braintreeCustomerDetails['wptPlanName']}" ?></p>
+            <p>Active Plan: <?= $wptCustomer->getWptPlanName() ?></p>
             <button type="submit" class="pill-button red">Cancel Subscription</button>
             <input type="hidden" name="type" value="cancel-subscription" />
             <input type="hidden" name="subscription-id" value="<?= $braintreeCustomerDetails['subscriptionId'] ?>" />
