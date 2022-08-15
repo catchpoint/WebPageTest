94b79fee7cc791b8a3642fb964e23ba42f9431bf
deathbearbrown <deathbearbrown@gmail.com>
2022-08-15 20:25:10 +0000

    change the cc form text color if it's in the account page

--- www/templates/account/includes/chargify-payment-form.php
+++ www/templates/account/includes/chargify-payment-form.php
@@ -1,6 +1,6 @@
 <div class="signup-card-container">
     <div class="signup-card-hed">
-      <h4>Pay with card</h4>
+        <h4>Pay with card</h4>
     </div>
     <div class="signup-card-body">
         <div>
@@ -19,125 +19,129 @@
 </div>
 <script src="https://js.chargify.com/latest/chargify.js"></script>
 <script>
-(() => {
-  var chargify = new Chargify();
+    (() => {
+        var chargify = new Chargify();
+        var textColor = document.querySelectorAll('.my-account-page').length > 0 ? '#111111' : '#ffffff';
+        chargify.load({
+            publicKey: "<?= $ch_client_token ?>",
+            type: 'card',
+            serverHost: "<?= $ch_site ?>", //'https://acme.chargify.com'
+            hideCardImage: true,
+            optionalLabel: ' ',
+            requiredLabel: '*',
+            style: {
+                input: {
+                    fontSize: '1rem',
+                    border: '1px solid #999999',
+                    padding: '.375rem 0.75rem',
+                    lineHeight: '1.5',
+                    backgroundColor: "#ffffff"
+                },
+                label: {
+                    backgroundColor: 'transparent',
+                    paddingTop: '0px',
+                    paddingBottom: '1px',
+                    fontSize: '16px',
+                    fontWeight: '400',
+                    color: textColor
+                }
+            },
+            fields: {
+                firstName: {
+                    selector: '#cc_cardholder_first_name',
+                    label: 'Cardholder first name',
+                    required: true
+                },
+                lastName: {
+                    selector: '#cc_cardholder_last_name',
+                    label: 'Cardholder last name',
+                    required: true
+                },
+                number: {
+                    selector: '#cc_number',
+                    label: 'Card Number',
+                    placeholder: 'Card Number',
+                    message: 'Invalid Card',
+                    required: true,
+                    style: {
+                        input: {
+                            padding: '8px 48px',
+                            width: '494px'
+                        }
+                    }
+                },
+                month: {
+                    selector: '#cc_month',
+                    label: 'Month',
+                    placeholder: 'MM',
+                    message: 'Invalid Month',
+                    required: true,
+                    style: {
+                        input: {
+                            width: '158px'
+                        }
+                    }
+                },
+                year: {
+                    selector: '#cc_year',
+                    label: 'Year',
+                    placeholder: 'YYYY',
+                    message: 'Invalid Year',
+                    required: true,
+                    style: {
+                        input: {
+                            width: '158px'
+                        }
+                    }
+                },
+                cvv: {
+                    selector: '#cc_cvv',
+                    label: 'CVC',
+                    placeholder: 'CVC',
+                    required: true,
+                    message: 'Invalid CVC',
+                    required: true,
+                    style: {
+                        input: {
+                            width: '158px'
+                        }
+                    }
+                }
+            }
+        });
 
-  chargify.load({
-      publicKey: "<?= $ch_client_token ?>",
-      type: 'card',
-      serverHost: "<?= $ch_site ?>", //'https://acme.chargify.com'
-      hideCardImage: true,
-      optionalLabel: ' ',
-      requiredLabel: '*',
-      style: {
-          input: {
-              fontSize: '1rem',
-              border: '1px solid #999999',
-              padding: '.375rem 0.75rem',
-              lineHeight: '1.5',
-              backgroundColor: "#ffffff"
-          },
-          label: {
-              backgroundColor: 'transparent',
-              paddingTop: '0px',
-              paddingBottom: '1px',
-              fontSize: '16px',
-              fontWeight: '400',
-              color: '#ffffff'
-          }
-      },
-      fields: {
-              firstName: {
-                  selector: '#cc_cardholder_first_name',
-                  label: 'Cardholder first name',
-                  required: true
-              },
-              lastName: {
-                  selector: '#cc_cardholder_last_name',
-                  label: 'Cardholder last name',
-                  required: true
-              },
-              number: {
-                  selector: '#cc_number',
-                  label: 'Card Number',
-                  placeholder: 'Card Number',
-                  message: 'Invalid Card',
-                  required: true,
-                  style: {
-                    input: {
-                      padding: '8px 48px',
-                      width: '494px'
-                    }
-                  }
-              },
-              month: {
-                  selector: '#cc_month',
-                  label: 'Month',
-                  placeholder: 'MM',
-                  message: 'Invalid Month',
-                  required: true,
-                  style: {
-                    input: {
-                      width: '158px'
-                    }
-                  }
-              },
-              year: {
-                  selector: '#cc_year',
-                  label: 'Year',
-                  placeholder: 'YYYY',
-                  message: 'Invalid Year',
-                  required: true,
-                  style: {
-                    input: {
-                      width: '158px'
-                    }
-                  }
-              },
-              cvv: {
-                  selector: '#cc_cvv',
-                  label: 'CVC',
-                  placeholder: 'CVC',
-                  required: true,
-                  message: 'Invalid CVC',
-                  required: true,
-                  style: {
-                    input: {
-                      width: '158px'
-                    }
-                  }
-              }
-          }
-  });
-
-  window.chargify = chargify;
-})();
+        window.chargify = chargify;
+    })();
 </script>
 
 <style>
-.signup-card-container {
-    border: 1px solid #334870;
-    border-radius: 8px;
-}
+    .signup-card-container {
+        border: 1px solid #334870;
+        border-radius: 8px;
+    }
 
-.signup-card-hed,
-.signup-card-body {
-    padding: 0 24px;
-}
-.signup-card-hed {
-    border-bottom: 1px solid #334870;
-}
-.signup-card-hed h4 {
-    font-weight: 400;
-    margin: 0;
-}
-#cc_cardholder_first_name iframe,
-#cc_cardholder_last_name iframe {
-    width: 235px !important;
-}
-#cc_year iframe,
-#cc_month iframe,
-#cc_cvv iframe {
-    width: 158px !important;
-}
+    .signup-card-hed,
+    .signup-card-body {
+        padding: 0 24px;
+    }
+
+    .signup-card-hed {
+        border-bottom: 1px solid #334870;
+    }
+
+    .signup-card-hed h4 {
+        font-weight: 400;
+        margin: 0;
+    }
+
+    #cc_cardholder_first_name iframe,
+    #cc_cardholder_last_name iframe {
+        width: 235px !important;
+    }
+
+    #cc_year iframe,
+    #cc_month iframe,
+    #cc_cvv iframe {
+        width: 158px !important;
+    }
 </style>
