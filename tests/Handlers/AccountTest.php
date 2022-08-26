6cc7a68c38d0dfd32fc0a24b83d2c538517e0510
Jeffrey Lembeck <jlembeck@gmail.com>
2022-08-25 17:22:37 -0700

    chore(account handler): tests and types
    Add more tests to the handler. Add more types

--- tests/Handlers/AccountTest.php
+++ tests/Handlers/AccountTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace WebPageTest\Handlers;
 
+use stdClass;
 use PHPUnit\Framework\TestCase;
+use WebPageTest\CPClient;
 use WebPageTest\Handlers\Account;
 use WebPageTest\Exception\ClientException;
 use WebPageTest\RequestContext;
@@ -45,5 +47,97 @@ final class AccountTest extends TestCase
         $req = new RequestContext([]);
         $url = Account::postPlanUpgrade($req, $post_body);
         $this->assertEquals('http://127.0.0.1/account/plan_summary', $url);
+    }
+
+    public function testPostUpdatePlanSummary(): void
+    {
+        $body = [
+            'subscription_id' => 'abcdef',
+            'plan' => 'ap74'
+        ];
+
+        $client = $this->createMock(CPClient::class);
+        $client->expects($this->once())
+          ->method('updatePlan')
+          ->with('abcdef', 'ap74');
+
+        $req = new RequestContext([]);
+        $req->setClient($client); // intelephense gets made about MockObject getting passed here
+        $url = Account::postUpdatePlanSummary($req, $body);
+
+        $this->assertEquals('http://127.0.0.1/account', $url);
+    }
+
+    public function testValidateChangeContactInfo(): void
+    {
+        $expected = new stdClass();
+        $expected->first_name = "Glooby";
+        $expected->last_name = "Plz";
+        $expected->id = "5";
+        $expected->company_name = "Catchpoint";
+
+        $body = [
+          'first-name' => "Glooby",
+          'last-name' => "Plz",
+          'id' => "5",
+          'company-name' => "Catchpoint"
+        ];
+
+        $actual = Account::validateChangeContactInfo($body);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testValidateChangeContactInfoMissingId(): void
+    {
+        $body = [
+          'first-name' => "Glooby",
+          'last-name' => "Plz",
+          'company-name' => "Catchpoint"
+        ];
+
+        $this->expectException(ClientException::class);
+        Account::validateChangeContactInfo($body);
+    }
+
+    public function testValidateChangeContactInfoMissingFirstName(): void
+    {
+        $body = [
+          'id' => "5",
+          'last-name' => "Plz",
+          'company-name' => "Catchpoint"
+        ];
+
+        $this->expectException(ClientException::class);
+        Account::validateChangeContactInfo($body);
+    }
+
+    public function testValidateChangeContactInfoMissingLastName(): void
+    {
+        $body = [
+          'id' => "5",
+          'first-name' => "Plz",
+          'company-name' => "Catchpoint"
+        ];
+
+        $this->expectException(ClientException::class);
+        Account::validateChangeContactInfo($body);
+    }
+
+    public function testValidateChangeContactInfoNoCompanyName(): void
+    {
+        $expected = new stdClass();
+        $expected->first_name = "Glooby";
+        $expected->last_name = "Plz";
+        $expected->id = "5";
+
+        $body = [
+          'id' => "5",
+          'first-name' => "Glooby",
+          'last-name' => "Plz"
+        ];
+
+        $actual = Account::validateChangeContactInfo($body);
+        $this->assertEquals($expected, $actual);
     }
 }
