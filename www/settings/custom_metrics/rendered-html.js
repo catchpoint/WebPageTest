0b7f674474ef7fef08df82f3dc347b8c7d5355c5
Scott Jehl <scottjehl@gmail.com>
2022-09-16 20:50:42 +0000

    set style elements' css to their cssom explicitly before collecting rendered html

--- www/settings/custom_metrics/rendered-html.js
+++ www/settings/custom_metrics/rendered-html.js
@@ -1,1 +1,10 @@
+// fix up that output in case there are style elements with cssom rules not in the source
+// this often happens with dynamic stylesheet frameworks
+let styles = document.querySelectorAll('style').forEach(style => {
+    let thisText = "";
+    for (let i in style.sheet.cssRules) {
+        thisText += style.sheet.cssRules[i].cssText;
+    }
+    style.innerText = "/* inner styles set by WPT to match CSSOM */" + thisText;
+});
 return document.documentElement.outerHTML;
