f24398d00b5491913f59cef6e8d7297856c222e7
Stoyan Stefanov <stoyan@yahoo.com>
2022-11-18 19:48:28 +0000

    Anchor-able line numbers

--- www/resources/views/pages/htmldiff.blade.php
+++ www/resources/views/pages/htmldiff.blade.php
@@ -2,23 +2,41 @@
 
 @section('style')
 <style>
-del {
-    text-decoration: none;
-    color: #b30000;
-    background: #fadad7;
-}
-ins {
-    background: #eaf2c2;
-    color: #406619;
-    text-decoration: none;
-}
-#delivered, #rendered {
-    display: none;
-}
-#diff-result {
-    white-space: pre-wrap;
-    font-family: Consolas, Monaco, Andale Mono, Ubuntu Mono, monospace;
-}
+    del {
+        text-decoration: none;
+        color: #b30000;
+        background: #fadad7;
+    }
+
+    ins {
+        background: #eaf2c2;
+        color: #406619;
+        text-decoration: none;
+    }
+
+    a.del {
+        color: #b30000;
+    }
+
+    a.ins {
+        color: #406619;
+    }
+
+    #delivered,
+    #rendered {
+        display: none;
+    }
+
+    pre {
+        white-space: pre-wrap;
+        font-family: Consolas, Monaco, Andale Mono, Ubuntu Mono, monospace;
+        margin: 0;
+    }
+    pre a {
+        text-decoration: none;
+        color: #888;
+    }
+
 </style>
 @endsection
 
@@ -32,9 +50,9 @@ ins {
             </div>
         </div>
         @if ($error_message)
-            <div id="result" class="results_body error-banner">
-                <div>{{ $error_message }}</div>
-            </div>
+        <div id="result" class="results_body error-banner">
+            <div>{{ $error_message }}</div>
+        </div>
         @else
         <label>
             <input type="checkbox" id="prettier">
@@ -49,6 +67,7 @@ ins {
         </div>
         <script src="/assets/js/vendor/diff-5.1.0.min.js"></script>
         <script type="module">
+            let locateHash = false;
             async function diff() {
                 let before = document.getElementById('delivered').innerText;
                 let after = document.getElementById('rendered').innerText;
@@ -66,32 +85,69 @@ ins {
                 }
 
                 const diff = Diff.diffLines(before, after);
-                const fragment = document.createDocumentFragment();
+                const table = document.createElement('table');
+                let addedLineCount = 0;
+                let removedLineCount = 0;
+                let pretty_href = document.getElementById('prettier').checked ? '_pretty' : '';
                 for (let i = 0; i < diff.length; i++) {
-
-                    if (diff[i].added && diff[i + 1] && diff[i + 1].removed) {
-                        let swap = diff[i];
-                        diff[i] = diff[i + 1];
-                        diff[i + 1] = swap;
-                    }
-
-                    let node;
-                    if (diff[i].removed) {
-                        node = document.createElement('del');
-                        node.appendChild(document.createTextNode(diff[i].value));
-                    } else if (diff[i].added) {
-                        node = document.createElement('ins');
-                        node.appendChild(document.createTextNode(diff[i].value));
-                    } else {
-                        node = document.createTextNode(diff[i].value);
-                    }
-                    fragment.appendChild(node);
+                    diff[i].value.split('\n').forEach(value => {
+                        let row = document.createElement('tr');
+                        let td, node;
+                        if (diff[i].removed) {
+                            removedLineCount++;
+                            td = document.createElement('td');
+                            td.innerHTML = `<a class="del" name="removed_${removedLineCount}${pretty_href}" href="#removed_${removedLineCount}${pretty_href}">${removedLineCount}</a>`;
+                            row.appendChild(td);
+                            td = document.createElement('td');
+                            td.appendChild(document.createTextNode(''));
+                            row.appendChild(td);
+                            node = document.createElement('del');
+                            node.appendChild(document.createTextNode(value));
+                        } else if (diff[i].added) {
+                            addedLineCount++;
+                            td = document.createElement('td');
+                            td.appendChild(document.createTextNode(''));
+                            row.appendChild(td);
+                            td = document.createElement('td');
+                            td.innerHTML = `<a class="ins" name="added_${addedLineCount}${pretty_href}" href="#added_${addedLineCount}${pretty_href}">${addedLineCount}</a>`;
+                            row.appendChild(td);
+                            node = document.createElement('ins');
+                            node.appendChild(document.createTextNode(value));
+                        } else {
+                            addedLineCount++;
+                            removedLineCount++;
+                            td = document.createElement('td');
+                            td.innerHTML = `<a name="removed_${removedLineCount}${pretty_href}" href="#removed_${removedLineCount}${pretty_href}">${removedLineCount}</a>`;
+                            row.appendChild(td);
+                            td = document.createElement('td');
+                            td.innerHTML = `<a name="added_${addedLineCount}${pretty_href}" href="#added_${addedLineCount}${pretty_href}">${addedLineCount}</a>`;
+                            row.appendChild(td);
+                            node = document.createTextNode(value);
+                        }
+                        td = document.createElement('td');
+                        td.setAttribute('class', 'left');
+                        let pre = document.createElement('pre');
+                        pre.appendChild(node);
+                        td.appendChild(pre);
+                        row.appendChild(td);
+                        table.appendChild(row);
+                    });
                 }
                 document.getElementById('diff-result').innerHTML = '';
-                document.getElementById('diff-result').appendChild(fragment);
+                document.getElementById('diff-result').appendChild(table);
+                if (locateHash) {
+                    location = location.hash;
+                    locateHash = false;
+                }
             }
-            diff();
+
             document.getElementById('prettier').addEventListener('change', diff);
+            if (location.hash.endsWith('_pretty')) {
+                document.getElementById('prettier').click();
+                locateHash = true;
+            } else {
+                diff();
+            }
         </script>
         @endif
 
