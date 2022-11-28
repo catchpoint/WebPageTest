db0df25f24c29171860c8b1ef692d73daff76474
Stoyan Stefanov <stoyan@yahoo.com>
2022-11-28 19:07:36 +0000

    Simpler and better stingification

--- www/settings/custom_metrics/doctype.js
+++ www/settings/custom_metrics/doctype.js
@@ -1,16 +1,1 @@
-if (!document.doctype) {
-  return "";
-}
-const doctype = document.doctype;
-const parts = ["<!DOCTYPE", " "];
-parts.push(doctype.name);
-if (doctype.publicId) {
-  parts.push(" PUBLIC ");
-  parts.push(`"${doctype.publicId}"`);
-}
-if (doctype.systemId) {
-  parts.push(" ");
-  parts.push(`"${doctype.systemId}"`);
-}
-parts.push(">");
-return parts.join("");
+return new XMLSerializer().serializeToString(document.doctype);
