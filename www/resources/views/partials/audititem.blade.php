3f5d9d9215fc7dec0498a4593c2092070b2692fc
Scott Jehl <scottjehl@gmail.com>
2022-12-09 18:03:46 +0000

    show details when fail or avg

--- www/resources/views/partials/audititem.blade.php
+++ www/resources/views/partials/audititem.blade.php
@@ -1,9 +1,10 @@
 <li class="experiments_details lh_audit-{{ $audit->scoreDisplayMode }} lh_audit-{{ $audit->scoreDescription }}">
-    <details>
+    <details @if ($audit->scoreDescription === "fail" || $audit->scoreDescription === "average") open @endif>
         <summary>{!! md($audit->title) !!}</summary>
         <div class="experiments_details_body">
             <div class="experiments_details_desc">
                 <p>{!! md($audit->description) !!}</p>
+                @include('partials.details')
             </div>
     </details>
 </li>
