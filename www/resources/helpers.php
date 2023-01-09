d27246eeeac2d9a6d2641d712e1983709644e877
Stoyan Stefanov <stoyan@yahoo.com>
2023-01-09 20:59:39 +0000

    Subitems in a subtable

--- www/resources/helpers.php
+++ www/resources/helpers.php
@@ -4,3 +4,10 @@ function md($in)
 {
     return Illuminate\Support\Str::of($in)->markdown();
 }
+
+function prettyKey($key) {
+    if (strtolower($key) === 'url') {
+        return 'URL';
+    }
+    return Illuminate\Support\Str::headline($key);
+}
--- www/resources/views/partials/lighthouse/details.blade.php
+++ www/resources/views/partials/lighthouse/details.blade.php
@@ -14,17 +14,24 @@ $subkeys = [];
             <tr>
                 @foreach ($audit->details->headings as $heading)
                     <?php
-                    $itemkeys[] = $heading->key;
-                    $subkeys[]  = $heading->subItemsHeading->key;
+                    $headingText = $heading->text ?? $heading->label;
+                    if ($headingText) {
+                        $itemkeys[] = $heading->key;
+                    }
+                    if ($heading->subItemsHeading) {
+                        $subkeys[]  = $heading->subItemsHeading->key;
+                    }
                     ?>
+                    @if ($headingText)
                     <th class="{{ $heading->key }}">
-                        {{ $heading->text ?? $heading->label }}
+                        {{ $headingText }}
                         @if ($heading->valueType && in_array($heading->valueType, ['bytes', 'timespanMs']))
                             ({{ $heading->valueType === 'timespanMs' ? 'ms' : $heading->valueType}})
                         @elseif ($heading->itemType && in_array($heading->itemType, ['bytes', 'ms']))
                             ({{ $heading->itemType }})
                         @endif
                     </th>
+                    @endif
                 @endforeach
             </tr>
         </thead>
@@ -35,15 +42,29 @@ $subkeys = [];
                         <x-lhdetail :item="$item->$key" />
                     @endforeach
                 </tr>
-                @if ($item->subItems)
-
-                @foreach ($item->subItems->items as $sub)
-                    <tr class="lh-subitem">
-                        @foreach ($subkeys as $subkey)
-                            <x-lhdetail :item="$sub->$subkey" />
-                        @endforeach
+                @if ($item->subItems && count($item->subItems->items))
+                    <tr>
+                        <td colspan="{{ count($itemkeys) }}">
+                            <table width="100%">
+                                <thead>
+                                    <tr>
+                                        @foreach ($subkeys as $subkey)
+                                        <th>{{ prettyKey($subkey) }}</th>
+                                        @endforeach
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($item->subItems->items as $sub)
+                                        <tr>
+                                            @foreach ($subkeys as $subkey)
+                                                <x-lhdetail :item="$sub->$subkey" />
+                                            @endforeach
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </td>
                     </tr>
-                    @endforeach
                 @endif
 
             @endforeach
