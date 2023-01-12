dab283e61693af868e4f0777af4ff9d0cbb8017c
Stoyan Stefanov <stoyan@yahoo.com>
2023-01-12 21:28:29 +0000

    Refactor opportunities & experiments includes
    * Refactoring due to a recent global var mishap
    * Add `AssesmentRegistry` singleton
    * Each opp calls `register()` to add an opportunity

---
+++ www/experiments/AssesmentRegistry.php
@@ -0,0 +1,64 @@
+<?php
+
+class AssesmentRegistry {
+    private static $instance = null;
+
+    const Quick = 'Quick';
+    const Usable = 'Usable';
+    const Resilient = 'Resilient';
+    const Custom = 'Custom';
+
+    private $assessments = [
+        self::Quick => [
+            'grade' => '',
+            'sentiment' => '',
+            'summary' => '',
+            'opportunities' => [],
+            'num_recommended' => 0,
+            'num_experiments' => 0
+        ],
+        self::Usable => [
+            'grade' => '',
+            'sentiment' => '',
+            'summary' => '',
+            'opportunities' => [],
+            'num_recommended' => 0,
+            'num_experiments' => 0
+        ],
+        self::Resilient => [
+            'grade' => '',
+            'sentiment' => '',
+            'summary' => '',
+            'opportunities' => [],
+            'num_recommended' => 0,
+            'num_experiments' => 0
+        ],
+        self::Custom => [
+            'grade' => '',
+            'sentiment' => 'Advanced!',
+            'summary' => '',
+            'opportunities' => [],
+            'num_recommended' => 0,
+            'num_experiments' => 0
+        ],
+    ];
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new AssesmentRegistry();
+        }
+
+        return self::$instance;
+    }
+
+    public function register($category, $opportunity) {
+        $this->assessments[$category]['opportunities'][] = $opportunity;
+    }
+
+    public function getAll() {
+        return $this->assessments;
+    }
+
+}
+
