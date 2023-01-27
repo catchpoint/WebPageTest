45e22e790e6cbb97c7243160a5486306318b852a
Scott Jehl <scottjehl@gmail.com>
2023-01-27 16:42:30 +0000

    move var outside if

--- www/experiments/getExperimentHTML.php
+++ www/experiments/getExperimentHTML.php
@@ -3,10 +3,9 @@ require_once '../common.inc';
 use WebPageTest\Util;
 require_once '../include/TestInfo.php';
 $testInfo = json_decode(gz_file_get_contents("../$testPath/testinfo.json.gz"));
-
+$html = "";
 if($testInfo && $testInfo->metadata){
     $recipes = json_decode($testInfo->metadata)->experiment->recipes;
-    $html = "";
 
     foreach($recipes as $recipe){
         if( $recipe->{'054'} ){
