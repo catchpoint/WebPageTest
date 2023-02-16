ab0700e4411dc4e7f8fe8cdd3ff8cad83fce9b80
Jeffrey Lembeck <jlembeck@gmail.com>
2023-02-16 05:34:01 -0800

    fix(account): log user out if contact info is null
    This means that the user's logged in client is suspended by admin while
    they were logged in. This will log them out, so they can log in and use
    the correct client id
    
    Also, change the psalm setup because composer build is blowing up and
    needs to be fixed, but it's just a dev helper right now

--- www/src/CPClient.php
+++ www/src/CPClient.php
@@ -261,7 +261,7 @@ class CPClient
     }
 
 
-    public function getUserContactInfo(int $id): array
+    public function getUserContactInfo(int $id): ?array
     {
         $gql = (new Query('contact'))
             ->setVariables([
