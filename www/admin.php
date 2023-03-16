279a0855caed1336b79703cd3ac78defe4508fb6
Jeffrey Lembeck <jlembeck@gmail.com>
2023-03-16 09:25:56 -0700

    fix(admin): move cache_check behind admin handler

--- www/admin.php
+++ www/admin.php
@@ -5,14 +5,21 @@ declare(strict_types=1);
 require_once __DIR__ . '/common.inc';
 
 use WebPageTest\Handlers\Admin as AdminHandler;
+use WebPageTest\RequestContext;
 
-$page = (string) filter_input(INPUT_GET, 'page', FILTER_SANITIZE_STRING);
-switch ($page) {
-    case ('chargify-sandbox'):
-        $contents = AdminHandler::getChargifySandbox($request_context);
-        echo $contents;
-        exit();
-    default:
-        http_response_code(404);
-        die();
-}
+(function (RequestContext $request_context) {
+    $page = (string) filter_input(INPUT_GET, 'page', FILTER_UNSAFE_RAW);
+    switch ($page) {
+        case ('chargify-sandbox'):
+            $response = AdminHandler::getChargifySandbox($request_context);
+            $response->send();
+            exit();
+        case ('cache-check'):
+            $response = AdminHandler::cacheCheck($request_context);
+            $response->send();
+            exit();
+        default:
+            http_response_code(404);
+            die();
+    }
+})($request_context);
