c24a83546eadd84313af261ad1a2f20d5d1d0037
Stoyan Stefanov <stoyan@yahoo.com>
2023-04-24 19:31:49 +0000

    - youtube

--- www/resources/views/pages/blocked.blade.php
+++ www/resources/views/pages/blocked.blade.php
@@ -6,8 +6,7 @@
     <h1>Oops...</h1>
     <p>Your test request was intercepted by our abuse filters
         (or because we need to talk to you about how you are submitting tests or the volume of tests you are submitting).</p>
-    <p>Most free web hosts have been blocked from testing because of excessive link spam and
-        YouTube has also been blocked due to spammers abusing WebPageTest to inflate video views.</p>
+    <p>Most free web hosts have been blocked from testing because of excessive link spam.</p>
     <p>If you are trying to test with an URL-shortener (t.co, goo.gl, bit.ly, etc) then those are also blocked, please test the URL directly.</p>
     @if ($contact)
         <p>If there is a site you want tested that was blocked, please <a href="mailto:{{ $contact }}">contact us</a>
