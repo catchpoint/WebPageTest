b427efa123027cb6d25a79f1ca2f03fe53a2e74f
Stoyan Stefanov <stoyan@yahoo.com>
2023-04-26 16:56:34 +0000

    unbreak under construction page and move to Blade

--- www/construction.php
+++ www/construction.php
@@ -3,29 +3,11 @@
 // Copyright 2020 Catchpoint Systems Inc.
 // Use of this source code is governed by the Polyform Shield 1.0.0 license that can be
 // found in the LICENSE.md file.
-include 'common.inc';
-?>
-<!DOCTYPE html>
-<html lang="en-us">
-    <head>
-        <title>WebPageTest - Under Construction</title>
-        <?php include('head.inc'); ?>
-    </head>
-    <body>
-        <div class="page">
-            <?php
-            $tab = 'Home';
-            include 'header.inc';
-            ?>
 
-            <div class="translucent">
-                <p>
-                WebPageTest is currently unavailable - A router software update went bad and we are in the process of getting things running again.
-                Shouldn't be down for more than a couple of hours, sorry for the inconvenience (and a perfect case of the risks involved in remotely updating code).
-                </p>
-            </div>
 
-            <?php include('footer.inc'); ?>
-        </div>
-    </body>
-</html>
+require_once __DIR__ . '/common.inc';
+echo view('pages.construction', [
+    'page_title' => 'WebPageTest - Under Construction',
+    'body_class' => 'four-oh-four',
+]);
+
