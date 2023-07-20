93ebb8df7fb40a8ec30cadf111a98952db1c2892
Claudio Biselli <cbiselli89@gmail.om>
2023-07-20 13:49:18 +0000

    refactored error template

--- www/templates/errors/runtest.php
+++ www/templates/errors/runtest.php
@@ -1,12 +1,4 @@
 <div class="testerror box">
-  <h1>Oops! <em>
-  <?php
-    $title = "There was a problem with the test.";
-    if (strlen($errorTitle)) {
-	$title = $errorTitle ;
-    }
-    echo $title;
-  ?>
-  </em></h1>
-  <?php echo $error; ?>
+  <h1>Oops! <em><?= $errorTitle ?? "There was a problem with the test." ?></em></h1>
+  <?= $error ?>
 </div>
