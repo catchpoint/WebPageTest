b2e8a9cea704f02bbf0cb56db6a2f00c5e38f5f6
Hernan Claveria <Hernan.Claveria@endava.com>
2023-10-31 08:51:02 -0500

    Adjustment on styling

--- www/templates/account/includes/modals/contact-info.php
+++ www/templates/account/includes/modals/contact-info.php
@@ -32,10 +32,10 @@
             <input type="hidden" name="type" value="contact-info" />
             <input type="hidden" name="csrf_token" value="<?= $csrf_token ?>" />
             <div class="section">
-				<div class="save-button">
-					<button type="submit" class="pill-button blue">Save</button>
-				</div>
-			</div>
+                <div class="save-button">
+                    <button type="submit" class="pill-button blue">Save</button>
+                </div>
+            </div>
         </div>
     </form>
 </fg-modal>
