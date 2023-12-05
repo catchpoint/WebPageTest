7f00a1e2681823dced6f590cb8ab1c7eb62690fa
Hernan Claveria <Hernan.Claveria@endava.com>
2023-12-05 09:41:40 -0500

    Format fix

--- www/resources/views/pages/testhistory.blade.php
+++ www/resources/views/pages/testhistory.blade.php
@@ -52,7 +52,7 @@
                             <tr>
                                 <th class="history_checkbox"><input type="checkbox" name="t[]" value="{{ $record->getTestId() }}" aria-label="Select this test" /></th>
                                 <td class="url scrollable-td"><div class="scrollable-td-content"><a href="/result/{{ $record->getTestId() }}/">{{ $record->getUrl() }}</a></div></td>
-                                <td class="date">{{ date_format(date_create($record->getStartTime()), 'M d, Y g:i:s A e') }}</td>                                
+                                <td class="date">{{ date_format(date_create($record->getStartTime()), 'M d, Y g:i:s A e') }}</td>
                                 <td class="location scrollable-td"><div class="scrollable-td-content">{{ $record->getLocation() }}</div></td>
                                 <td class="label">{{ $record->getLabel() }}</td>
                             </tr>
